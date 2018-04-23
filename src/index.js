63ea7abbffef0acacc4f81022b286d171b1d752c
William Viker <william@trippelm.no>
2018-04-24 00:09:37 +0200

    several changes, yarn, updated submodules, etc.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 var EventEmitter = require('events');
 var system = new EventEmitter();
 
-setInterval(function() {
-	system.emit('status', Date.now());
-}, 100);
+system.emit('skeleton-log', "Version");
+
+var config = new (require('../bitfocus-libs/config'))(system, {
+	http_port: 8000,
+	http_bind: "0.0.0.0"
+});
+
+system.on('config_loaded', function(config) {
+	console.log("config", config)	
+})
 
 exports = module.exports = function() {
 	return system;
