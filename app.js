6fb3f8cfd71fd00bba48ec2c645aa11eb9f896f2
William Viker <william@trippelm.no>
2018-04-26 21:19:15 +0200

    network interface selector

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var fs = require("fs");
 
 var config = new (require('./bitfocus-libs/config'))(system, {
 	http_port: 8000,
-	http_bind: "0.0.0.0"
+	bind_ip: "127.0.0.1",
 });
 
 var packageinfo = new (require('./bitfocus-libs/packageinfo'))(system);
@@ -13,18 +13,25 @@ console.log("packageinfo", packageinfo);
 
 var http   = new (require('./lib/http'))(system);
 var elgato = new (require('./lib/elgato'))(system);
+
 system.on('skeleton-ready', function() {
-	console.log("SKEL READY");
-	system.emit('skeleton-log', JSON.stringify({path: fs.readdirSync(__dirname),pak:packageinfo }));
+	system.emit('skeleton-log', JSON.stringify({  }));
 });
 
 system.on('config_loaded', function(config) {
 	system.emit('skeleton-info', 'appName', packageinfo.description);
 	system.emit('skeleton-info', 'appVersion', packageinfo.version);
-	system.emit('skeleton-info', 'appURL', 'http://127.0.0.1:8000');
+  //system.emit('skeleton-info', 'appURL', 'http://'+config.bind_ip+':'+config.http_port);
 	system.emit('skeleton-info', 'appStatus', 'Server running');
+	system.emit('skeleton-info', 'bindInterface', config.bind_ip);
 });
 
+system.on('skeleton-bind-ip', function(ip) {
+	system.emit('config_set', 'bind_ip', ip);
+	config.bind_ip = ip;
+	console.log("sysip", config.bind_ip);
+	system.emit('ip_rebind');
+});
 
 system.on('exit', function() {
 	console.log("somewhere, the system wants to exit. kthxbai");
