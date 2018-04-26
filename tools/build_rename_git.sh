5d49776faafae70a01bcdfef6fe86756475ccfc0
William Viker <william@trippelm.no>
2018-04-26 22:02:59 +0200

    proper build filenames

---
+++ tools/build_rename_git.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+
+function parse_git_dirty() {
+  git diff --quiet --ignore-submodules HEAD 2>/dev/null; [ $? -eq 1 ] && echo ""
+}
+
+# gets the current git branch
+function parse_git_branch() {
+  git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e "s/* \(.*\)/\1$(parse_git_dirty)/"
+}
+
+# get last commit hash prepended with @ (i.e. @8a323d0)
+function parse_git_hash() {
+  git rev-parse --short HEAD 2> /dev/null | sed "s/\(.*\)/\1/"
+}
+
+function release() {
+  cat package.json |grep \"version\"|cut -f4 -d\"
+}
+ 
+# DEMO
+GIT_BRANCH=$(release)-$(parse_git_hash)
+
+mv electron-output/companion-win64 electron-output/companion-${GIT_BRANCH}-win64.exe
