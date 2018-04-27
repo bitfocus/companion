d82b6851953e540ca631fb126b07b1737ce1d92a
William Viker <william@trippelm.no>
2018-04-27 16:47:52 +0200

    getting versioning in packages to work

--- app.js
+++ app.js
@@ -1,18 +1,14 @@
 var EventEmitter = require('events');
 var system = new EventEmitter();
 var fs = require("fs");
+var path = require('path')
 
 var config = new (require('./bitfocus-libs/config'))(system, {
 	http_port: 8000,
 	bind_ip: "127.0.0.1",
 });
 
-var packageinfo = new (require('./bitfocus-libs/packageinfo'))(system);
-
 system.on('config_loaded', function(config) {
-	var build = new String(fs.readFileSync( '../BUILD' ));
-	system.emit('skeleton-info', 'appName', packageinfo.description);
-	system.emit('skeleton-info', 'appVersion', packageinfo.version + " (" + build.trim() + ")" );
   system.emit('skeleton-info', 'appURL', 'Waiting for webserver..');
 	system.emit('skeleton-info', 'appStatus', 'Starting');
 	system.emit('skeleton-info', 'bindInterface', config.bind_ip);
