0fd83b64cdc31c9f46a334dc347ca97455624aa9
William Viker <william@trippelm.no>
2018-04-27 12:46:10 +0200

    interface selector and catching exceptions for listen-errors

--- app.js
+++ app.js
@@ -8,35 +8,31 @@ var config = new (require('./bitfocus-libs/config'))(system, {
 });
 
 var packageinfo = new (require('./bitfocus-libs/packageinfo'))(system);
-console.log("packageinfo", packageinfo);
-
-
-var http   = new (require('./lib/http'))(system);
-var elgato = new (require('./lib/elgato'))(system);
-
-system.on('skeleton-ready', function() {
-	system.emit('skeleton-log', JSON.stringify({  }));
-});
 
 system.on('config_loaded', function(config) {
+	var build = new String(fs.readFileSync( '../BUILD' ));
 	system.emit('skeleton-info', 'appName', packageinfo.description);
-	system.emit('skeleton-info', 'appVersion', packageinfo.version);
-  //system.emit('skeleton-info', 'appURL', 'http://'+config.bind_ip+':'+config.http_port);
-	system.emit('skeleton-info', 'appStatus', 'Server running');
+	system.emit('skeleton-info', 'appVersion', packageinfo.version + " (" + build.trim() + ")" );
+  system.emit('skeleton-info', 'appURL', 'Waiting for webserver..');
+	system.emit('skeleton-info', 'appStatus', 'Starting');
 	system.emit('skeleton-info', 'bindInterface', config.bind_ip);
-});
-
-system.on('skeleton-bind-ip', function(ip) {
-	system.emit('config_set', 'bind_ip', ip);
-	config.bind_ip = ip;
-	console.log("sysip", config.bind_ip);
-	system.emit('ip_rebind');
 });
 
 system.on('exit', function() {
 	console.log("somewhere, the system wants to exit. kthxbai");
 	process.exit();
 })
+
+system.on('skeleton-bind-ip', function(ip) {
+	config.bind_ip = ip;
+	system.emit('config_set', 'bind_ip', ip);
+	system.emit('ip_rebind');
+});
+
+system.on('skeleton-ready', function() {
+	var http   = new (require('./lib/http'))(system);
+	var elgato = new (require('./lib/elgato'))(system);
+});
 
 exports = module.exports = function() {
 	return system;
