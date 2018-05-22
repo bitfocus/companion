f33969b85d077b1de3134610e8c4d50889e52bbc
William Viker <william@trippelm.no>
2018-05-22 11:48:27 +0200

    preparing for companion beta1

--- app.js
+++ app.js
@@ -5,11 +5,23 @@ var system = new EventEmitter();
 var fs = require("fs");
 var path = require('path')
 var debug = require('debug')('app');
+var mkdirp = require('mkdirp');
+var config;
+var cfgDir;
 
-var config = new (require('./bitfocus-libs/config'))(system, {
-	http_port: 8000,
-	bind_ip: "127.0.0.1",
-});
+system.on('skeleton-info', function(key, val) {
+		if (key == 'configDir') {
+			debug('configuration directory', val);
+			cfgDir = val + "/elgato/";
+			mkdirp(cfgDir, function(err) {
+				debug("mkdirp",cfgDir,err);
+				config = new (require('./bitfocus-libs/config'))(system, cfgDir, {
+					http_port: 8000,
+					bind_ip: "127.0.0.1"
+				});
+			});
+		}
+ });
 
 system.on('config_loaded', function(config) {
 	system.emit('skeleton-info', 'appURL', 'Waiting for webserver..');
@@ -24,6 +36,7 @@ system.on('exit', function() {
 	});
 });
 
+
 system.on('skeleton-bind-ip', function(ip) {
 	config.bind_ip = ip;
 	system.emit('config_set', 'bind_ip', ip);
@@ -34,7 +47,7 @@ system.on('skeleton-ready', function() {
 
 	var http = require('./lib/http')(system, 80);
 	var io   = require('./lib/io')(system, http);
-	var db = new (require('./lib/db'))(system);
+	var db = new (require('./lib/db'))(system,cfgDir);
 	var appRoot = require('app-root-path');
 	var express = require('express');
 	var panel = new (require('./lib/elgato'))(system);
---
+++ tools/build.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+yarn
+npm run dist
