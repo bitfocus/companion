b2bcfa629ba156c19adb72aced910b1ebaa60a4c
William Viker <william@trippelm.no>
2018-05-30 01:11:28 +0200

    Implementing dropdown options for actions

--- public/action.js
+++ public/action.js
@@ -9,9 +9,14 @@ $(function() {
 		$("#addBankAction").val($("#addBankAction option:first").val());
 	});
 
-	$('#bankActions').on('keyup', '.action-option', function() {
+	$('#bankActions').on('keyup', '.action-option-keyup', function() {
 		socket.emit('bank_update_action_option', page, bank,  $(this).data('action-id'), $(this).data('option-id'), $(this).val() );
 	});
+	$('#bankActions').on('change', '.action-option-change', function() {
+		socket.emit('bank_update_action_option', page, bank,  $(this).data('action-id'), $(this).data('option-id'), $(this).val() );
+	});
+
+
 
 	socket.on('bank_getActions:result', function(page, bank, actions) {
 
@@ -53,8 +58,11 @@ $(function() {
 						var $opt_label = $("<label>"+option.label+"</label>");
 						$options.append($opt_label);
 
+
+
+
 						if (option.type == 'textinput') {
-							var $opt_input = $("<input type='text' class='action-option form-control'>");
+							var $opt_input = $("<input type='text' class='action-option-keyup form-control'>");
 							$opt_input.data('action-id', action.id);
 							$opt_input.data('option-id', option.id);
 
@@ -74,10 +82,62 @@ $(function() {
 								$opt_input.val( action.options[option.id] );
 							}
 
+							$options.append($opt_input);
+
+						}
+
+
+
+
+
+						if (option.type == 'dropdown') {
+
+							var $opt_input = $("<select class='action-option-change form-control'></select>");
+							$opt_input.data('action-id', action.id);
+							$opt_input.data('option-id', option.id);
+
+							for (var x in option.choices) {
+								var str = new String(option.choices[x]);
+								var $opt_choice = $("<option value='"+x+"'>"+str.toString()+"</option>");
+								$opt_choice.data('id', x);
+								$opt_input.append($opt_choice);
+							}
+
+
+							// if options never been stored on this action
+							if (action.options === undefined) {
+								action.options = {};
+							}
+
+							// if this option never has been saved, set default
+							if (action.options[option.id] === undefined) {
+								socket.emit('bank_update_action_option', page, bank, action.id, option.id, option.default);
+								$opt_input.val(option.default);
+							}
+
+							// else set the db value for this option.
+							else {
+								$opt_input.val( action.options[option.id] );
+							}
 
 							$options.append($opt_input);
 
 						}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 					}
