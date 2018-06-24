f7a2fc758c05e6193b56cde1aa014b053e667ed0
William Viker <william@trippelm.no>
2018-06-24 09:28:46 +0200

    updated yarn script to say which module that is being yarned:)

--- tools/yarn.sh
+++ tools/yarn.sh
@@ -4,7 +4,9 @@ yarn
 yarn --cwd bitfocus-skeleton/
 
 for module in lib/module/*/; do
+	echo ${module}
 	grep '"dependencies"' ${module}package.json > /dev/null 2>&1 && yarn --cwd ${module}
+	echo
 done
 
 exit 0
