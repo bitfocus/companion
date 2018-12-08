958258bb307e6c21dca67410cd398d566f7185e9
William Viker <william@trippelm.no>
2018-12-08 17:07:31 +0100

    Major: Part 1 of enabling '15 button action'

--- public/bank.js
+++ public/bank.js
@@ -62,7 +62,7 @@ $(function() {
 
 	function bank_preview_page(_page) {
 		var cachedata = {};
-		for (var _bank = 1; _bank <= 12; ++_bank) {
+		for (var _bank = 1; _bank <= 15; ++_bank) {
 			if (image_cache[_page + '_' + _bank] !== undefined) {
 				cachedata[_bank] = image_cache[_page + '_' + _bank].updated;
 			}
@@ -347,7 +347,7 @@ $(function() {
 	});
 
 	socket.on('preview_page_data', function (images) {
-		for (var key = 1; key <= 12; ++key) {
+		for (var key = 1; key <= 15; ++key) {
 			var imageData;
 
 			if (images[key] === undefined) {
@@ -539,7 +539,7 @@ $(function() {
 		$pagenav.append($('<div class="pageat col-lg-4"><small>(Page '+page+')</small> <input id="page_title" placeholder="Page name" type="text" value="'+ pname +'"></div>'));
 		$pagenav.append($('<div class="pagenav text-right col-lg-4"><div id="btn_pageup" class="btn btn-primary"><i class="fa fa-chevron-right"></i></div></div>'));
 
-		for (var bank = 1; bank <= 12; bank++) {
+		for (var bank = 1; bank <= 15; bank++) {
 			var $div = $('<div class="bank col-lg-3"><div class="border" data-bank="' + bank + '" data-page="' + page + '"><canvas width=72 height=72 id="bank_' + page + '_' + bank + '"></canvas></div></div>');
 			$div.find('.border').droppable({
 				activeClass: 'drophere',
