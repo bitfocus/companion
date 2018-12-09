cde9a108321d88a20cfa06c61df8200107beecbc
William Viker <william@trippelm.no>
2018-12-09 18:21:02 +0100

    Minor: Show build number in the WebUI. Closes #335

--- public/main.js
+++ public/main.js
@@ -159,7 +159,7 @@ $(function() {
 
 	socket.on('skeleton-info', function(hash) {
 		console.log("skeleton-info", hash);
-		$("#versiontext").text(hash.appVersion+hash.appBuild);
+		$("#versiontext").text(hash.appVersion+ " (" + hash.appBuild.replace("master-","") + ")");
 	});
 
 
