a943e07a54b4e9f1d78fa9a0bf07c7ad71333fd0
Julian Waller <git@julusian.co.uk>
2021-01-05 20:41:37 +0000

    wip: basic page layout

--- webui/src/App.js
+++ webui/src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import {CNavbar, CNavbarBrand, CHeader, CSidebar, CSidebarNav, CSidebarNavItem, CSidebarBrand, CContainer,CRow, CCol} from '@coreui/react'
+import {faBug, faComments, faDollarSign, faGamepad, faInfo, faMousePointer, faTabletAlt, faUsers} from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="c-app">
+      <CSidebar>
+        <CSidebarNav>
+          <CSidebarBrand></CSidebarBrand>
+
+          <CSidebarNavItem target="_new" href="/emulator.html" icon={<FontAwesomeIcon icon={faGamepad} />} name="Emulator" />
+          <CSidebarNavItem target="_new" href="/tablet.html" icon={<FontAwesomeIcon icon={faMousePointer} />} name="Web buttons" />
+          <CSidebarNavItem target="_new" href="/tablet2.html" icon={<FontAwesomeIcon icon={faTabletAlt} />} name="Mobile buttons" />
+
+          <CSidebarNavItem target="_new" href="https://github.com/bitfocus/companion/issues" icon={<FontAwesomeIcon icon={faBug} />} name="Bugs &amp; Features" />
+          <CSidebarNavItem target="_new" href="https://www.facebook.com/groups/companion/" icon={<FontAwesomeIcon icon={faUsers} />} name="Facebook" />
+          <CSidebarNavItem target="_new" href="https://join.slack.com/t/bitfocusio/shared_invite/enQtODk4NTYzNTkzMjU1LTMzZDY1Njc2MmE3MzVlNmJhMTBkMzFjNTQ2NzZlYzQyZWIzZTJkZWIyNmJlY2U0NzM1NGEzNzNlZWY3OWJlNGE" icon={<FontAwesomeIcon icon={faComments} />} name="Slack Chat" />
+          <CSidebarNavItem target="_new" href="https://donorbox.org/bitfocus-opensource" icon={<FontAwesomeIcon icon={faDollarSign} />} name="Donate" />
+
+          <CSidebarNavItem target="_new" href="/help.html" icon={<FontAwesomeIcon icon={faInfo} />} name="Getting Started" />
+        </CSidebarNav>
+      </CSidebar>
+      <div className="c-wrapper">
+        <CHeader>
+          <CNavbar fixed={true} light={false} color='danger'>
+
+            <CNavbarBrand>
+              <span style={{fontWeight: 'bold'}}>Bitfocus</span> Companion
+            </CNavbarBrand>
+          </CNavbar>
+        </CHeader>
+        <div className="c-body">
+          <CContainer fluid className="animated fadeIn">
+            <CRow>
+              <CCol xs={12} xl={6}>
+
+                
+
+              <p>Hi</p>
+              </CCol>
+
+            </CRow>
+
+          </CContainer>
+        </div>
+      </div>
     </div>
   );
 }
--- webui/src/index.js
+++ webui/src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './App.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+// import "@coreui/coreui/scss/coreui.scss"
 
 ReactDOM.render(
   <React.StrictMode>
