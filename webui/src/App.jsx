5f9d5620c9058972945f306af409e5a39e71806d
Julian Waller <git@julusian.co.uk>
2021-01-15 19:13:57 +0000

    wip: prototype translations hooks

--- webui/src/App.jsx
+++ webui/src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Suspense } from 'react'
 import {CHeader, CSidebar, CSidebarNav, CSidebarNavItem, CSidebarBrand, CContainer, CRow, CCol, CTabs, CTabContent, CTabPane, CNav, CNavItem, CNavLink, CNavbarNav, CCollapse, CHeaderBrand, CHeaderNavItem, CHeaderNav, CHeaderNavLink} from '@coreui/react'
 import {faBug, faCalculator, faCalendarAlt, faClipboardList, faClock, faCog, faComments, faDollarSign, faFileImport, faGamepad, faGift, faInfo, faMousePointer, faPlug, faTabletAlt, faUserNinja, faUsers} from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
@@ -15,6 +15,7 @@ import { Surfaces } from './Surfaces'
 import { UserConfig } from './UserConfig'
 import { LogPanel } from './LogPanel'
 import { InstancePresets } from './Presets'
+import { useTranslation } from 'react-i18next'
 
 export default class App extends React.Component {
   constructor(props) {
@@ -196,126 +197,140 @@ export default class App extends React.Component {
 
     return (
       <CompanionContext.Provider value={{ socket: this.socket, instances: this.state.instances, modules: this.state.modules  }} >
-        <div className="c-app">
-          <HelpModal content={this.state.helpContent} hide={() => this.setState({ helpContent: null })} />
+        <Suspense fallback={<Spinner />}>
+          <div className="c-app">
+            <HelpModal content={this.state.helpContent} hide={() => this.setState({ helpContent: null })} />
 
-          <CSidebar>
-            <CSidebarNav>
-              <CSidebarBrand></CSidebarBrand>
+          <Sidebar />
+            <div className="c-wrapper">
+              <CHeader colorScheme="dark">
+                {/* <CNavbar fixed="top" light={false} color='danger'> */}
 
-              <CSidebarNavItem target="_new" href="/emulator.html" icon={<FontAwesomeIcon icon={faGamepad} />} name="Emulator" />
-              <CSidebarNavItem target="_new" href="/tablet.html" icon={<FontAwesomeIcon icon={faMousePointer} />} name="Web buttons" />
-              <CSidebarNavItem target="_new" href="/tablet2.html" icon={<FontAwesomeIcon icon={faTabletAlt} />} name="Mobile buttons" />
+                  <CHeaderBrand>
+                    <span style={{fontWeight: 'bold'}}>Bitfocus</span> Companion
+                  </CHeaderBrand>
 
-              <CSidebarNavItem target="_new" href="https://github.com/bitfocus/companion/issues" icon={<FontAwesomeIcon icon={faBug} />} name="Bugs &amp; Features" />
-              <CSidebarNavItem target="_new" href="https://www.facebook.com/groups/companion/" icon={<FontAwesomeIcon icon={faUsers} />} name="Facebook" />
-              <CSidebarNavItem target="_new" href="https://join.slack.com/t/bitfocusio/shared_invite/enQtODk4NTYzNTkzMjU1LTMzZDY1Njc2MmE3MzVlNmJhMTBkMzFjNTQ2NzZlYzQyZWIzZTJkZWIyNmJlY2U0NzM1NGEzNzNlZWY3OWJlNGE" icon={<FontAwesomeIcon icon={faComments} />} name="Slack Chat" />
-              <CSidebarNavItem target="_new" href="https://donorbox.org/bitfocus-opensource" icon={<FontAwesomeIcon icon={faDollarSign} />} name="Donate" />
+                  <CHeaderNav>
+                    <CHeaderNavItem>
+                      <CHeaderNavLink target="_new" title="Version Number" href="https://bitfocus.io/companion/">
+                          { this.getVersionString() }
+                      </CHeaderNavLink>
+                    </CHeaderNavItem>
 
-              <CSidebarNavItem target="_new" href="/help.html" icon={<FontAwesomeIcon icon={faInfo} />} name="Getting Started" />
-            </CSidebarNav>
-          </CSidebar>
-          <div className="c-wrapper">
-            <CHeader colorScheme="dark">
-              {/* <CNavbar fixed="top" light={false} color='danger'> */}
+                    <CHeaderNavItem>
+                      <CHeaderNavLink target="_new" href={this.state.updateData?.link || "https://bitfocus.io/companion/"}>
+                        { this.state.updateData?.message || '' }
+                      </CHeaderNavLink>
+                    </CHeaderNavItem>
+                  </CHeaderNav>
+                {/* </CNavbar> */}
+              </CHeader>
+              <div className="c-body">
+                <CContainer fluid className="animated fadeIn">
+                  {
+                    this.socket && this.state.connected ? 
+                    <CRow>
 
-                <CHeaderBrand>
-                  <span style={{fontWeight: 'bold'}}>Bitfocus</span> Companion
-                </CHeaderBrand>
+                      <CCol xs={12} xl={6}>
+                        <CTabs activeTab={this.state.activeTab1} onActiveTabChange={(a) => this.setState({ activeTab1: a})}>
+                          <CNav variant="tabs">
+                            <CNavItem><CNavLink data-tab="instances"><FontAwesomeIcon icon={faPlug} /> Instances</CNavLink></CNavItem>
+                            <CNavItem hidden={!showInstanceConfig}><CNavLink data-tab="instanceConfig"><FontAwesomeIcon icon={faCog} /> Config</CNavLink></CNavItem>
+                            <CNavItem><CNavLink data-tab="buttons"><FontAwesomeIcon icon={faCalendarAlt} /> Buttons</CNavLink></CNavItem>
+                            <CNavItem><CNavLink data-tab="surfaces"><FontAwesomeIcon icon={faGamepad} /> Surfaces</CNavLink></CNavItem>
+                            <CNavItem><CNavLink data-tab="triggers"><FontAwesomeIcon icon={faClock} /> Triggers</CNavLink></CNavItem>
+                            <CNavItem><CNavLink data-tab="userconfig"><FontAwesomeIcon icon={faUserNinja} /> Settings</CNavLink></CNavItem>
+                          </CNav>
+                          <CTabContent fade={false}>
+                            <CTabPane data-tab="instances">
+                              <ErrorBoundary>
+                                <Instances configureInstance={this.configureInstance} showHelp={this.showHelp} />
+                              </ErrorBoundary>
+                            </CTabPane>
+                            <CTabPane data-tab="instanceConfig">
+                              <ErrorBoundary>
+                                {
+                                  this.state.configureInstanceId
+                                  ? <InstanceConfig
+                                      key={this.state.configureInstanceToken}
+                                      instanceId={this.state.configureInstanceId}
+                                      showHelp={this.showHelp}
+                                      variableDefinitions={this.state.variableDefinitions}
+                                      variableValues={this.state.variableValues}
+                                    />
+                                  : 'No instance specified'
+                                }
+                              </ErrorBoundary>
+                            </CTabPane>
+                            <CTabPane data-tab="buttons">
+                              <Buttons buttonGridClick={this.buttonGridClick} isHot={this.state.hotPress} />
+                            </CTabPane>
+                            <CTabPane data-tab="surfaces">
+                              <Surfaces />
+                            </CTabPane>
+                            <CTabPane data-tab="triggers">t</CTabPane>
+                            <CTabPane data-tab="userconfig">
+                              <UserConfig />
+                            </CTabPane>
+                          </CTabContent>
+                        </CTabs>
+                      </CCol>
 
-                <CHeaderNav>
-                  <CHeaderNavItem>
-                    <CHeaderNavLink target="_new" title="Version Number" href="https://bitfocus.io/companion/">
-                        { this.getVersionString() }
-                    </CHeaderNavLink>
-                  </CHeaderNavItem>
+                      <CCol xs={12} xl={6}>
+                        <CTabs>
+                          <CNav variant="tabs">
+                            <CNavItem><CNavLink data-tab="log"><FontAwesomeIcon icon={faClipboardList} /> Log</CNavLink></CNavItem>
+                            <CNavItem><CNavLink data-tab="edit"><FontAwesomeIcon icon={faCalculator} /> Edit Button</CNavLink></CNavItem>
+                            <CNavItem><CNavLink data-tab="presets" onClick={(a) => this.setState({ activePresetToken: shortid() })}><FontAwesomeIcon icon={faGift} /> Presets</CNavLink></CNavItem>
+                            <CNavItem><CNavLink data-tab="importexport"><FontAwesomeIcon icon={faFileImport} /> Import / Export</CNavLink></CNavItem>
+                          </CNav>
+                          <CTabContent fade={false}>
+                            <CTabPane data-tab="log">
+                              <LogPanel />
+                            </CTabPane>
+                            <CTabPane data-tab="presets">
+                              <InstancePresets token={this.state.activePresetToken} />
+                            </CTabPane>
+                          </CTabContent>
+                        </CTabs>
+                      </CCol>
 
-                  <CHeaderNavItem>
-                    <CHeaderNavLink target="_new" href={this.state.updateData?.link || "https://bitfocus.io/companion/"}>
-                      { this.state.updateData?.message || '' }
-                    </CHeaderNavLink>
-                  </CHeaderNavItem>
-                </CHeaderNav>
-              {/* </CNavbar> */}
-            </CHeader>
-            <div className="c-body">
-              <CContainer fluid className="animated fadeIn">
-                {
-                  this.socket && this.state.connected ? 
-                  <CRow>
-
-                    <CCol xs={12} xl={6}>
-                      <CTabs activeTab={this.state.activeTab1} onActiveTabChange={(a) => this.setState({ activeTab1: a})}>
-                        <CNav variant="tabs">
-                          <CNavItem><CNavLink data-tab="instances"><FontAwesomeIcon icon={faPlug} /> Instances</CNavLink></CNavItem>
-                          <CNavItem hidden={!showInstanceConfig}><CNavLink data-tab="instanceConfig"><FontAwesomeIcon icon={faCog} /> Config</CNavLink></CNavItem>
-                          <CNavItem><CNavLink data-tab="buttons"><FontAwesomeIcon icon={faCalendarAlt} /> Buttons</CNavLink></CNavItem>
-                          <CNavItem><CNavLink data-tab="surfaces"><FontAwesomeIcon icon={faGamepad} /> Surfaces</CNavLink></CNavItem>
-                          <CNavItem><CNavLink data-tab="triggers"><FontAwesomeIcon icon={faClock} /> Triggers</CNavLink></CNavItem>
-                          <CNavItem><CNavLink data-tab="userconfig"><FontAwesomeIcon icon={faUserNinja} /> Settings</CNavLink></CNavItem>
-                        </CNav>
-                        <CTabContent fade={false}>
-                          <CTabPane data-tab="instances">
-                            <ErrorBoundary>
-                              <Instances configureInstance={this.configureInstance} showHelp={this.showHelp} />
-                            </ErrorBoundary>
-                          </CTabPane>
-                          <CTabPane data-tab="instanceConfig">
-                            <ErrorBoundary>
-                              {
-                                this.state.configureInstanceId
-                                ? <InstanceConfig
-                                    key={this.state.configureInstanceToken}
-                                    instanceId={this.state.configureInstanceId}
-                                    showHelp={this.showHelp}
-                                    variableDefinitions={this.state.variableDefinitions}
-                                    variableValues={this.state.variableValues}
-                                  />
-                                : 'No instance specified'
-                              }
-                            </ErrorBoundary>
-                          </CTabPane>
-                          <CTabPane data-tab="buttons">
-                            <Buttons buttonGridClick={this.buttonGridClick} isHot={this.state.hotPress} />
-                          </CTabPane>
-                          <CTabPane data-tab="surfaces">
-                            <Surfaces />
-                          </CTabPane>
-                          <CTabPane data-tab="triggers">t</CTabPane>
-                          <CTabPane data-tab="userconfig">
-                            <UserConfig />
-                          </CTabPane>
-                        </CTabContent>
-                      </CTabs>
-                    </CCol>
-
-                    <CCol xs={12} xl={6}>
-                      <CTabs>
-                        <CNav variant="tabs">
-                          <CNavItem><CNavLink data-tab="log"><FontAwesomeIcon icon={faClipboardList} /> Log</CNavLink></CNavItem>
-                          <CNavItem><CNavLink data-tab="edit"><FontAwesomeIcon icon={faCalculator} /> Edit Button</CNavLink></CNavItem>
-                          <CNavItem><CNavLink data-tab="presets" onClick={(a) => this.setState({ activePresetToken: shortid() })}><FontAwesomeIcon icon={faGift} /> Presets</CNavLink></CNavItem>
-                          <CNavItem><CNavLink data-tab="importexport"><FontAwesomeIcon icon={faFileImport} /> Import / Export</CNavLink></CNavItem>
-                        </CNav>
-                        <CTabContent fade={false}>
-                          <CTabPane data-tab="log">
-                            <LogPanel />
-                          </CTabPane>
-                          <CTabPane data-tab="presets">
-                            <InstancePresets token={this.state.activePresetToken} />
-                          </CTabPane>
-                        </CTabContent>
-                      </CTabs>
-                    </CCol>
-
-                  </CRow>
-                  : 'Connecting' 
-                }
-              </CContainer>
+                    </CRow>
+                    : 'Connecting' 
+                  }
+                </CContainer>
+              </div>
             </div>
           </div>
-        </div>
+        </Suspense>
       </CompanionContext.Provider>
     );
   }
+}
+
+function Spinner() {
+  return <p>Loading</p>
+}
+
+function Sidebar() {
+  const { t } = useTranslation();
+
+  return (
+    <CSidebar>
+      <CSidebarNav>
+        <CSidebarBrand></CSidebarBrand>
+
+        <CSidebarNavItem target="_new" href="/emulator.html" icon={<FontAwesomeIcon icon={faGamepad} />} name={t("Emulator")} />
+        <CSidebarNavItem target="_new" href="/tablet.html" icon={<FontAwesomeIcon icon={faMousePointer} />} name={t("Web buttons")} />
+        <CSidebarNavItem target="_new" href="/tablet2.html" icon={<FontAwesomeIcon icon={faTabletAlt} />} name={t("Mobile buttons")} />
+
+        <CSidebarNavItem target="_new" href="https://github.com/bitfocus/companion/issues" icon={<FontAwesomeIcon icon={faBug} />} name={t("Bugs & Features")} />
+        <CSidebarNavItem target="_new" href="https://www.facebook.com/groups/companion/" icon={<FontAwesomeIcon icon={faUsers} />} name={t("Facebook")} />
+        <CSidebarNavItem target="_new" href="https://join.slack.com/t/bitfocusio/shared_invite/enQtODk4NTYzNTkzMjU1LTMzZDY1Njc2MmE3MzVlNmJhMTBkMzFjNTQ2NzZlYzQyZWIzZTJkZWIyNmJlY2U0NzM1NGEzNzNlZWY3OWJlNGE" icon={<FontAwesomeIcon icon={faComments} />} name={t("Slack Chat")} />
+        <CSidebarNavItem target="_new" href="https://donorbox.org/bitfocus-opensource" icon={<FontAwesomeIcon icon={faDollarSign} />} name={t("Donate")} />
+
+        <CSidebarNavItem target="_new" href="/help.html" icon={<FontAwesomeIcon icon={faInfo} />} name={t("Getting Started")} />
+      </CSidebarNav>
+    </CSidebar>
+  )
 }
--- webui/src/index.js
+++ webui/src/index.js
@@ -4,7 +4,28 @@ import './App.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import i18n from 'i18next';
+import Backend from 'i18next-http-backend';
+import LanguageDetector from 'i18next-browser-languagedetector';
+import { initReactI18next } from 'react-i18next';
+
 // import "@coreui/coreui/scss/coreui.scss"
+
+i18n
+.use(Backend)
+.use(LanguageDetector)
+.use(initReactI18next) // passes i18n down to react-i18next
+  .init({
+    lng: "en",
+    fallbackLng: "en",
+
+    debug: true, // TODO disable
+
+    interpolation: {
+      escapeValue: false
+    }
+  });
+
 
 ReactDOM.render(
   <React.StrictMode>
