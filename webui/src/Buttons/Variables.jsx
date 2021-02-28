9ba51bad6a438dc19fa7ec00be99f7a07acab708
William Viker <william@trippelm.no>
2021-02-28 03:33:05 +0100

    chore: webui: indenting/formatting scss and instances-stuff.

--- webui/src/Buttons/Variables.jsx
+++ webui/src/Buttons/Variables.jsx
@@ -1,74 +1,111 @@
-import React, { useCallback, useContext, useEffect, useMemo, useState } from 'react'
-import { CAlert, CButton } from '@coreui/react'
-import { CompanionContext } from '../util'
-import { VariablesTable } from '../Components/VariablesTable'
+import React, {
+  useCallback,
+  useContext,
+  useEffect,
+  useMemo,
+  useState,
+} from "react";
+import { CAlert, CButton } from "@coreui/react";
+import { CompanionContext } from "../util";
+import { VariablesTable } from "../Components/VariablesTable";
 
 export const InstanceVariables = function InstanceVariables({ resetToken }) {
-	const context = useContext(CompanionContext)
+  const context = useContext(CompanionContext);
 
-	const [instanceId, setInstance] = useState(null)
+  const [instanceId, setInstance] = useState(null);
 
-    const instancesLabelMap = useMemo(() => {
-        const labelMap = new Map()
-        for (const [id, instance] of Object.entries(context.instances)) {
-            labelMap.set(instance.label, id)
-        }
-        return labelMap
-    }, [context.instances])
+  const instancesLabelMap = useMemo(() => {
+    const labelMap = new Map();
+    for (const [id, instance] of Object.entries(context.instances)) {
+      labelMap.set(instance.label, id);
+    }
+    return labelMap;
+  }, [context.instances]);
 
-	// Reset selection on resetToken change
-	useEffect(() => {
-		setInstance(null)
-	}, [resetToken])
+  // Reset selection on resetToken change
+  useEffect(() => {
+    setInstance(null);
+  }, [resetToken]);
 
-	if (instanceId) {
-		const instance = context.instances[instanceId]
-		
-        return <VariablesList selectedInstanceLabel={instance?.label} setInstance={setInstance} />
-	} else {
-		return <VariablesInstanceList setInstance={setInstance} instancesLabelMap={instancesLabelMap} />
-	}
-}
+  if (instanceId) {
+    const instance = context.instances[instanceId];
+
+    return (
+      <VariablesList
+        selectedInstanceLabel={instance?.label}
+        setInstance={setInstance}
+      />
+    );
+  } else {
+    return (
+      <VariablesInstanceList
+        setInstance={setInstance}
+        instancesLabelMap={instancesLabelMap}
+      />
+    );
+  }
+};
 
 function VariablesInstanceList({ setInstance, instancesLabelMap }) {
-	const context = useContext(CompanionContext)
+  const context = useContext(CompanionContext);
 
-	const options = Object.entries(context.variableDefinitions || []).map(([label, defs]) => {
-        if (!defs || defs.length === 0) return ''
-        
-		const id = instancesLabelMap.get(label)
-        const instance = id ? context.instances[id] : undefined
-		const module = instance ? context.modules[instance.instance_type] : undefined
+  const options = Object.entries(context.variableDefinitions || []).map(
+    ([label, defs]) => {
+      if (!defs || defs.length === 0) return "";
 
-		return <div key={id}>
-			<CButton color='primary' className="choose_instance mb-3 mr-2" onClick={() => setInstance(id)}>
-				{module?.label ?? module?.name ?? '?'} ({label ?? id})
-			</CButton>
-		</div>
-	})
+      const id = instancesLabelMap.get(label);
+      const instance = id ? context.instances[id] : undefined;
+      const module = instance
+        ? context.modules[instance.instance_type]
+        : undefined;
 
-	return <div>
-		<h5>Variables</h5>
-		<p>Some connection types provide variables for you to use in button text.</p>
-		{
-			options.length === 0
-				? <CAlert color='primary'>You have no connections that support variables at the moment. More modules will support variables in the future.</CAlert>
-				: options
-		}
-	</div>
+      return (
+        <div key={id}>
+          <CButton
+            color="primary"
+            className="choose_instance mb-3 mr-2"
+            onClick={() => setInstance(id)}
+          >
+            {module?.label ?? module?.name ?? "?"} ({label ?? id})
+          </CButton>
+        </div>
+      );
+    }
+  );
+
+  return (
+    <div>
+      <h5>Variables</h5>
+      <p>
+        Some connection types provide variables for you to use in button text.
+      </p>
+      {options.length === 0 ? (
+        <CAlert color="primary">
+          You have no connections that support variables at the moment. More
+          modules will support variables in the future.
+        </CAlert>
+      ) : (
+        options
+      )}
+    </div>
+  );
 }
 
 function VariablesList({ selectedInstanceLabel, setInstance }) {
-	const doBack = useCallback(() => setInstance(null), [setInstance])
+  const doBack = useCallback(() => setInstance(null), [setInstance]);
 
-	return <div>
-		<h4>
-			<CButton color='primary' size="sm" onClick={doBack}>Back</CButton>
-			Variables for {selectedInstanceLabel}
-		</h4>
-		
-        <VariablesTable label={selectedInstanceLabel} />
+  return (
+    <div>
+      <h5>
+        <CButton color="primary" size="sm" onClick={doBack}>
+          Back
+        </CButton>
+        Variables for {selectedInstanceLabel}
+      </h5>
 
-		<br style={{ clear: 'both' }} />
-	</div>
+      <VariablesTable label={selectedInstanceLabel} />
+
+      <br style={{ clear: "both" }} />
+    </div>
+  );
 }
--- webui/src/Instances/index.jsx
+++ webui/src/Instances/index.jsx
@@ -1,12 +1,12 @@
 import {
-	CCol,
-	CRow,
-	CTabs,
-	CTabContent,
-	CTabPane,
-	CNavItem,
-	CNavLink,
-	CNav,
+  CCol,
+  CRow,
+  CTabs,
+  CTabContent,
+  CTabPane,
+  CNavItem,
+  CNavLink,
+  CNav,
 } from "@coreui/react";
 import { memo, useCallback, useContext, useRef, useState } from "react";
 import { HelpModal } from "./HelpModal";
@@ -19,105 +19,105 @@ import shortid from "shortid";
 import { faCog, faPlus } from "@fortawesome/free-solid-svg-icons";
 
 export const InstancesPage = memo(function InstancesPage() {
-	const context = useContext(CompanionContext);
+  const context = useContext(CompanionContext);
 
-	const helpModalRef = useRef();
+  const helpModalRef = useRef();
 
-	const [tabResetToken, setTabResetToken] = useState(shortid());
-	const [activeTab, setActiveTab] = useState("add");
-	const [selectedInstanceId, setSelectedInstanceId] = useState(null);
-	const doChangeTab = useCallback((newTab) => {
-		setActiveTab((oldTab) => {
-			if (oldTab !== newTab) {
-				setSelectedInstanceId(null);
-				setTabResetToken(shortid());
-			}
-			return newTab;
-		});
-	}, []);
+  const [tabResetToken, setTabResetToken] = useState(shortid());
+  const [activeTab, setActiveTab] = useState("add");
+  const [selectedInstanceId, setSelectedInstanceId] = useState(null);
+  const doChangeTab = useCallback((newTab) => {
+    setActiveTab((oldTab) => {
+      if (oldTab !== newTab) {
+        setSelectedInstanceId(null);
+        setTabResetToken(shortid());
+      }
+      return newTab;
+    });
+  }, []);
 
-	const showHelp = useCallback(
-		(name) => {
-			socketEmit(context.socket, "instance_get_help_md", [name]).then(
-				([err, result]) => {
-					if (err) {
-						context.notifier.current.show(
-							"Instance help",
-							`Failed to get help text: ${err}`
-						);
-						return;
-					}
-					if (result) {
-						helpModalRef.current?.show(name, result);
-					}
-				}
-			);
-		},
-		[context.socket, context.notifier]
-	);
+  const showHelp = useCallback(
+    (name) => {
+      socketEmit(context.socket, "instance_get_help_md", [name]).then(
+        ([err, result]) => {
+          if (err) {
+            context.notifier.current.show(
+              "Instance help",
+              `Failed to get help text: ${err}`
+            );
+            return;
+          }
+          if (result) {
+            helpModalRef.current?.show(name, result);
+          }
+        }
+      );
+    },
+    [context.socket, context.notifier]
+  );
 
-	const doConfigureInstance = useCallback((id) => {
-		setSelectedInstanceId(id);
-		setTabResetToken(shortid());
-		setActiveTab(id ? "edit" : "add");
-	}, []);
+  const doConfigureInstance = useCallback((id) => {
+    setSelectedInstanceId(id);
+    setTabResetToken(shortid());
+    setActiveTab(id ? "edit" : "add");
+  }, []);
 
-	return (
-		<CRow className="instances-page">
-			<HelpModal ref={helpModalRef} />
+  return (
+    <CRow className="instances-page">
+      <HelpModal ref={helpModalRef} />
 
-			<CCol xl={6} className="instances-panel">
-				<InstancesList
-					showHelp={showHelp}
-					doConfigureInstance={doConfigureInstance}
-				/>
-			</CCol>
+      <CCol xl={6} className="instances-panel">
+        <InstancesList
+          showHelp={showHelp}
+          doConfigureInstance={doConfigureInstance}
+        />
+      </CCol>
 
-			<CCol
-				xl={6}
-				className="instances-panel secondary-panel add-instances-panel"
-			>
-				<div className="secondary-panel-inner">
-					<CTabs activeTab={activeTab} onActiveTabChange={doChangeTab}>
-						<CNav variant="tabs">
-							<CNavItem>
-								<CNavLink data-tab="add">
-									<FontAwesomeIcon icon={faPlus} /> Add connection
-								</CNavLink>
-							</CNavItem>
-							<CNavItem hidden={!selectedInstanceId}>
-								<CNavLink data-tab="edit">
-									<FontAwesomeIcon icon={faCog} /> Edit Instance
-								</CNavLink>
-							</CNavItem>
-						</CNav>
-						<CTabContent fade={false}>
-							<CTabPane data-tab="add">
-								<MyErrorBoundary>
-									<AddInstancesPanel
-										showHelp={showHelp}
-										doConfigureInstance={doConfigureInstance}
-									/>
-								</MyErrorBoundary>
-							</CTabPane>
-							<CTabPane data-tab="edit">
-								<MyErrorBoundary>
-									{selectedInstanceId ? (
-										<InstanceEditPanel
-											key={tabResetToken}
-											showHelp={showHelp}
-											doConfigureInstance={doConfigureInstance}
-											instanceId={selectedInstanceId}
-										/>
-									) : (
-										""
-									)}
-								</MyErrorBoundary>
-							</CTabPane>
-						</CTabContent>
-					</CTabs>
-				</div>
-			</CCol>
-		</CRow>
-	);
+      <CCol
+        xl={6}
+        className="instances-panel secondary-panel add-instances-panel"
+      >
+        <div className="secondary-panel-inner">
+          <CTabs activeTab={activeTab} onActiveTabChange={doChangeTab}>
+            <CNav variant="tabs">
+              <CNavItem>
+                <CNavLink data-tab="add">
+                  <FontAwesomeIcon icon={faPlus} /> Add connection
+                </CNavLink>
+              </CNavItem>
+              <CNavItem hidden={!selectedInstanceId}>
+                <CNavLink data-tab="edit">
+                  <FontAwesomeIcon icon={faCog} /> Edit Instance
+                </CNavLink>
+              </CNavItem>
+            </CNav>
+            <CTabContent fade={false}>
+              <CTabPane data-tab="add">
+                <MyErrorBoundary>
+                  <AddInstancesPanel
+                    showHelp={showHelp}
+                    doConfigureInstance={doConfigureInstance}
+                  />
+                </MyErrorBoundary>
+              </CTabPane>
+              <CTabPane data-tab="edit">
+                <MyErrorBoundary>
+                  {selectedInstanceId ? (
+                    <InstanceEditPanel
+                      key={tabResetToken}
+                      showHelp={showHelp}
+                      doConfigureInstance={doConfigureInstance}
+                      instanceId={selectedInstanceId}
+                    />
+                  ) : (
+                    ""
+                  )}
+                </MyErrorBoundary>
+              </CTabPane>
+            </CTabContent>
+          </CTabs>
+        </div>
+      </CCol>
+    </CRow>
+  );
 });
