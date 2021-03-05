17bd7e954f7917890d7e7b491159124ff7afb925
Julian Waller <git@julusian.co.uk>
2021-03-05 23:53:00 +0000

    feat: web buttons better caching/preloading

--- webui/src/Tablet.jsx
+++ webui/src/Tablet.jsx
@@ -10,8 +10,75 @@ import { parse } from 'query-string'
 import rangeParser from 'parse-numeric-range'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faArrowLeft, faArrowRight } from '@fortawesome/free-solid-svg-icons'
+import EventEmitter from 'eventemitter3'
 
 const socket = new io(SERVER_URL)
+
+/** Cache of images across grid components */
+class ImageCache extends EventEmitter {
+	constructor(socket) {
+		super()
+
+		this.socket = socket
+
+		this.loadedPages = new Set()
+		this.imageCache = new Map()
+
+		this.socket.on('connect', () => {
+			// resubscribe on reconnect
+			for (const k of this.loadedPages.values()) {
+				this._doSubscribe(k)
+			}
+		})
+
+		this.socket.on('buttons_bank_data', this._updateImages)
+	}
+
+	loadPage(page) {
+		if (!this.loadedPages.has(page)) {
+			this.loadedPages.add(page)
+			this._doSubscribe(page)
+		}
+	}
+
+	getPage(page) {
+		return this.imageCache.get(page) || {}
+	}
+
+	_doSubscribe(page) {
+		const lastUpdated = {} // We won't ever have an existing data
+		socketEmit(this.socket, 'web_buttons_page', [page, lastUpdated])
+			.then(([n, data]) => {
+				this._updateImages(n, data)
+			})
+			.catch((e) => {
+				// TODO - report to user
+				console.error(`Failed to load page buttons: ${e}`)
+			})
+	}
+
+	_updateImages = (page, data) => {
+		if (this.loadedPages.has(page)) {
+			const newImages = { ...this.imageCache.get(page) }
+
+			let changed = false
+			for (let key = 1; key <= MAX_BUTTONS; ++key) {
+				if (data[key] !== undefined) {
+					changed = true
+					newImages[key] = {
+						image: dataToButtonImage(data[key].buffer),
+						updated: data[key].updated,
+					}
+				}
+			}
+
+			this.imageCache.set(page, newImages)
+			if (changed) {
+				this.emit(`${page}`, newImages)
+			}
+		}
+	}
+}
 
 export function Tablet() {
 	const [pages, setPages] = useState(null)
@@ -28,6 +95,8 @@ export function Tablet() {
 			orderedPages: pagesRange,
 		}
 	}, [locationSearch])
+
+	const imageCache = useMemo(() => new ImageCache(socket), [])
 
 	const [retryToken, setRetryToken] = useState(shortid())
 	const doRetryLoad = useCallback(() => setRetryToken(shortid()), [])
@@ -78,10 +147,12 @@ export function Tablet() {
 
 	// Compile the list of pages we will be showing
 	let validPages = orderedPages.filter((p) => pages && !!pages[p])
-	if (validPages.length === 0)
-		validPages = Object.keys(pages || {})
-			.map((p) => Number(p))
-			.sort()
+	if (validPages.length === 0) validPages = Object.keys(pages || {}).map((p) => Number(p))
+
+	let cols = Number(parsedQuery['cols'])
+	if (isNaN(cols) || cols <= 0) cols = MAX_COLS
+	let rows = Number(parsedQuery['rows'])
+	if (isNaN(rows) || rows <= 0) rows = MAX_ROWS
 
 	const layout = parsedQuery['layout']
 	return (
@@ -93,9 +164,25 @@ export function Tablet() {
 
 				{pages ? (
 					layout === 'cycle' ? (
-						<CyclePages socket={socket} pages={pages} orderedPages={validPages} query={parsedQuery} />
+						<CyclePages
+							socket={socket}
+							pages={pages}
+							imageCache={imageCache}
+							orderedPages={validPages}
+							query={parsedQuery}
+							cols={cols}
+							rows={rows}
+						/>
 					) : (
-						<InfinitePages socket={socket} pages={pages} orderedPages={validPages} />
+						<InfinitePages
+							socket={socket}
+							pages={pages}
+							imageCache={imageCache}
+							orderedPages={validPages}
+							query={parsedQuery}
+							cols={cols}
+							rows={rows}
+						/>
 					)
 				) : (
 					''
@@ -109,7 +196,7 @@ function clamp(val, max) {
 	return Math.min(Math.max(0, val), max)
 }
 
-function CyclePages({ socket, pages, orderedPages, query }) {
+function CyclePages({ socket, pages, imageCache, orderedPages, query, cols, rows }) {
 	const rawIndex = Number(query['index'])
 	const loop = query['loop']
 	const defaultIndex = isNaN(rawIndex) ? 0 : clamp(rawIndex, orderedPages.length - 1)
@@ -120,14 +207,13 @@ function CyclePages({ socket, pages, orderedPages, query }) {
 	const safeIndex = clamp(currentIndex, orderedPages.length - 1)
 	const currentPage = orderedPages[safeIndex]
 
-	// // Preload some pages for more seamless cycling
-	// const preloadPages = new Set()
-	// {
-	// 	const prevPage = orderedPages[safeIndex - 1]
-	// 	if (prevPage !== undefined) preloadPages.add(prevPage)
-	// 	const nextPage = orderedPages[safeIndex + 1]
-	// 	if (nextPage !== undefined) preloadPages.add(nextPage)
-	// }
+	{
+		// Ensure next and prev pages are preloaded for more seamless cycling
+		const prevPage = orderedPages[safeIndex - 1]
+		if (prevPage !== undefined) imageCache.loadPage(prevPage)
+		const nextPage = orderedPages[safeIndex + 1]
+		if (nextPage !== undefined) imageCache.loadPage(nextPage)
+	}
 
 	const prevPage = useCallback(() => {
 		setCurrentIndex((oldIndex) => {
@@ -140,7 +226,7 @@ function CyclePages({ socket, pages, orderedPages, query }) {
 	}, [orderedPages])
 	const nextPage = useCallback(() => {
 		setCurrentIndex((oldIndex) => {
-			if (oldIndex >= orderedPages.length) {
+			if (oldIndex >= orderedPages.length - 1) {
 				return 0
 			} else {
 				return oldIndex + 1
@@ -172,7 +258,14 @@ function CyclePages({ socket, pages, orderedPages, query }) {
 						</h1>
 					</div>
 					<div>
-						<ButtonGrid key={currentPage} socket={socket} number={currentPage} />
+						<ButtonGrid
+							key={currentPage}
+							socket={socket}
+							imageCache={imageCache}
+							number={currentPage}
+							cols={cols}
+							rows={rows}
+						/>
 					</div>
 				</MyErrorBoundary>
 			</div>
@@ -180,14 +273,20 @@ function CyclePages({ socket, pages, orderedPages, query }) {
 	)
 }
 
-function InfinitePages({ socket, pages, orderedPages }) {
-	const pageElements = orderedPages.map((number) => (
-		<MyErrorBoundary key={number}>
+function InfinitePages({ socket, pages, imageCache, orderedPages, query, cols, rows }) {
+	const noHeadings = query['noheadings']
+
+	const pageElements = orderedPages.map((number, i) => (
+		<MyErrorBoundary key={i}>
+			{!noHeadings ? (
+				<CRow>
+					<h1 id={`page_${number}`}>{pages[number]?.name}</h1>
+				</CRow>
+			) : (
+				''
+			)}
 			<CRow>
-				<h1 id={`page_${number}`}>{pages[number]?.name}</h1>
-			</CRow>
-			<CRow>
-				<ButtonGrid socket={socket} number={number} />
+				<ButtonGrid socket={socket} imageCache={imageCache} number={number} cols={cols} rows={rows} />
 			</CRow>
 		</MyErrorBoundary>
 	))
@@ -195,79 +294,27 @@ function InfinitePages({ socket, pages, orderedPages }) {
 	return <>{pageElements}</>
 }
 
-function ButtonGrid({ socket, number }) {
+function ButtonGrid({ socket, imageCache, number, cols, rows }) {
 	const { ref, inView } = useInView({
 		rootMargin: '50%',
 		/* Optional options */
 		threshold: 0,
 	})
 
-	const [_viewToken, setViewToken] = useState(shortid())
-	const getViewToken = useCallback(() => {
-		// Hack to get the token without depending on it..
-		let v
-		setViewToken((oldV) => {
-			v = oldV
-			return oldV
-		})
-		return v
-	}, [])
+	// load existing images from the cache at mount
+	const [images, setImages] = useState(() => imageCache.getPage(number))
 
-	const [imageCache, setImageCache] = useState({})
-
+	// Ensure the page is loaded when it comes into view
 	useEffect(() => {
 		if (inView) {
-			const newToken = shortid()
-			setViewToken(newToken)
+			imageCache.on(`${number}`, setImages)
+			imageCache.loadPage(number)
 
-			const updateImages = (page, data) => {
-				if (page === number) {
-					setImageCache((oldImages) => {
-						const newImages = { ...oldImages }
-
-						for (let key = 1; key <= MAX_BUTTONS; ++key) {
-							if (data[key] !== undefined) {
-								newImages[key] = {
-									image: dataToButtonImage(data[key].buffer),
-									updated: data[key].updated,
-								}
-							}
-						}
-
-						return newImages
-					})
-				}
-			}
-
-			const lastUpdated = {}
-			setImageCache((imageCache) => {
-				// don't want to change imageCache, but this avoids the dependency
-				for (const [id, data] of Object.entries(imageCache)) {
-					lastUpdated[id] = { updated: data.updated }
-				}
-				return imageCache
-			})
-
-			// Load 'initial' data
-			socketEmit(socket, 'web_buttons_page', [number, lastUpdated])
-				.then(([n, data]) => {
-					// Only use if the token hasnt changed
-					if (getViewToken() === newToken) {
-						updateImages(n, data)
-					}
-				})
-				.catch((e) => {
-					// TODO - report to user
-					console.error(`Failed to load page buttons: ${e}`)
-				})
-
-			// Subscribe for changes
-			socket.on('buttons_bank_data', updateImages)
 			return () => {
-				socket.off('buttons_bank_data', updateImages)
+				imageCache.off(`${number}`, setImages)
 			}
 		}
-	}, [socket, number, inView, getViewToken])
+	}, [imageCache, number, inView])
 
 	const bankClick = useCallback(
 		(bank, pressed) => {
@@ -279,12 +326,12 @@ function ButtonGrid({ socket, number }) {
 	return (
 		<div ref={ref} className="bankgrid">
 			{' '}
-			{Array(MAX_ROWS)
+			{Array(Math.min(MAX_ROWS, rows))
 				.fill(0)
 				.map((_, y) => {
 					return (
 						<CCol key={y} sm={12} className="pagebank-row">
-							{Array(MAX_COLS)
+							{Array(Math.min(MAX_COLS, cols))
 								.fill(0)
 								.map((_, x) => {
 									const index = y * MAX_COLS + x + 1
@@ -293,7 +340,7 @@ function ButtonGrid({ socket, number }) {
 											key={x}
 											page={number}
 											index={index}
-											preview={imageCache[index]?.image}
+											preview={images[index]?.image}
 											onClick={bankClick}
 											alt={`Bank ${index}`}
 											selected={false}
