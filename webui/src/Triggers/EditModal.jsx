776be7ca6ca7f89de650c1b33832415a9ab03b85
Julian Waller <git@julusian.co.uk>
2022-04-28 00:22:10 +0100

    feat: add `internal:` ui control types for the internal module

--- webui/src/Triggers/EditModal.jsx
+++ webui/src/Triggers/EditModal.jsx
@@ -187,6 +187,7 @@ export function TriggerEditModal({ doClose, doSave, item, plugins }) {
 						</CCol>
 					</CRow>
 					<ActionsPanelInner
+						isOnBank={false}
 						dragId={'triggerAction'}
 						addPlaceholder="+ Add action"
 						actions={config.actions || []}
@@ -263,6 +264,7 @@ function TriggerEditModalConfig({ pluginSpec, config, updateConfig }) {
 								<td>
 									<MyErrorBoundary>
 										<FeedbackEditor
+											isOnBank={false}
 											feedback={conf}
 											setValue={(id, k, v) => updateFeedbackOptionConfig(i, k, v)}
 											innerDelete={() => delRow(i)}
