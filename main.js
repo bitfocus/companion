0a4c384e04fdc0938af2e372d9759ef0f59b8377
Julian Waller <git@julusian.co.uk>
2022-05-31 00:10:49 +0100

    fix: replace meow with something webpack friendly

--- main.js
+++ main.js
@@ -1,65 +1,39 @@
 #!/usr/bin/env node
+
 // Setup logging before anything else runs
 import Logging from './lib/Log/Controller.js'
 
 // Now we can think about startup
-import meow from 'meow'
+import { Command } from 'commander'
 import Registry from './lib/Registry.js'
 import os from 'os'
 import path from 'path'
 import fs from 'fs-extra'
 import envPaths from 'env-paths'
 import { nanoid } from 'nanoid'
+import logger from './lib/Log/Controller.js'
 
-const cli = meow(
-	`
-	Usage
-	  $ ./main.js --admin-port 8000
+const program = new Command()
 
-	Options
-	  --list-interfaces       List the available network interfaces that can be passed to --admin-interface
-	  --admin-port            Set the port the admin ui should bind to (default: 8000)
-	  --admin-interface       Set the interface the admin ui should bind to. The first ip on this interface will be used (default: '')
-	  --admin-address         Set the ip address the admin ui should bind to (default: 0.0.0.0)
-	  --config-dir            Use the specified directory for storing configuration. The default path varies by system, and is different to 2.2 (the old path will be used if existing config is found)
-	  --extra-module-path     Search an extra directory for modules to load
-`,
-	{
-		importMeta: { url: import.meta.url },
-		flags: {
-			listInterfaces: {
-				type: 'boolean',
-				default: false,
-			},
-			adminPort: {
-				type: 'number',
-				default: 8000,
-				// alias: 'r',
-			},
-			adminInterface: {
-				type: 'string',
-			},
-			adminAddress: {
-				type: 'string',
-			},
-			configDir: {
-				type: 'string',
-			},
-			machineId: {
-				// Note: intentionally omitted from the docs, as it should only be set by electron
-				type: 'string',
-			},
-			extraModulePath: {
-				type: 'string',
-			},
-			logToFile: {
-				type: 'boolean',
-			},
-		},
-	}
-)
+program
+	.option('--list-interfaces', 'List the available network interfaces that can be passed to --admin-interface')
+	.option('--admin-port <number>', 'Set the port the admin ui should bind to (default: 8000)', 8000)
+	.option(
+		'--admin-interface <string>',
+		"Set the interface the admin ui should bind to. The first ip on this interface will be used (default: '')"
+	)
+	.option('--admin-address <string>', 'Set the ip address the admin ui should bind to (default: 0.0.0.0)', '0.0.0.0')
+	.option(
+		'--config-dir <string>',
+		'Use the specified directory for storing configuration. The default path varies by system, and is different to 2.2 (the old path will be used if existing config is found)'
+	)
+	.option('--extra-module-path <string>', 'Search an extra directory for modules to load')
 
-if (cli.flags.listInterfaces) {
+program.parse()
+
+const options = program.opts()
+
+if (options.listInterfaces) {
 	console.error('Available Interfaces:')
 
 	const interfaces = os.networkInterfaces()
@@ -75,22 +49,24 @@ if (cli.flags.listInterfaces) {
 	process.exit(0)
 }
 
-if (isNaN(cli.flags.adminPort)) {
+logger.logger.info('Application starting')
+
+if (isNaN(options.adminPort)) {
 	console.error(`Port number is not valid`)
 	process.exit(1)
 }
 
-if (cli.flags.adminAddress && cli.flags.adminInterface) {
+if (options.adminAddress && options.adminInterface) {
 	console.error(`Only one of admin-interface and admin-address can be specified`)
 	process.exit(1)
 }
 
-let adminIp = cli.flags.adminAddress || '0.0.0.0' // default to admin global
+let adminIp = options.adminAddress || '0.0.0.0' // default to admin global
 
-if (cli.flags.adminInterface) {
+if (options.adminInterface) {
 	adminIp = null
 
-	const interfaceInfo = os.networkInterfaces()[cli.flags.adminInterface]
+	const interfaceInfo = os.networkInterfaces()[options.adminInterface]
 	if (interfaceInfo) {
 		for (const ifAddr of interfaceInfo) {
 			// only show non-ipv4 addresses for now
@@ -102,12 +78,12 @@ if (cli.flags.adminInterface) {
 	}
 
 	if (!adminIp) {
-		console.error(`Invalid interface name "${cli.flags.adminInterface}"`)
+		console.error(`Invalid interface name "${options.adminInterface}"`)
 		process.exit(1)
 	}
 }
 
-let configDir = cli.flags.configDir
+let configDir = options.configDir
 if (!configDir) {
 	// Check the old location first
 	configDir = path.join(
@@ -128,11 +104,11 @@ try {
 	process.exit(1)
 }
 
-if (cli.flags.logToFile) {
+if (options.logToFile) {
 	Logging.setupLogToFile(configDir)
 }
 
-let machineId = cli.flags.machineId
+let machineId = options.machineId
 if (!machineId) {
 	// Use stored value
 	const machineIdPath = path.join(configDir, 'machid')
@@ -159,7 +135,7 @@ if (!machineId) {
 const registry = new Registry(configDir, machineId)
 
 registry
-	.ready(cli.flags.extraModulePath, adminIp, cli.flags.adminPort)
+	.ready(options.extraModulePath, adminIp, options.adminPort)
 	.then(() => {
 		console.log('Started')
 	})
