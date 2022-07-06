e521d7ea007965dba2fbceb76b68ecc7b8845037
Julian Waller <git@julusian.co.uk>
2022-07-07 00:21:24 +0100

    chore: remove unused web-vitals

--- webui/src/index.js
+++ webui/src/index.js
@@ -17,7 +17,6 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
 import App from './App'
-import reportWebVitals from './reportWebVitals'
 
 // import i18n from 'i18next'
 // import Backend from 'i18next-http-backend'
@@ -81,8 +80,3 @@ ReactDOM.render(
 	</React.StrictMode>,
 	document.getElementById('root')
 )
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals()
