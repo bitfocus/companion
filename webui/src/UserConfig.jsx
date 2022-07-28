a36f792a6ac45a7bc3681e9f18d6ce01bbaf1b13
Alberto Righetto <arighetto88@gmail.com>
2022-07-28 16:38:40 +0200

    feat: Set custom variable value using remote control protocols (#2056)
    Co-authored-by: Keith Rocheck <749812+krocheck@users.noreply.github.com>
    Co-authored-by: Julian Waller <git@julusian.co.uk>

--- webui/src/UserConfig.jsx
+++ webui/src/UserConfig.jsx
@@ -804,6 +804,16 @@ function RemoteControlInfo() {
 									<br />
 									<i>Change background color on a button (#000000)</i>
 								</li>
+								<li>
+									<code>CUSTOM-VARIABLE</code> &lt;name&gt; <code>SET-VALUE</code> &lt;value&gt;
+									<br />
+									<i>Change custom variable value</i>
+								</li>
+								<li>
+									<code>RESCAN</code>
+									<br />
+									<i>Make Companion rescan for newly attached USB surfaces</i>
+								</li>
 							</ul>
 
 							<p>
@@ -820,6 +830,12 @@ function RemoteControlInfo() {
 								Press page 1 button 2
 								<br />
 								<code>BANK-PRESS 1 2</code>
+							</p>
+
+							<p>
+								Change custom variable &quot;cue&quot; to value &quot;intro&quot;
+								<br />
+								<code>CUSTOM-VARIABLE cue SET-VALUE intro</code>
 							</p>
 						</MyErrorBoundary>
 					</CTabPane>
@@ -860,6 +876,16 @@ function RemoteControlInfo() {
 										<br />
 										<i>Change text size on a button (between the predefined values)</i>
 									</li>
+									<li>
+										<code>/api/custom-variable/</code>&lt;name&gt;<code>?value=</code>&lt;value&gt;
+										<br />
+										<i>Change custom variable value</i>
+									</li>
+									<li>
+										<code>/rescan</code>
+										<br />
+										<i>Make Companion rescan for newly attached USB surfaces</i>
+									</li>
 								</ul>
 							</p>
 
@@ -884,6 +910,12 @@ function RemoteControlInfo() {
 								font size to 28px
 								<br />
 								<code>/style/bank/2/4/?text=TEST&bgcolor=%23ffffff&color=%23000000&size=28px</code>
+							</p>
+
+							<p>
+								Change custom variable &quot;cue&quot; to value &quot;intro&quot;
+								<br />
+								<code>/api/custom-variable/cue?value=intro</code>
 							</p>
 						</MyErrorBoundary>
 					</CTabPane>
@@ -934,6 +966,16 @@ function RemoteControlInfo() {
 									<br />
 									<i>Change text on a button</i>
 								</li>
+								<li>
+									<code>/custom-variable/</code>&lt;name&gt;<code>/value</code> &lt;value&gt;
+									<br />
+									<i>Change custom variable value</i>
+								</li>
+								<li>
+									<code>/rescan</code> 1
+									<br />
+									<i>Make Companion rescan for newly attached USB surfaces</i>
+								</li>
 							</ul>
 
 							<p>
@@ -956,6 +998,12 @@ function RemoteControlInfo() {
 								Change the text of button 5 on page 1 to ONLINE
 								<br />
 								<code>/style/text/1/5 ONLINE</code>
+							</p>
+
+							<p>
+								Change custom variable &quot;cue&quot; to value &quot;intro&quot;
+								<br />
+								<code>/custom-variable/cue/value intro</code>
 							</p>
 						</MyErrorBoundary>
 					</CTabPane>
