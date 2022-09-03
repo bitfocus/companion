fc7532a13ce9b96b421668757a70b57d2bb15ac1
Julian Waller <git@julusian.co.uk>
2022-09-03 14:21:17 +0100

    chore: check types of properties in module package tests

--- test/module-syntax.test.js
+++ test/module-syntax.test.js
@@ -43,16 +43,40 @@ describe('package.json has all fields', function () {
 				const js = JSON.parse(data.toString())
 
 				expect(js).toHaveProperty('name')
+				expect(js.name).toBeTruthy()
+				expect(typeof js.name).toBe('string')
+
 				expect(js).toHaveProperty('version')
+				expect(js.version).toBeTruthy()
+				expect(typeof js.version).toBe('string')
+
 				// expect(js).toHaveProperty('api_version')
 				// expect(js).toHaveProperty('description')
+
 				expect(js).toHaveProperty('main')
+				expect(js.main).toBeTruthy()
+				expect(typeof js.main).toBe('string')
+
 				expect(js).toHaveProperty('keywords')
+				expect(js.keywords).toBeInstanceOf(Array)
+
 				expect(js).toHaveProperty('manufacturer')
+				expect(js.manufacturer).toBeTruthy()
+
 				expect(js).toHaveProperty('product')
+				expect(js.product).toBeTruthy()
+				// expect(typeof js.product).toBe('string')
+
 				expect(js).toHaveProperty('shortname')
+				expect(js.shortname).toBeTruthy()
+				// expect(typeof js.shortname).toBe('string')
+
 				expect(js).toHaveProperty('author')
+				expect(js.author).toBeTruthy()
+
 				expect(js).toHaveProperty('license')
+				expect(js.license).toBeTruthy()
+				expect(typeof js.license).toBe('string')
 			},
 			10000
 		)
