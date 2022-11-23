bitfocus/companion

commit ef64755d58324c84c959bd7d9fdbc0f985a37031

fix: select input dropdowns being clipped inside scroll regions

webui/src/Buttons/EditButton/ActionsPanel.jsx
@@ -571,6 +571,10 @@ function AddActionDropdown({ onSelect, placeholder, recentActions }) {
 
 	return (
 		<Select
+			menuPortalTarget={document.body}
+			menuPosition={'fixed'}
+			classNamePrefix="select-control"
+			menuPlacement="auto"
 			isClearable={false}
 			isSearchable={true}
 			isMulti={false}

webui/src/Buttons/EditButton/FeedbackPanel.jsx
@@ -616,7 +616,10 @@ export function AddFeedbackDropdown({ onSelect, booleanOnly, recentFeedbacks }) 
 
 	return (
 		<Select
-			menuPlacement="top"
+			menuPortalTarget={document.body}
+			menuPosition={'fixed'}
+			classNamePrefix="select-control"
+			menuPlacement="auto"
 			isClearable={false}
 			isSearchable={true}
 			isMulti={false}

webui/src/Components/DropdownInputField.jsx
@@ -124,6 +124,8 @@ export function DropdownInputField({ definition, multiple, value, setValue, setV
 
 	const selectProps = {
 		classNamePrefix: 'select-control',
+		menuPortalTarget: document.body,
+		menuPosition: 'fixed',
 		menuPlacement: 'auto',
 		isClearable: false,
 		isSearchable: minChoicesForSearch <= options.length,

webui/src/scss/_controls.scss
@@ -72,3 +72,7 @@ input[type='checkbox']:checked {
 		color: red;
 	}
 }
+
+.select-control__menu-portal {
+	z-index: 100 !important;
+}

commit 23fc35a65531b27623651067eb66fdc1b4cfcc29

fix: dropdowns in modals broken #2196
broken by ef64755d

webui/src/Buttons/EditButton/ActionsPanel.jsx
@@ -18,6 +18,7 @@ import { ActionTableRowOption } from './Table'
 import { useDrag, useDrop } from 'react-dnd'
 import { GenericConfirmModal } from '../../Components/GenericConfirmModal'
 import { AddActionsModal } from './AddModal'
+import { MenuPortalContext } from '../../Components/DropdownInputField'
 
 export function ActionsPanel({
 	page,
@@ -521,6 +522,7 @@ const noOptionsMessage = ({ inputValue }) => {
 }
 
 function AddActionDropdown({ onSelect, placeholder, recentActions }) {
+	const menuPortal = useContext(MenuPortalContext)
 	const instancesContext = useContext(InstancesContext)
 	const actionsContext = useContext(ActionsContext)
 
@@ -571,7 +573,8 @@ function AddActionDropdown({ onSelect, placeholder, recentActions }) {
 
 	return (
 		<Select
-			menuPortalTarget={document.body}
+			menuShouldBlockScroll={!!menuPortal} // The dropdown doesn't follow scroll when in a modal
+			menuPortalTarget={menuPortal || document.body}
 			menuPosition={'fixed'}
 			classNamePrefix="select-control"
 			menuPlacement="auto"

webui/src/Buttons/EditButton/FeedbackPanel.jsx
@@ -19,6 +19,7 @@ import { GenericConfirmModal } from '../../Components/GenericConfirmModal'
 import { DropdownInputField } from '../../Components'
 import { ButtonStyleConfigFields } from './ButtonStyleConfig'
 import { AddFeedbacksModal } from './AddModal'
+import { MenuPortalContext } from '../../Components/DropdownInputField'
 
 export const FeedbacksPanel = function ({
 	page,
@@ -564,6 +565,7 @@ const noOptionsMessage = ({ inputValue }) => {
 }
 
 export function AddFeedbackDropdown({ onSelect, booleanOnly, recentFeedbacks }) {
+	const menuPortal = useContext(MenuPortalContext)
 	const feedbacksContext = useContext(FeedbacksContext)
 	const instancesContext = useContext(InstancesContext)
 
@@ -616,7 +618,8 @@ export function AddFeedbackDropdown({ onSelect, booleanOnly, recentFeedbacks }) 
 
 	return (
 		<Select
-			menuPortalTarget={document.body}
+			menuShouldBlockScroll={!!menuPortal} // The dropdown doesn't follow scroll when in a modal
+			menuPortalTarget={menuPortal || document.body}
 			menuPosition={'fixed'}
 			classNamePrefix="select-control"
 			menuPlacement="auto"

webui/src/Components/DropdownInputField.jsx
@@ -1,9 +1,15 @@
 import classNames from 'classnames'
+import { createContext } from 'react'
+import { useContext } from 'react'
 import { useMemo, useEffect, useCallback } from 'react'
 import Select from 'react-select'
 import CreatableSelect from 'react-select/creatable'
 
+export const MenuPortalContext = createContext(null)
+
 export function DropdownInputField({ definition, multiple, value, setValue, setValid }) {
+	const menuPortal = useContext(MenuPortalContext)
+
 	const options = useMemo(() => {
 		let choices = []
 		if (definition.choices) {
@@ -124,7 +130,8 @@ export function DropdownInputField({ definition, multiple, value, setValue, setV
 
 	const selectProps = {
 		classNamePrefix: 'select-control',
-		menuPortalTarget: document.body,
+		menuPortalTarget: menuPortal || document.body,
+		menuShouldBlockScroll: !!menuPortal, // The dropdown doesn't follow scroll when in a modal
 		menuPosition: 'fixed',
 		menuPlacement: 'auto',
 		isClearable: false,

webui/src/Triggers/EditModal.jsx
@@ -20,6 +20,7 @@ import { ActionsPanelInner } from '../Buttons/EditButton/ActionsPanel'
 import { CheckboxInputField } from '../Components'
 import { AddFeedbacksModal } from '../Buttons/EditButton/AddModal'
 import { useEffect } from 'react'
+import { MenuPortalContext } from '../Components/DropdownInputField'
 
 function getPluginSpecDefaults(pluginOptions) {
 	const config = {}
@@ -183,81 +184,85 @@ export function TriggerEditModal({ doClose, doSave, item, plugins }) {
 		}))
 	}, [])
 
+	const [modalRef, setModalRef] = useState(null)
+
 	return (
-		<CModal show={true} onClose={doClose} size="lg">
-			<CForm onSubmit={doSaveInner} className={'edit-button-panel'}>
-				<CModalHeader closeButton>
-					<h5>Trigger Editor</h5>
-				</CModalHeader>
-				<CModalBody>
-					<CFormGroup>
-						<label>Name</label>
-						<CInput required value={config.title} onChange={setTitle} />
-					</CFormGroup>
+		<CModal innerRef={setModalRef} show={true} onClose={doClose} size="lg">
+			<MenuPortalContext.Provider value={modalRef}>
+				<CForm onSubmit={doSaveInner} className={'edit-button-panel'}>
+					<CModalHeader closeButton>
+						<h5>Trigger Editor</h5>
+					</CModalHeader>
+					<CModalBody>
+						<CFormGroup>
+							<label>Name</label>
+							<CInput required value={config.title} onChange={setTitle} />
+						</CFormGroup>
 
-					<legend>Condition</legend>
-					<CFormGroup>
-						<label>Type</label>
-						<Select
-							value={pluginChoices.find((c) => c.value === config.type)}
-							onChange={changeType}
-							isSearchable={false}
-							isClearable={false}
-							options={pluginChoices}
-							required
+						<legend>Condition</legend>
+						<CFormGroup>
+							<label>Type</label>
+							<Select
+								value={pluginChoices.find((c) => c.value === config.type)}
+								onChange={changeType}
+								isSearchable={false}
+								isClearable={false}
+								options={pluginChoices}
+								required
+							/>
+						</CFormGroup>
+
+						{pluginSpec?.options ? (
+							<TriggerEditModalConfig pluginSpec={pluginSpec} config={config.config} setConfig={setConfig} />
+						) : (
+							'Unknown type selected'
+						)}
+
+						<hr />
+						<legend>Action</legend>
+						<CRow form className="button-style-form">
+							<CCol className="fieldtype-checkbox" sm={2} xs={3}>
+								<CButton
+									color="warning"
+									onMouseDown={() =>
+										context.socket.emit('schedule_test_actions', config.title, config.actions, config.relative_delays)
+									}
+								>
+									Test actions
+								</CButton>
+							</CCol>
+							<CCol className="fieldtype-checkbox" sm={2} xs={3}>
+								<CLabel>Relative Delays</CLabel>
+								<p>
+									<CheckboxInputField
+										definition={{ default: false }}
+										value={config.relative_delays ?? false}
+										setValue={setRelativeDelays}
+									/>
+									&nbsp;
+								</p>
+							</CCol>
+						</CRow>
+						<ActionsPanelInner
+							isOnBank={false}
+							dragId={'triggerAction'}
+							addPlaceholder="+ Add action"
+							actions={config.actions || []}
+							setActions={setActions}
+							addAction={addActionSelect}
+							emitLearn={doLearn}
 						/>
-					</CFormGroup>
-
-					{pluginSpec?.options ? (
-						<TriggerEditModalConfig pluginSpec={pluginSpec} config={config.config} setConfig={setConfig} />
-					) : (
-						'Unknown type selected'
-					)}
-
-					<hr />
-					<legend>Action</legend>
-					<CRow form className="button-style-form">
-						<CCol className="fieldtype-checkbox" sm={2} xs={3}>
-							<CButton
-								color="warning"
-								onMouseDown={() =>
-									context.socket.emit('schedule_test_actions', config.title, config.actions, config.relative_delays)
-								}
-							>
-								Test actions
-							</CButton>
-						</CCol>
-						<CCol className="fieldtype-checkbox" sm={2} xs={3}>
-							<CLabel>Relative Delays</CLabel>
-							<p>
-								<CheckboxInputField
-									definition={{ default: false }}
-									value={config.relative_delays ?? false}
-									setValue={setRelativeDelays}
-								/>
-								&nbsp;
-							</p>
-						</CCol>
-					</CRow>
-					<ActionsPanelInner
-						isOnBank={false}
-						dragId={'triggerAction'}
-						addPlaceholder="+ Add action"
-						actions={config.actions || []}
-						setActions={setActions}
-						addAction={addActionSelect}
-						emitLearn={doLearn}
-					/>
-				</CModalBody>
-				<CModalFooter>
-					<CButton color="secondary" onClick={doClose}>
-						Cancel
-					</CButton>
-					<CButton color="primary" type="submit">
-						Save
-					</CButton>
-				</CModalFooter>
-			</CForm>
+					</CModalBody>
+					<CModalFooter>
+						<CButton color="secondary" onClick={doClose}>
+							Cancel
+						</CButton>
+						<CButton color="primary" type="submit">
+							Save
+						</CButton>
+					</CModalFooter>
+				</CForm>
+			</MenuPortalContext.Provider>
 		</CModal>
 	)
 }