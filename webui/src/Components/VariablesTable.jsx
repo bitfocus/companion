e1f199fa3173a1ede07ad8049e0e885b42102600
William Viker <william.viker@gmail.com>
2023-02-09 20:08:06 +0100

    fix: ui: changing more ui components, WIP

--- webui/src/Components/VariablesTable.jsx
+++ webui/src/Components/VariablesTable.jsx
@@ -110,7 +110,7 @@ export function VariablesTable({ label }) {
 					<tr>
 						<th>Variable</th>
 						<th>Description</th>
-						<th>Current value</th>
+						<th>Value</th>
 						<th>&nbsp;</th>
 					</tr>
 				</thead>
@@ -164,7 +164,13 @@ const VariablesTableRow = memo(function VariablesTableRow({ variable, value, lab
 				$({label}:{variable.name})
 			</td>
 			<td>{variable.label}</td>
-			<td>{elms}</td>
+			<td>
+				{elms === '' || elms === null || elms === undefined ? (
+					'(empty)'
+				) : (
+					<code style={{ backgroundColor: 'rgba(255,0,0,0.1)', padding: '1px 4px' }}>{elms}</code>
+				)}
+			</td>
 			<td>
 				<CopyToClipboard text={`$(${label}:${variable.name})`} onCopy={onCopied}>
 					<CButton size="sm">
