edf16943aa67a26aa40386cdb7001b4eb9268e21
Julian Waller <git@julusian.co.uk>
2023-04-23 22:05:54 +0100

    feat: bonjour discovery helper

---
+++ webui/src/Components/BonjourDeviceInputField.jsx
@@ -0,0 +1,104 @@
+import { createContext, useRef, useState } from 'react'
+import { useContext } from 'react'
+import { useMemo, useEffect } from 'react'
+import { SocketContext, socketEmitPromise } from '../util'
+import { DropdownInputField } from './DropdownInputField'
+
+export const MenuPortalContext = createContext(null)
+
+export function BonjourDeviceInputField({ value, setValue, filter, allowNone, noneLabel }) {
+	const socket = useContext(SocketContext)
+
+	const [_subId, setSubId] = useState(null)
+	const subIdRef = useRef(null)
+
+	const [services, setServices] = useState({})
+
+	// Listen for data
+	useEffect(() => {
+		const onUp = (svc) => {
+			if (svc.subId !== subIdRef.current) return
+
+			// console.log('up', svc)
+
+			setServices((svcs) => {
+				return {
+					...svcs,
+					[svc.fqdn]: svc,
+				}
+			})
+		}
+		const onDown = (svc) => {
+			if (svc.subId !== subIdRef.current) return
+
+			// console.log('down', svc)
+
+			setServices((svcs) => {
+				const res = { ...svcs }
+				delete res[svc.fqdn]
+				return res
+			})
+		}
+
+		socket.on('bonjour:service:up', onUp)
+		socket.on('bonjour:service:down', onDown)
+
+		return () => {
+			socket.off('bonjour:service:up', onUp)
+			socket.off('bonjour:service:down', onDown)
+		}
+	}, [])
+
+	// Start/Stop the subscription
+	useEffect(() => {
+		let killed = false
+		let mySubId = null
+		socketEmitPromise(socket, 'bonjour:subscribe', [filter])
+			.then((newSubId) => {
+				// Make sure it hasnt been terminated
+				if (killed) {
+					socket.emit('bonjour:unsubscribe', [newSubId])
+					return
+				}
+
+				// Sub is good, set it up
+				mySubId = newSubId
+				subIdRef.current = newSubId
+				setSubId(newSubId)
+				setServices({})
+			})
+			.catch((e) => {
+				console.error('Bonjour subscription failed: ', e)
+			})
+
+		return () => {
+			killed = true
+
+			subIdRef.current = null
+			setSubId(null)
+			setServices({})
+
+			if (mySubId) socket.emit('bonjour:unsubscribe', mySubId)
+		}
+	}, [socket, filter])
+
+	const choices = useMemo(() => {
+		const choices = []
+
+		if (allowNone) {
+			choices.push({ id: null, label: noneLabel || 'None' })
+		}
+
+		for (const svc of Object.values(services)) {
+			const address = `${svc.host}:${svc.port}`
+			choices.push({
+				id: address,
+				label: `${svc.name} (${address})`,
+			})
+		}
+
+		return choices
+	}, [services, allowNone, noneLabel])
+
+	return <DropdownInputField value={value} setValue={setValue} choices={choices} />
+}
--- webui/src/Instances/InstanceEditPanel.jsx
+++ webui/src/Instances/InstanceEditPanel.jsx
@@ -8,6 +8,7 @@ import { faQuestionCircle } from '@fortawesome/free-solid-svg-icons'
 import sanitizeHtml from 'sanitize-html'
 import { isLabelValid } from '@companion/shared/Label'
 import CSwitch from '../CSwitch'
+import { BonjourDeviceInputField } from '../Components/BonjourDeviceInputField'
 
 export function InstanceEditPanel({ instanceId, instanceStatus, doConfigureInstance, showHelp }) {
 	console.log('status', instanceStatus)
@@ -319,6 +320,16 @@ function ConfigField({ setValue, setValid, definition, value }) {
 			)
 			break
 		}
+		case 'bonjour-device':
+			return (
+				<BonjourDeviceInputField
+					value={value}
+					setValue={setValue2}
+					filter={definition.bonjour}
+					allowNone={definition.allowNone}
+					noneLabel={definition.noneLabel}
+				/>
+			)
 		default:
 			return <p>Unknown field "{definition.type}"</p>
 	}
