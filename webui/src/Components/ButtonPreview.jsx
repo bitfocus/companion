7ad0ed32db7e2be1847292b2d85f3a485f884a62
dnmeid <dnmeid@gmx.net>
2023-06-02 23:32:32 +0200

    feat: increase internal button resolution

--- webui/src/Components/ButtonPreview.jsx
+++ webui/src/Components/ButtonPreview.jsx
@@ -5,6 +5,26 @@ import { Buffer } from 'buffer'
 
 export function dataToButtonImage(data) {
 	const sourceData = Buffer.from(data)
+	const imageSize = Math.sqrt(sourceData.length / 3)
+
+	const bmpHeaderSize = 54
+	const bmpHeader = Buffer.alloc(bmpHeaderSize)
+	bmpHeader.write('BM', 0, 2) // flag
+	bmpHeader.writeUInt32LE(sourceData.length + bmpHeaderSize, 2) // filesize
+	bmpHeader.writeUInt32LE(0, 6) // reserver
+	bmpHeader.writeUInt32LE(bmpHeaderSize, 10) // data start
+
+	bmpHeader.writeUInt32LE(40, 14) // header info size
+	bmpHeader.writeUInt32LE(imageSize, 18) // width
+	bmpHeader.writeInt32LE(imageSize * -1, 22) // height
+	bmpHeader.writeUInt16LE(1, 26) // planes
+	bmpHeader.writeUInt16LE(24, 28) // bits per pixel
+	bmpHeader.writeUInt32LE(0, 30) // compress
+	bmpHeader.writeUInt32LE(sourceData.length, 34) // data size
+	bmpHeader.writeUInt32LE(0, 38) // hr
+	bmpHeader.writeUInt32LE(0, 42) // vr
+	bmpHeader.writeUInt32LE(0, 46) // colors
+	bmpHeader.writeUInt32LE(0, 48) // importantColors
 
 	const convertedData = Buffer.alloc(sourceData.length)
 	for (let i = 0; i < sourceData.length; i += 3) {
@@ -14,7 +34,7 @@ export function dataToButtonImage(data) {
 		convertedData.writeUInt8(sourceData.readUInt8(i + 2), i)
 	}
 
-	return 'data:image/bmp;base64,' + Buffer.concat([PREVIEW_BMP_HEADER, convertedData]).toString('base64')
+	return 'data:image/bmp;base64,' + Buffer.concat([bmpHeader, convertedData]).toString('base64')
 }
 
 export const BlackImage = dataToButtonImage(Buffer.alloc(72 * 72 * 3))
