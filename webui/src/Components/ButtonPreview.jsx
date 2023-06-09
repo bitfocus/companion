2efc0132db16d66d93fe2388a5699f1049279713
dnmeid <dnmeid@gmx.net>
2023-06-09 16:32:08 +0200

    fix: draw border correct, cleanup

--- webui/src/Components/ButtonPreview.jsx
+++ webui/src/Components/ButtonPreview.jsx
@@ -2,26 +2,32 @@ import React from 'react'
 import classnames from 'classnames'
 import { Buffer } from 'buffer'
 
-export function dataToButtonImage(data) {
-	const sourceData = Buffer.from(data)
-	const imageSize = Math.sqrt(sourceData.length / 4)
-
+/**
+ * Creates a BMP image header for the given size
+ * assuming RGBA channel order, 32Bit/Pixel, starting with top left pixel
+ * @param {number} imageWidth 
+ * @param {number} imageHeight 
+ * @returns {Buffer} buffer containing the header
+ */
+export function createBmpHeader(imageWidth = 72, imageHeight = 72) {
+	const dataLength = imageWidth * imageHeight * 4
 	const bmpHeaderSize = 138
 	const bmpHeader = Buffer.alloc(bmpHeaderSize, 0)
+	// file header
 	bmpHeader.write('BM', 0, 2) // flag
-	bmpHeader.writeUInt32LE(sourceData.length + bmpHeaderSize, 2) // filesize
+	bmpHeader.writeUInt32LE(dataLength + bmpHeaderSize, 2) // filesize
 	bmpHeader.writeUInt32LE(0, 6) // reserved
 	bmpHeader.writeUInt32LE(bmpHeaderSize, 10) // data start
-
+	// image header
 	bmpHeader.writeUInt32LE(124, 14) // header info size
-	bmpHeader.writeUInt32LE(imageSize, 18) // width
-	bmpHeader.writeInt32LE(imageSize * -1, 22) // height
+	bmpHeader.writeUInt32LE(imageWidth, 18) // width
+	bmpHeader.writeInt32LE(imageHeight * -1, 22) // height
 	bmpHeader.writeUInt16LE(1, 26) // planes
 	bmpHeader.writeUInt16LE(32, 28) // bits per pixel
 	bmpHeader.writeUInt32LE(3, 30) // compress
-	bmpHeader.writeUInt32LE(sourceData.length, 34) // data size
-	bmpHeader.writeUInt32LE(Math.round(39.375 * imageSize), 38) // hr
-	bmpHeader.writeUInt32LE(Math.round(39.375 * imageSize), 42) // vr
+	bmpHeader.writeUInt32LE(dataLength, 34) // data size
+	bmpHeader.writeUInt32LE(Math.round(39.375 * imageWidth), 38) // hr
+	bmpHeader.writeUInt32LE(Math.round(39.375 * imageHeight), 42) // vr
 	bmpHeader.writeUInt32LE(0, 46) // colors
 	bmpHeader.writeUInt32LE(0, 50) // importantColors
 	bmpHeader.writeUInt32LE(0x000000ff, 54) // Red Bitmask
@@ -29,6 +35,14 @@ export function dataToButtonImage(data) {
 	bmpHeader.writeUInt32LE(0x00ff0000, 62) // Blue Bitmask
 	bmpHeader.writeUInt32LE(0xff000000, 66) // Alpha Bitmask
 	bmpHeader.write('sRGB', 70, 4) // colorspace
+
+	return bmpHeader
+}
+
+export function dataToButtonImage(data) {
+	const sourceData = Buffer.from(data)
+	const imageSize = Math.sqrt(sourceData.length / 4)
+	const bmpHeader = createBmpHeader(imageSize, imageSize)
 
 	return 'data:image/bmp;base64,' + Buffer.concat([bmpHeader, sourceData]).toString('base64')
 }
