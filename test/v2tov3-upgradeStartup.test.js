89f839b841418be541c6d1c34cc2428879586d81
Julian Waller <git@julusian.co.uk>
2023-09-27 21:44:09 +0100

    wip: refactor to avoid depending on Registry.js

--- test/v2tov3-upgradeStartup.test.js
+++ test/v2tov3-upgradeStartup.test.js
@@ -1,12 +1,10 @@
-import Registry from '../lib/Registry.js'
 import DataStoreBase from '../lib/Data/StoreBase.js'
 import DataDatabase from '../lib/Data/Database.js'
 import LogController from '../lib/Log/Controller.js'
 import v2tov3 from '../lib/Data/Upgrades/v2tov3.js'
 
 function CreateDataDatabase(dbContents) {
-	const registry = new Registry('test/config/empty/', 'foo')
-	const db = new DataStoreBase(registry, 'db', 4000, DataDatabase.defaults, 'Data/Database')
+	const db = new DataStoreBase('test/config/empty/', 'db', 4000, DataDatabase.defaults, 'Data/Database')
 	// Bypass loading and just set it to our test data
 	db.store = dbContents
 	console.log('Got: ')
