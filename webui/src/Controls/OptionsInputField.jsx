47872c4158b60942e42dae14c95b3804c6e2015e
Julian Waller <git@julusian.co.uk>
2023-10-09 22:16:49 +0100

    feat: indiciate variables support on input field (#2583)

--- webui/src/Controls/OptionsInputField.jsx
+++ webui/src/Controls/OptionsInputField.jsx
@@ -9,7 +9,7 @@ import {
 } from '../Components'
 import { InternalCustomVariableDropdown, InternalInstanceField } from './InternalInstanceFields'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faQuestionCircle } from '@fortawesome/free-solid-svg-icons'
+import { faDollarSign, faQuestionCircle } from '@fortawesome/free-solid-svg-icons'
 
 export function OptionsInputField({
 	instanceId,
@@ -29,6 +29,7 @@ export function OptionsInputField({
 	}
 
 	let control = undefined
+	let features = {}
 	switch (option.type) {
 		case 'textinput': {
 			control = (
@@ -42,6 +43,7 @@ export function OptionsInputField({
 					setValue={setValue2}
 				/>
 			)
+			features.variables = !!option.useVariables
 			break
 		}
 		case 'dropdown': {
@@ -130,13 +132,16 @@ export function OptionsInputField({
 		control = <CInputGroupText>Unknown type "{option.type}"</CInputGroupText>
 	}
 
+	const featureIcons = []
+	if (features.variables)
+		featureIcons.push(<FontAwesomeIcon key="variables" icon={faDollarSign} title={'Supports variables'} />)
+
 	return (
 		<CFormGroup style={{ display: visibility === false ? 'none' : null }}>
 			<CLabel>
 				{option.label}
-				{option.tooltip && (
-					<FontAwesomeIcon style={{ marginLeft: '5px' }} icon={faQuestionCircle} title={option.tooltip} />
-				)}
+				{featureIcons.length && <span className="feature-icons">{featureIcons}</span>}
+				{option.tooltip && <FontAwesomeIcon icon={faQuestionCircle} title={option.tooltip} />}
 			</CLabel>
 			{control}
 		</CFormGroup>
