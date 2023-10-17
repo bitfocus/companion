2220704677a04dc00a3170e29f033607f019f545
Julian Waller <git@julusian.co.uk>
2023-10-17 20:34:48 +0100

    wip: types

--- lib/Data/Model/ButtonModel.ts
+++ lib/Data/Model/ButtonModel.ts
@@ -23,7 +23,7 @@ export interface NormalButtonModel {
 
 	feedbacks: FeedbackInstance[]
 
-	steps?: NormalButtonSteps
+	steps: NormalButtonSteps
 }
 
 export type NormalButtonSteps = Record<
--- lib/Data/Model/ExportModel.ts
+++ lib/Data/Model/ExportModel.ts
@@ -4,64 +4,63 @@ import type { CustomVariablesModel } from './CustomVariableModel.js'
 export type SomeExport4 = ExportFull4 | ExportPageModel4 | ExportTriggersList4
 
 export interface ExportBase<Type extends string> {
-    readonly version: 4
-    readonly type: Type
+	readonly version: 4
+	readonly type: Type
 }
 
 export interface ExportFull4 extends ExportBase<'full'> {
-    pages?: Record<number, ExportPageContent4>
-    triggers?: Record<string, ExportTriggerContent4>
-    custom_variables?: CustomVariablesModel
-    instances?: ExportInstances4
-    surfaces?: unknown
+	pages?: Record<number, ExportPageContent4>
+	triggers?: Record<string, ExportTriggerContent4>
+	custom_variables?: CustomVariablesModel
+	instances?: ExportInstances4
+	surfaces?: unknown
 }
 
 export interface ExportPageModel4 extends ExportBase<'page'> {
-    page: ExportPageContent4
-    instances: ExportInstances4
-    oldPageNumber: number
+	page: ExportPageContent4
+	instances: ExportInstances4
+	oldPageNumber: number
 }
 
-
 export interface ExportTriggersList4 extends ExportBase<'trigger_list'> {
-    triggers: Record<string, ExportTriggerContent4>
-    instances: ExportInstances4
+	triggers: Record<string, ExportTriggerContent4>
+	instances: ExportInstances4
 }
 
 export type ExportTriggerContent4 = Record<string, any> // TODO
 
 export interface ExportPageContent4 {
-    name: string
-    controls: Record<number, Record<number, ExportControl4>>
+	name: string
+	controls: Record<number, Record<number, ExportControl4>>
 
-    gridSize: { columns: number; rows: number }
+	gridSize: ExportGridSize
 }
 
 export type ExportControl4 = Record<string, any> // TODO
 
 export type ExportInstances4 =
-    | Record<string, ExportInstanceFull4 | ExportInstanceMinimal4>
-    | Record<string, ConnectionConfig | undefined> // TODO - tidy
+	| Record<string, ExportInstanceFull4 | ExportInstanceMinimal4>
+	| Record<string, ConnectionConfig | undefined> // TODO - tidy
 
 export type ExportInstanceFull4 = {
-    label: string
-    config: unknown
-    isFirstInit: boolean
-    lastUpgradeIndex: number
-    instance_type: string
-    enabled: boolean
-    sortOrder: number
+	label: string
+	config: unknown
+	isFirstInit: boolean
+	lastUpgradeIndex: number
+	instance_type: string
+	enabled: boolean
+	sortOrder: number
 }
 
 export type ExportInstanceMinimal4 = {
-    label: string
-    instance_type: string
-    lastUpgradeIndex: number
+	label: string
+	instance_type: string
+	lastUpgradeIndex: number
 }
 
 export interface ExportGridSize {
-    minColumn: number
-    maxColumn: number
-    minRow: number
-    maxRow: number
+	minColumn: number
+	maxColumn: number
+	minRow: number
+	maxRow: number
 }
