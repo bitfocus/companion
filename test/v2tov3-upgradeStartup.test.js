4d2e9f22283a06e0bba043301d78fdb1f9be0cdb
Peter Newman <peterjnewman@gmail.com>
2023-10-20 11:39:02 +0100

    Fix the expected data and tidy the test a bit

--- test/v2tov3-upgradeStartup.test.js
+++ test/v2tov3-upgradeStartup.test.js
@@ -6,7 +6,7 @@ import { cloneDeep } from 'lodash-es'
 
 function CreateDataDatabase(dbContents) {
 	const db = new DataStoreBase('test/config/empty/', 'db', 4000, DataDatabase.defaults, 'Data/Database')
-	// Bypass loading and just set it to our test data
+	// Bypass loading data properly and just set it to our test data
 	db.store = cloneDeep(dbContents)
 	console.log('Got: ')
 	console.log(db.store)
@@ -17,10 +17,11 @@ function CreateDataDatabase(dbContents) {
 describe('upgrade', () => {
 	it('empty', () => {
 		const db = CreateDataDatabase(DataDatabase.Defaults)
-		const result = v2tov3.upgradeStartup(db, LogController.createLogger('foo'))
-		console.log(result)
-		console.log(db.store)
+		const result = v2tov3.upgradeStartup(db, LogController.createLogger('test-logger'))
 		expect(db.store).toEqual({
+			bank_rotate_left_actions: {},
+			bank_rotate_right_actions: {},
+			controls: {},
 			page_config_version: 3,
 		})
 	})
