bitfocus/companion

commit c052ee080631ea36372064031273f3ed83526c93

feat: surface groups (#2655)

webui/src/Controls/InternalInstanceFields.jsx
@@ -52,6 +52,7 @@ export function InternalInstanceField(option, isOnControl, readonly, value, setV
 					value={value}
 					setValue={setValue}
 					includeSelf={option.includeSelf}
+					useRawSurfaces={option.useRawSurfaces}
 				/>
 			)
 		case 'internal:trigger':
@@ -181,8 +182,8 @@ function InternalVariableDropdown({ value, setValue, disabled }) {
 	)
 }
 
-function InternalSurfaceBySerialDropdown({ isOnControl, value, setValue, disabled, includeSelf }) {
-	const context = useContext(SurfacesContext)
+function InternalSurfaceBySerialDropdown({ isOnControl, value, setValue, disabled, includeSelf, useRawSurfaces }) {
+	const surfacesContext = useContext(SurfacesContext)
 
 	const choices = useMemo(() => {
 		const choices = []
@@ -190,21 +191,26 @@ function InternalSurfaceBySerialDropdown({ isOnControl, value, setValue, disable
 			choices.push({ id: 'self', label: 'Current surface' })
 		}
 
-		for (const surface of Object.values(context?.available ?? {})) {
-			choices.push({
-				label: `${surface.name || surface.type} (${surface.id})`,
-				id: surface.id,
-			})
+		if (!useRawSurfaces) {
+			for (const group of surfacesContext ?? []) {
+				choices.push({
+					label: group.displayName,
+					id: group.id,
+				})
+			}
+		} else {
+			for (const group of surfacesContext ?? []) {
+				for (const surface of group.surfaces) {
+					choices.push({
+						label: surface.displayName,
+						id: surface.id,
+					})
+				}
+			}
 		}
 
-		for (const surface of Object.values(context?.offline ?? {})) {
-			choices.push({
-				label: `${surface.name || surface.type} (${surface.id}) - Offline`,
-				id: surface.id,
-			})
-		}
 		return choices
-	}, [context, isOnControl, includeSelf])
+	}, [surfacesContext, isOnControl, includeSelf, useRawSurfaces])
 
 	return <DropdownInputField disabled={disabled} value={value} choices={choices} multiple={false} setValue={setValue} />
 }

webui/src/Surfaces/index.jsx
@@ -1,60 +1,26 @@
-import React, { memo, useCallback, useContext, useEffect, useRef, useState } from 'react'
+import React, { memo, useCallback, useContext, useRef, useState } from 'react'
 import { CAlert, CButton, CButtonGroup } from '@coreui/react'
 import { SurfacesContext, socketEmitPromise, SocketContext } from '../util'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faAdd, faCog, faFolderOpen, faSync, faTrash } from '@fortawesome/free-solid-svg-icons'
 import { TextInputField } from '../Components/TextInputField'
-import { useMemo } from 'react'
 import { GenericConfirmModal } from '../Components/GenericConfirmModal'
 import { SurfaceEditModal } from './EditModal'
+import { AddSurfaceGroupModal } from './AddGroupModal'
+import classNames from 'classnames'
 
 export const SurfacesPage = memo(function SurfacesPage() {
 	const socket = useContext(SocketContext)
-	const surfaces = useContext(SurfacesContext)
+	const surfacesContext = useContext(SurfacesContext)
 
 	const confirmRef = useRef(null)
 
-	const surfacesList = useMemo(() => {
-		const ary = Object.values(surfaces.available)
-
-		ary.sort((a, b) => {
-			if (a.index !== b.index) {
-				return a.index - b.index
-			}
-
-			// fallback to serial
-			return a.id.localeCompare(b.id)
-		})
-
-		return ary
-	}, [surfaces.available])
-	const offlineSurfacesList = useMemo(() => {
-		const ary = Object.values(surfaces.offline)
-
-		ary.sort((a, b) => {
-			if (a.index !== b.index) {
-				return a.index - b.index
-			}
-
-			// fallback to serial
-			return a.id.localeCompare(b.id)
-		})
-
-		return ary
-	}, [surfaces.offline])
-
-	const editModalRef = useRef()
+	const editModalRef = useRef(null)
+	const addGroupModalRef = useRef(null)
 	const confirmModalRef = useRef(null)
 
 	const [scanning, setScanning] = useState(false)
 	const [scanError, setScanError] = useState(null)
-
-	useEffect(() => {
-		// If surface disappears, hide the edit modal
-		if (editModalRef.current) {
-			editModalRef.current.ensureIdIsValid(Object.keys(surfaces))
-		}
-	}, [surfaces])
 
 	const refreshUSB = useCallback(() => {
 		setScanning(true)
@@ -89,8 +55,27 @@ export const SurfacesPage = memo(function SurfacesPage() {
 		[socket]
 	)
 
-	const configureSurface = useCallback((surface) => {
-		editModalRef.current.show(surface)
+	const addGroup = useCallback(() => {
+		addGroupModalRef.current.show()
+	}, [socket])
+
+	const deleteGroup = useCallback(
+		(groupId) => {
+			confirmRef?.current?.show('Remove Group', 'Are you sure?', 'Remove', () => {
+				socketEmitPromise(socket, 'surfaces:group-remove', [groupId]).catch((err) => {
+					console.error('Group remove failed', err)
+				})
+			})
+		},
+		[socket]
+	)
+
+	const configureSurface = useCallback((surfaceId) => {
+		editModalRef.current.show(surfaceId, null)
+	}, [])
+
+	const configureGroup = useCallback((groupId) => {
+		editModalRef.current.show(null, groupId)
 	}, [])
 
 	const forgetSurface = useCallback(
@@ -148,16 +133,16 @@ export const SurfacesPage = memo(function SurfacesPage() {
 				<CButton color="danger" onClick={addEmulator}>
 					<FontAwesomeIcon icon={faAdd} /> Add Emulator
 				</CButton>
+				<CButton color="warning" onClick={addGroup}>
+					<FontAwesomeIcon icon={faAdd} /> Add Group
+				</CButton>
 			</CButtonGroup>
 
-			<p>&nbsp;</p>
-
 			<SurfaceEditModal ref={editModalRef} />
+			<AddSurfaceGroupModal ref={addGroupModalRef} />
 			<GenericConfirmModal ref={confirmModalRef} />
 
-			<h5>Connected</h5>
-
-			<table className="table table-responsive-sm">
+			<table className="table table-responsive-sm table-margin-top">
 				<thead>
 					<tr>
 						<th>NO</th>
@@ -169,48 +154,34 @@ export const SurfacesPage = memo(function SurfacesPage() {
 					</tr>
 				</thead>
 				<tbody>
-					{surfacesList.map((surface) => (
-						<AvailableSurfaceRow
-							key={surface.id}
-							surface={surface}
-							updateName={updateName}
-							configureSurface={configureSurface}
-							deleteEmulator={deleteEmulator}
-						/>
-					))}
-
-					{surfacesList.length === 0 && (
-						<tr>
-							<td colSpan={4}>No control surfaces have been detected</td>
-						</tr>
+					{surfacesContext.map((group) =>
+						group.isAutoGroup && (group.surfaces || []).length === 1 ? (
+							<SurfaceRow
+								key={group.id}
+								surface={group.surfaces[0]}
+								index={group.index}
+								updateName={updateName}
+								configureSurface={configureSurface}
+								deleteEmulator={deleteEmulator}
+								forgetSurface={forgetSurface}
+							/>
+						) : (
+							<ManualGroupRow
+								key={group.id}
+								group={group}
+								configureGroup={configureGroup}
+								deleteGroup={deleteGroup}
+								updateName={updateName}
+								configureSurface={configureSurface}
+								deleteEmulator={deleteEmulator}
+								forgetSurface={forgetSurface}
+							/>
+						)
 					)}
-				</tbody>
-			</table>
 
-			<h5>Disconnected</h5>
-
-			<table className="table table-responsive-sm">
-				<thead>
-					<tr>
-						<th>ID</th>
-						<th>Name</th>
-						<th>Type</th>
-						<th>&nbsp;</th>
-					</tr>
-				</thead>
-				<tbody>
-					{offlineSurfacesList.map((surface) => (
-						<OfflineSuraceRow
-							key={surface.id}
-							surface={surface}
-							updateName={updateName}
-							forgetSurface={forgetSurface}
-						/>
-					))}
-
-					{offlineSurfacesList.length === 0 && (
+					{surfacesContext.length === 0 && (
 						<tr>
-							<td colSpan={4}>No items</td>
+							<td colSpan={7}>No control surfaces have been detected</td>
 						</tr>
 					)}
 				</tbody>
@@ -219,57 +190,98 @@ export const SurfacesPage = memo(function SurfacesPage() {
 	)
 })
 
-function AvailableSurfaceRow({ surface, updateName, configureSurface, deleteEmulator }) {
-	const updateName2 = useCallback((val) => updateName(surface.id, val), [updateName, surface.id])
-	const configureSurface2 = useCallback(() => configureSurface(surface), [configureSurface, surface])
-	const deleteEmulator2 = useCallback(() => deleteEmulator(surface.id), [deleteEmulator, surface.id])
+function ManualGroupRow({
+	group,
+	configureGroup,
+	deleteGroup,
+	updateName,
+	configureSurface,
+	deleteEmulator,
+	forgetSurface,
+}) {
+	const configureGroup2 = useCallback(() => configureGroup(group.id), [configureGroup, group.id])
+	const deleteGroup2 = useCallback(() => deleteGroup(group.id), [deleteGroup, group.id])
+	const updateName2 = useCallback((val) => updateName(group.id, val), [updateName, group.id])
 
 	return (
-		<tr>
-			<td>#{surface.index}</td>
-			<td>{surface.id}</td>
-			<td>
-				<TextInputField value={surface.name} setValue={updateName2} />
-			</td>
-			<td>{surface.type}</td>
-			<td>{surface.location}</td>
-			<td className="text-right">
-				<CButtonGroup>
-					<CButton onClick={configureSurface2} title="Configure">
-						<FontAwesomeIcon icon={faCog} /> Settings
-					</CButton>
+		<>
+			<tr>
+				<td>#{group.index}</td>
+				<td>{group.id}</td>
+				<td>
+					<TextInputField value={group.displayName} setValue={updateName2} />
+				</td>
+				<td>Group</td>
+				<td>-</td>
+				<td className="text-right">
+					<CButtonGroup>
+						<CButton onClick={configureGroup2} title="Configure">
+							<FontAwesomeIcon icon={faCog} /> Settings
+						</CButton>
 
-					{surface.integrationType === 'emulator' && (
-						<>
-							<CButton href={`/emulator/${surface.id.substring(9)}`} target="_blank" title="Open Emulator">
-								<FontAwesomeIcon icon={faFolderOpen} />
-							</CButton>
-							<CButton onClick={deleteEmulator2} title="Delete Emulator">
-								<FontAwesomeIcon icon={faTrash} />
-							</CButton>
-						</>
-					)}
-				</CButtonGroup>
-			</td>
-		</tr>
+						<CButton onClick={deleteGroup2} title="Delete group">
+							<FontAwesomeIcon icon={faTrash} /> Delete
+						</CButton>
+					</CButtonGroup>
+				</td>
+			</tr>
+			{(group.surfaces || []).map((surface) => (
+				<SurfaceRow
+					key={surface.id}
+					surface={surface}
+					updateName={updateName}
+					configureSurface={configureSurface}
+					deleteEmulator={deleteEmulator}
+					forgetSurface={forgetSurface}
+					noBorder
+				/>
+			))}
+		</>
 	)
 }
 
-function OfflineSuraceRow({ surface, updateName, forgetSurface }) {
+function SurfaceRow({ surface, index, updateName, configureSurface, deleteEmulator, forgetSurface, noBorder }) {
 	const updateName2 = useCallback((val) => updateName(surface.id, val), [updateName, surface.id])
+	const configureSurface2 = useCallback(() => configureSurface(surface.id), [configureSurface, surface.id])
+	const deleteEmulator2 = useCallback(() => deleteEmulator(surface.id), [deleteEmulator, surface.id])
 	const forgetSurface2 = useCallback(() => forgetSurface(surface.id), [forgetSurface, surface.id])
 
 	return (
-		<tr>
+		<tr
+			className={classNames({
+				noBorder,
+			})}
+		>
+			<td>{index !== undefined ? `#${index}` : ''}</td>
 			<td>{surface.id}</td>
 			<td>
 				<TextInputField value={surface.name} setValue={updateName2} />
 			</td>
 			<td>{surface.type}</td>
+			<td>{surface.isConnected ? surface.location || 'Local' : 'Offline'}</td>
 			<td className="text-right">
-				<CButton onClick={forgetSurface2}>
-					<FontAwesomeIcon icon={faTrash} /> Forget
-				</CButton>
+				{surface.isConnected ? (
+					<CButtonGroup>
+						<CButton onClick={configureSurface2} title="Configure">
+							<FontAwesomeIcon icon={faCog} /> Settings
+						</CButton>
+
+						{surface.integrationType === 'emulator' && (
+							<>
+								<CButton href={`/emulator/${surface.id.substring(9)}`} target="_blank" title="Open Emulator">
+									<FontAwesomeIcon icon={faFolderOpen} />
+								</CButton>
+								<CButton onClick={deleteEmulator2} title="Delete Emulator">
+									<FontAwesomeIcon icon={faTrash} />
+								</CButton>
+							</>
+						)}
+					</CButtonGroup>
+				) : (
+					<CButton onClick={forgetSurface2}>
+						<FontAwesomeIcon icon={faTrash} /> Forget
+					</CButton>
+				)}
 			</td>
 		</tr>
 	)

lib/Data/ImportExport.js
@@ -363,6 +363,7 @@ class DataImportExport extends CoreBase {
 
 			if (!config || !isFalsey(config.surfaces)) {
 				exp.surfaces = this.surfaces.exportAll(false)
+				exp.surfaceGroups = this.surfaces.exportAllGroups(false)
 			}
 
 			return exp
@@ -790,9 +791,7 @@ class DataImportExport extends CoreBase {
 					}
 
 					if (!config || config.surfaces) {
-						for (const [id, surface] of Object.entries(data.surfaces || {})) {
-							this.surfaces.importSurface(id, surface)
-						}
+						this.surfaces.importSurfaces(data.surfaceGroups || {}, data.surfaces || {})
 					}
 
 					if (!config || config.triggers) {

lib/Data/Metrics.js
@@ -33,23 +33,26 @@ class DataMetrics extends CoreBase {
 	#cycle() {
 		this.logger.silly('cycle')
 
-		const devices = this.surfaces.getDevicesList().available
-
 		/**
 		 * @type {string[]}
 		 */
 		const relevantDevices = []
 
 		try {
-			Object.values(devices).forEach((device) => {
-				if (device.id !== undefined && !device.id.startsWith('emulator:')) {
-					// remove leading "satellite-" from satellite device serial numbers.
-					const serialNumber = device.id.replace('satellite-', '')
-					// normalize serialNumber by md5 hashing it, we don't want/need the specific serialNumber anyways.
-					const deviceHash = crypto.createHash('md5').update(serialNumber).digest('hex')
-					if (deviceHash && deviceHash.length === 32) relevantDevices.push(deviceHash)
+			const surfaceGroups = this.surfaces.getDevicesList()
+			for (const surfaceGroup of surfaceGroups) {
+				if (!surfaceGroup.surfaces) continue
+
+				for (const surface of surfaceGroup.surfaces) {
+					if (surface.id && surface.isConnected && !surface.id.startsWith('emulator:')) {
+						// remove leading "satellite-" from satellite device serial numbers.
+						const serialNumber = surface.id.replace('satellite-', '')
+						// normalize serialnumber by md5 hashing it, we don't want/need the specific serialnumber anyways.
+						const deviceHash = crypto.createHash('md5').update(serialNumber).digest('hex')
+						if (deviceHash && deviceHash.length === 32) relevantDevices.push(deviceHash)
+					}
 				}
-			})
+			}
 		} catch (e) {
 			// don't care
 		}

lib/Data/Model/ExportModel.ts
@@ -14,6 +14,7 @@ export interface ExportFullv4 extends ExportBase<'full'> {
 	custom_variables?: CustomVariablesModel
 	instances?: ExportInstancesv4
 	surfaces?: unknown
+	surfaceGroups?: unknown
 }
 
 export interface ExportPageModelv4 extends ExportBase<'page'> {

lib/Internal/Surface.js
@@ -18,17 +18,8 @@
 import { combineRgb } from '@companion-module/base'
 import LogController from '../Log/Controller.js'
 
-/** @type {import('./Types.js').InternalActionInputField} */
-const CHOICES_CONTROLLER = {
-	type: 'internal:surface_serial',
-	label: 'Surface / controller',
-	id: 'controller',
-	default: 'self',
-	includeSelf: true,
-}
-
 /** @type {import('./Types.js').InternalActionInputField[]} */
-const CHOICES_CONTROLLER_WITH_VARIABLES = [
+const CHOICES_SURFACE_GROUP_WITH_VARIABLES = [
 	{
 		type: 'checkbox',
 		label: 'Use variables for surface',
@@ -36,12 +27,43 @@ const CHOICES_CONTROLLER_WITH_VARIABLES = [
 		default: false,
 	},
 	{
-		...CHOICES_CONTROLLER,
+		type: 'internal:surface_serial',
+		label: 'Surface / group',
+		id: 'controller',
+		default: 'self',
+		includeSelf: true,
 		isVisible: (options) => !options.controller_from_variable,
 	},
 	{
 		type: 'textinput',
-		label: 'Surface / controller',
+		label: 'Surface / group',
+		id: 'controller_variable',
+		default: 'self',
+		isVisible: (options) => !!options.controller_from_variable,
+		useVariables: true,
+	},
+]
+
+/** @type {import('./Types.js').InternalActionInputField[]} */
+const CHOICES_SURFACE_ID_WITH_VARIABLES = [
+	{
+		type: 'checkbox',
+		label: 'Use variables for surface',
+		id: 'controller_from_variable',
+		default: false,
+	},
+	{
+		type: 'internal:surface_serial',
+		label: 'Surface / group',
+		id: 'controller',
+		default: 'self',
+		includeSelf: true,
+		useRawSurfaces: true,
+		isVisible: (options) => !options.controller_from_variable,
+	},
+	{
+		type: 'textinput',
+		label: 'Surface / group',
 		id: 'controller_variable',
 		default: 'self',
 		isVisible: (options) => !!options.controller_from_variable,
@@ -215,9 +237,9 @@ export default class Surface {
 	getActionDefinitions() {
 		return {
 			set_brightness: {
-				label: 'Surface: Set serialNumber to brightness',
+				label: 'Surface: Set to brightness',
 				options: [
-					...CHOICES_CONTROLLER_WITH_VARIABLES,
+					...CHOICES_SURFACE_ID_WITH_VARIABLES,
 
 					{
 						type: 'number',
@@ -233,8 +255,8 @@ export default class Surface {
 			},
 
 			set_page: {
-				label: 'Surface: Set serialNumber to page',
-				options: [...CHOICES_CONTROLLER_WITH_VARIABLES, ...CHOICES_PAGE_WITH_VARIABLES],
+				label: 'Surface: Set to page',
+				options: [...CHOICES_SURFACE_GROUP_WITH_VARIABLES, ...CHOICES_PAGE_WITH_VARIABLES],
 			},
 			set_page_byindex: {
 				label: 'Surface: Set by index to page',
@@ -255,20 +277,20 @@ export default class Surface {
 
 			inc_page: {
 				label: 'Surface: Increment page number',
-				options: [...CHOICES_CONTROLLER_WITH_VARIABLES],
+				options: [...CHOICES_SURFACE_GROUP_WITH_VARIABLES],
 			},
 			dec_page: {
 				label: 'Surface: Decrement page number',
-				options: [...CHOICES_CONTROLLER_WITH_VARIABLES],
+				options: [...CHOICES_SURFACE_GROUP_WITH_VARIABLES],
 			},
 
 			lockout_device: {
 				label: 'Surface: Lockout specified surface immediately.',
-				options: [...CHOICES_CONTROLLER_WITH_VARIABLES],
+				options: [...CHOICES_SURFACE_GROUP_WITH_VARIABLES],
 			},
 			unlockout_device: {
 				label: 'Surface: Unlock specified surface immediately.',
-				options: [...CHOICES_CONTROLLER_WITH_VARIABLES],
+				options: [...CHOICES_SURFACE_GROUP_WITH_VARIABLES],
 			},
 
 			lockout_all: {
@@ -346,7 +368,7 @@ export default class Surface {
 				}
 
 				setImmediate(() => {
-					this.#surfaceController.setDeviceLocked(theController, true, true)
+					this.#surfaceController.setSurfaceOrGroupLocked(theController, true, true)
 				})
 			}
 			return true
@@ -355,7 +377,7 @@ export default class Surface {
 			if (!theController) return true
 
 			setImmediate(() => {
-				this.#surfaceController.setDeviceLocked(theController, false, true)
+				this.#surfaceController.setSurfaceOrGroupLocked(theController, false, true)
 			})
 
 			return true
@@ -467,7 +489,7 @@ export default class Surface {
 				options: [
 					{
 						type: 'internal:surface_serial',
-						label: 'Surface / controller',
+						label: 'Surface / group',
 						id: 'controller',
 					},
 					{

commit 2e47ca3bffcd3e6e42a748eb1abb986bf4d415ec

fix: emulators list broken by #2655

lib/Surface/Controller.js
@@ -50,7 +50,7 @@ const SurfacesRoom = 'surfaces'
 class SurfaceController extends CoreBase {
 	/**
 	 * The last sent json object
-	 * @type {ClientDevicesListItem[] | null}
+	 * @type {Record<string,ClientDevicesListItem> | null}
 	 * @access private
 	 */
 	#lastSentJson = null
@@ -846,7 +846,12 @@ class SurfaceController extends CoreBase {
 	}
 
 	updateDevicesList() {
-		const newJson = cloneDeep(this.getDevicesList())
+		const newJsonArr = cloneDeep(this.getDevicesList())
+		/** @type {Record<string,ClientDevicesListItem>} */
+		const newJson = {}
+		for (const surface of newJsonArr) {
+			newJson[surface.id] = surface
+		}
 
 		if (this.io.countRoomMembers(SurfacesRoom) > 0) {
 			const patch = jsonPatch.compare(this.#lastSentJson || {}, newJson || {})

webui/src/Controls/InternalInstanceFields.jsx
@@ -192,14 +192,14 @@ function InternalSurfaceBySerialDropdown({ isOnControl, value, setValue, disable
 		}
 
 		if (!useRawSurfaces) {
-			for (const group of surfacesContext ?? []) {
+			for (const group of Object.values(surfacesContext ?? {})) {
 				choices.push({
 					label: group.displayName,
 					id: group.id,
 				})
 			}
 		} else {
-			for (const group of surfacesContext ?? []) {
+			for (const group of Object.values(surfacesContext ?? {})) {
 				for (const surface of group.surfaces) {
 					choices.push({
 						label: surface.displayName,

webui/src/Emulator/List.jsx
@@ -9,19 +9,19 @@ import { cloneDeep } from 'lodash-es'
 export function EmulatorList() {
 	const socket = useContext(SocketContext)
 
-	const [surfaces, setSurfaces] = useState(null)
+	const [surfaceGroups, setSurfaceGroups] = useState(null)
 	const [loadError, setLoadError] = useState(null)
 	const [reloadToken, setReloadToken] = useState(nanoid())
 
 	const doRetryLoad = useCallback(() => setReloadToken(nanoid()), [])
 
 	useEffect(() => {
-		setSurfaces(null)
+		setSurfaceGroups(null)
 		setLoadError(null)
 
 		socketEmitPromise(socket, 'surfaces:subscribe', [])
 			.then((surfaces) => {
-				setSurfaces(surfaces)
+				setSurfaceGroups(surfaces)
 			})
 			.catch((e) => {
 				console.error('Failed to load surfaces', e)
@@ -29,7 +29,7 @@ export function EmulatorList() {
 			})
 
 		const patchSurfaces = (patch) => {
-			setSurfaces((oldSurfaces) => {
+			setSurfaceGroups((oldSurfaces) => {
 				return jsonPatch.applyPatch(cloneDeep(oldSurfaces) || {}, patch).newDocument
 			})
 		}
@@ -43,14 +43,26 @@ export function EmulatorList() {
 	}, [socket, reloadToken])
 
 	const emulators = useMemo(() => {
-		return Object.values(surfaces?.available || {}).filter((s) => s.id.startsWith('emulator:'))
-	}, [surfaces])
+		const emulators = []
+
+		for (const group of Object.values(surfaceGroups ?? {})) {
+			if (!group) continue
+
+			for (const surface of group.surfaces) {
+				if (surface.integrationType === 'emulator' || surface.id.startsWith('emulator:')) {
+					emulators.push(surface)
+				}
+			}
+		}
+
+		return emulators
+	}, [surfaceGroups])
 
 	return (
 		<div className="page-emulator-list">
 			<CContainer fluid className="d-flex flex-column">
-				<LoadingRetryOrError error={loadError} dataReady={surfaces} doRetry={doRetryLoad} />
-				{surfaces && (
+				<LoadingRetryOrError error={loadError} dataReady={surfaceGroups} doRetry={doRetryLoad} />
+				{surfaceGroups && (
 					<CRow alignHorizontal="center">
 						<CCol sm={12}>
 							<p>&nbsp;</p>

webui/src/Surfaces/EditModal.jsx
@@ -31,8 +31,9 @@ export const SurfaceEditModal = forwardRef(function SurfaceEditModal(_props, ref
 
 	let surfaceInfo = null
 	if (surfaceId) {
-		for (const group of surfacesContext) {
+		for (const group of Object.values(surfacesContext)) {
 			if (surfaceInfo) break
+			if (!group) continue
 
 			for (const surface of group.surfaces) {
 				if (surface.id === surfaceId) {
@@ -49,8 +50,8 @@ export const SurfaceEditModal = forwardRef(function SurfaceEditModal(_props, ref
 	const groupId = surfaceInfo && !surfaceInfo.groupId ? surfaceId : rawGroupId
 	let groupInfo = null
 	if (groupId) {
-		for (const group of surfacesContext) {
-			if (group.id === groupId) {
+		for (const group of Object.values(surfacesContext)) {
+			if (group && group.id === groupId) {
 				groupInfo = group
 				break
 			}
@@ -114,7 +115,8 @@ export const SurfaceEditModal = forwardRef(function SurfaceEditModal(_props, ref
 		// If surface disappears/disconnects, hide this
 
 		const onlineSurfaceIds = new Set()
-		for (const group of surfacesContext) {
+		for (const group of Object.values(surfacesContext)) {
+			if (!group) continue
 			for (const surface of group.surfaces) {
 				if (surface.isConnected) {
 					onlineSurfaceIds.add(surface.id)
@@ -226,8 +228,8 @@ export const SurfaceEditModal = forwardRef(function SurfaceEditModal(_props, ref
 								>
 									<option value="null">Standalone (Default)</option>
 
-									{surfacesContext
-										.filter((group) => !group.isAutoGroup)
+									{Object.values(surfacesContext)
+										.filter((group) => group && !group.isAutoGroup)
 										.map((group) => (
 											<option key={group.id} value={group.id}>
 												{group.displayName}

webui/src/Surfaces/index.jsx
@@ -103,6 +103,8 @@ export const SurfacesPage = memo(function SurfacesPage() {
 		[socket]
 	)
 
+	const surfacesList = Object.values(surfacesContext).filter((grp) => !!grp)
+
 	return (
 		<div>
 			<GenericConfirmModal ref={confirmRef} />
@@ -154,7 +156,7 @@ export const SurfacesPage = memo(function SurfacesPage() {
 					</tr>
 				</thead>
 				<tbody>
-					{surfacesContext.map((group) =>
+					{surfacesList.map((group) =>
 						group.isAutoGroup && (group.surfaces || []).length === 1 ? (
 							<SurfaceRow
 								key={group.id}
@@ -179,7 +181,7 @@ export const SurfacesPage = memo(function SurfacesPage() {
 						)
 					)}
 
-					{surfacesContext.length === 0 && (
+					{surfacesList.length === 0 && (
 						<tr>
 							<td colSpan={7}>No control surfaces have been detected</td>
 						</tr>