b5866d845eba76d99a60c643bc827e6815e6fca7
Julian Waller <git@julusian.co.uk>
2024-01-17 18:55:36 +0000

    fix: http api documentation

--- webui/src/UserConfig/HttpProtocol.tsx
+++ webui/src/UserConfig/HttpProtocol.tsx
@@ -65,44 +65,60 @@ export function HttpProtocol() {
 				<br />
 
 				<li>
-					<code>/style/bank/</code>&lt;page&gt;<code>/</code>&lt;button&gt;
-					<code>?bgcolor=</code>&lt;bgcolor HEX&gt;
+					Change background color of button
 					<br />
-					<i>Change background color of button</i>
+					Method: <code>POST</code>
+					<br />
+					Path: <code>/api/location/</code>&lt;page&gt;<code>/</code>&lt;row&gt;<code>/</code>&lt;column&gt;
+					<code>/style?bgcolor=</code>&lt;bgcolor HEX&gt;
 				</li>
 				<li>
-					<code>/style/bank/</code>&lt;page&gt;<code>/</code>&lt;button&gt;
-					<code>?color=</code>&lt;color HEX&gt;
+					Change color of text on button
 					<br />
-					<i>Change color of text on button</i>
+					Method: <code>POST</code>
+					<br />
+					Path: <code>/api/location/</code>&lt;page&gt;<code>/</code>&lt;row&gt;<code>/</code>&lt;column&gt;
+					<code>/style?color=</code>&lt;color HEX&gt;
 				</li>
 				<li>
-					<code>/style/bank/</code>&lt;page&gt;<code>/</code>&lt;button&gt;
-					<code>?text=</code>&lt;text&gt;
+					Change text on a button
 					<br />
-					<i>Change text on a button</i>
+					Method: <code>POST</code>
+					<br />
+					Path: <code>/api/location/</code>&lt;page&gt;<code>/</code>&lt;row&gt;<code>/</code>&lt;column&gt;
+					<code>/style?text=</code>&lt;text&gt;
 				</li>
 				<li>
-					<code>/style/bank/</code>&lt;page&gt;<code>/</code>&lt;button&gt;
-					<code>?size=</code>&lt;text size&gt;
+					Change text size on a button (between the predefined values)
 					<br />
-					<i>Change text size on a button (between the predefined values)</i>
+					Method: <code>POST</code>
+					<br />
+					Path: <code>/api/location/</code>&lt;page&gt;<code>/</code>&lt;row&gt;<code>/</code>&lt;column&gt;
+					<code>/style?size=</code>&lt;text size&gt;
 				</li>
 
+				<br />
+
 				<li>
-					POST <code>/api/custom-variable/</code>&lt;name&gt;<code>/value?value=</code>&lt;value&gt;
+					Change custom variable value
 					<br />
-					<i>Change custom variable value</i>
+					Method: <code>POST</code>
+					<br />
+					Path: <code>/api/custom-variable/</code>&lt;name&gt;<code>/value?value=</code>&lt;value&gt;
 				</li>
 				<li>
-					POST <code>/api/custom-variable/</code>&lt;name&gt;<code>/value</code> <code>&lt;value&gt;</code>
+					Change custom variable value
 					<br />
-					<i>Change custom variable value</i>
+					Method: <code>POST</code>
+					<br />
+					Path: <code>/api/custom-variable/</code>&lt;name&gt;<code>/value</code> <code>&lt;value&gt;</code>
 				</li>
 				<li>
-					POST <code>/surfaces/rescan</code>
+					Make Companion rescan for newly attached USB surfaces
 					<br />
-					<i>Make Companion rescan for newly attached USB surfaces</i>
+					Method: <code>POST</code>
+					<br />
+					Path: <code>/api/surfaces/rescan</code>
 				</li>
 			</ul>
 
@@ -111,28 +127,29 @@ export function HttpProtocol() {
 			</p>
 
 			<p>
-				Press page 1 button 2
+				Press page 1 row 0 column 2
 				<br />
-				<code>/press/bank/1/2</code>
+				POST <code>/api/location/1/0/2/press</code>
 			</p>
 
 			<p>
-				Change the text of button 4 on page 2 to TEST
+				Change the text of row 0 column 4 on page 2 to TEST
 				<br />
-				<code>/style/bank/2/4/?text=TEST</code>
+				POST <code>/api/location/1/0/4/style?text=TEST`</code>
 			</p>
 
 			<p>
-				Change the text of button 4 on page 2 to TEST, background color to #ffffff, text color to #000000 and font size
-				to 28px
+				Change the text of row 1, column 4 on page 2 to TEST, background color to #ffffff, text color to #000000 and
+				font size to 28px
 				<br />
-				<code>/style/bank/2/4/?text=TEST&bgcolor=%23ffffff&color=%23000000&size=28px</code>
+				POST <code>/api/location/2/1/4/style</code> with body{' '}
+				<code>{'{ "text": "TEST", "bgcolor": "#ffffff", "color": "#000000", "size": 28 }'}</code>
 			</p>
 
 			<p>
-				Change custom variable &quot;cue&quot; to value &quot;intro&quot;
+				Change custom variable &quot;cue&quot; to value &quot;intro&quot
 				<br />
-				<code>/set/custom-variable/cue?value=intro</code>
+				POST <code>/api/custom-variable/cue/value?value=intro</code>
 			</p>
 
 			<p>
