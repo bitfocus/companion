e031c1dd99069b2c0497714aa13c2c9ebe9a3c8f
Jeff Martin <dist@dist.dev>
2024-05-24 14:56:05 +0100

    Feat: Time Expression Functions

--- shared-lib/lib/Expression/ExpressionFunctions.js
+++ shared-lib/lib/Expression/ExpressionFunctions.js
@@ -16,15 +16,6 @@ export const ExpressionFunctions = {
 	isNumber: (v) => !isNaN(v),
 	max: (...args) => Math.max(...args),
 	min: (...args) => Math.min(...args),
-	unixNow: () => Date.now(),
-	timestampToSeconds: (str) => {
-		const match = (str + '').match(/^(\d+)\:(\d+)\:(\d+)$/i)
-		if (match) {
-			return Number(match[1]) * 3600 + Number(match[2]) * 60 + Number(match[3])
-		} else {
-			return 0
-		}
-	},
 	randomInt: (min = 0, max = 10) => {
 		min = Number(min)
 		max = Number(max)
@@ -78,49 +69,6 @@ export const ExpressionFunctions = {
 		}
 		return Buffer.from('' + str).toString(enc)
 	},
-	secondsToTimestamp: (v, type) => {
-		v = Math.max(0, v)
-
-		const seconds = pad(Math.floor(v) % 60, '0', 2)
-		const minutes = pad(Math.floor(v / 60) % 60, '0', 2)
-		const hours = pad(Math.floor(v / 3600), '0', 2)
-
-		if (!type) return `${hours}:${minutes}:${seconds}`
-
-		const timestamp = []
-		if (type.includes('HH') || type.includes('hh')) timestamp.push(hours)
-		if (type.includes('mm')) timestamp.push(minutes)
-		if (type.includes('ss')) timestamp.push(seconds)
-
-		return timestamp.join(':')
-	},
-	msToTimestamp: (v, type) => {
-		v = Math.max(0, v)
-
-		const ms = v % 1000
-		const seconds = pad(Math.floor(v / 1000) % 60, '0', 2)
-		const minutes = pad(Math.floor(v / 60000) % 60, '0', 2)
-		const hours = pad(Math.floor(v / 3600000), '0', 2)
-
-		if (!type) return `${minutes}:${seconds}.${Math.floor(ms / 100)}`
-
-		const timestamp = []
-		if (type.includes('HH') || type.includes('hh')) timestamp.push(hours)
-		if (type.includes('mm')) timestamp.push(minutes)
-		if (type.includes('ss')) timestamp.push(seconds)
-
-		let timestampStr = timestamp.join(':')
-		if (type.endsWith('.ms') || type.endsWith('.S')) {
-			timestampStr += `.${Math.floor(ms / 100)}`
-		} else if (type.endsWith('.SS')) {
-			timestampStr += `.${pad(Math.floor(ms / 10), '0', 2)}`
-		} else if (type.endsWith('.SSS')) {
-			timestampStr += `.${pad(ms, '0', 3)}`
-		}
-
-		return timestampStr
-	},
-	// parseVariables is filled in from the caller
 
 	// Bool operations
 	bool: (v) => !!v && v !== 'false' && v !== '0',
@@ -152,7 +100,6 @@ export const ExpressionFunctions = {
 
 		return value
 	},
-
 	jsonparse: (str) => {
 		try {
 			return JSON.parse(str + '')
@@ -160,12 +107,139 @@ export const ExpressionFunctions = {
 			return null
 		}
 	},
-
 	jsonstringify: (obj) => {
 		try {
 			return JSON.stringify(obj)
 		} catch (_e) {
 			return null
 		}
+	},
+
+	// Time operations
+	unixNow: () => Date.now(),
+	timestampToSeconds: (str) => {
+		const match = (str + '').match(/^(\d+)\:(\d+)\:(\d+)$/i)
+		if (match) {
+			return Number(match[1]) * 3600 + Number(match[2]) * 60 + Number(match[3])
+		} else {
+			return 0
+		}
+	},
+	secondsToTimestamp: (v, type) => {
+		let negative = v < 0
+		v = Math.abs(v)
+
+		const seconds = pad(Math.floor(v) % 60, '0', 2)
+		const minutes = pad(Math.floor(v / 60) % 60, '0', 2)
+		const hours = pad(Math.floor(v / 3600), '0', 2)
+
+		if (!type) return `${negative ? '-' : ''}${hours}:${minutes}:${seconds}`
+
+		const timestamp = []
+		if (type.includes('HH') || type.includes('hh')) timestamp.push(hours)
+		if (type.includes('mm')) timestamp.push(minutes)
+		if (type.includes('ss')) timestamp.push(seconds)
+
+		return (negative ? '-' : '') + timestamp.join(':')
+	},
+	msToTimestamp: (v, type) => {
+		v = Math.max(0, v)
+
+		const ms = v % 1000
+		const seconds = pad(Math.floor(v / 1000) % 60, '0', 2)
+		const minutes = pad(Math.floor(v / 60000) % 60, '0', 2)
+		const hours = pad(Math.floor(v / 3600000), '0', 2)
+
+		if (!type) return `${minutes}:${seconds}.${Math.floor(ms / 100)}`
+
+		const timestamp = []
+		if (type.includes('HH') || type.includes('hh')) timestamp.push(hours)
+		if (type.includes('mm')) timestamp.push(minutes)
+		if (type.includes('ss')) timestamp.push(seconds)
+
+		let timestampStr = timestamp.join(':')
+		if (type.endsWith('.ms') || type.endsWith('.S')) {
+			timestampStr += `.${Math.floor(ms / 100)}`
+		} else if (type.endsWith('.SS')) {
+			timestampStr += `.${pad(Math.floor(ms / 10), '0', 2)}`
+		} else if (type.endsWith('.SSS')) {
+			timestampStr += `.${pad(ms, '0', 3)}`
+		}
+
+		return timestampStr
+	},
+	timeOffset: (time, offset, hr12 = false) => {
+		const date = new Date()
+
+		date.setHours(time.split(':')[0])
+		date.setMinutes(time.split(':')[1])
+		date.setSeconds(time.split(':')[2] || 0)
+
+		let diff = offset
+
+		if (typeof offset === 'string') {
+			let hours = 0
+			let minutes = 0
+			let seconds = 0
+			let negative = diff.startsWith('-')
+
+			if (diff.startsWith('+') || diff.startsWith('-')) {
+				diff = diff.substr(1)
+			}
+
+			if (offset.includes(':')) {
+				const split = diff.split(':')
+				hours = parseInt(split[0]) || 0
+				minutes = parseInt(split[1]) || 0
+				seconds = parseInt(split[2]) || 0
+			} else {
+				hours = parseInt(diff) || 0
+			}
+
+			date.setHours(date.getHours() + (negative ? -hours : hours))
+			date.setMinutes(date.getMinutes() + (negative ? -minutes : minutes))
+			date.setSeconds(date.getSeconds() + (negative ? -seconds : seconds))
+		} else {
+			date.setHours(date.getHours() + diff)
+		}
+
+		const displayHours = pad(date.getHours() > 12 && hr12 ? date.getHours() - 12 : date.getHours(), '0', 2)
+		const displayMinutes = pad(date.getMinutes(), '0', 2)
+		const displaySeconds = pad(date.getSeconds(), '0', 2)
+
+		if (time.split(':').length === 2) {
+			return `${displayHours}:${displayMinutes}`
+		} else if (time.split(':').length === 3) {
+			return `${displayHours}:${displayMinutes}:${displaySeconds}`
+		} else {
+			return ''
+		}
+	},
+	timeDiff: (from, to) => {
+		let diff = 0
+		let fromDate = new Date()
+		let toDate = new Date()
+
+		if (from.includes('T')) {
+			fromDate = new Date(from)
+		} else {
+			fromDate.setHours(from.split(':')[0])
+			fromDate.setMinutes(from.split(':')[1])
+			fromDate.setSeconds(from.split(':')[2] || 0)
+		}
+
+		if (to.includes('T')) {
+			toDate = new Date(to)
+		} else {
+			toDate.setHours(to.split(':')[0])
+			toDate.setMinutes(to.split(':')[1])
+			toDate.setSeconds(to.split(':')[2] || 0)
+		}
+
+		diff = toDate.getTime() - fromDate.getTime()
+
+		if (isNaN(diff)) return 'ERR'
+
+		return Math.round(diff / 1000)
 	},
 }
