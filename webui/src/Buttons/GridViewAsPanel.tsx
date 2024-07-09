d86ef71ee7b2e256d1937e8ef51765a4b502a241
Julian Waller <git@julusian.co.uk>
2024-07-09 13:46:12 +0100

    wip: page layout

--- webui/src/Buttons/GridViewAsPanel.tsx
+++ webui/src/Buttons/GridViewAsPanel.tsx
@@ -1,6 +1,6 @@
 import React, { useContext } from 'react'
-import { CAlert } from '@coreui/react'
-import { useComputed } from '../util.js'
+import { CAlert, CCol, CForm, CFormLabel } from '@coreui/react'
+import { PreventDefaultHandler, useComputed } from '../util.js'
 import { RootAppStoreContext } from '../Stores/RootAppStore.js'
 import { observer } from 'mobx-react-lite'
 import { GridViewAsController, GridViewSpecialSurface } from './GridViewAs.js'
@@ -36,41 +36,59 @@ export const GridViewAsPanel = observer(function GridViewAsPanel({ gridViewAsCon
 			<h5>View Grid As</h5>
 			<p>Here you can change how the grid is displayed, to view it as a particular surface.</p>
 
-			<DropdownInputField
-				label="Surface"
-				choices={gridViewAsController.surfaceChoices}
-				multiple={false}
-				value={gridViewAsController.selectedSurface.id}
-				setValue={(value) => gridViewAsController.setSelectedSurface(value as GridViewSpecialSurface | string)}
-			/>
+			<CForm className="row g-3" onSubmit={PreventDefaultHandler}>
+				<CFormLabel className="col-sm-2 col-form-label col-form-label-sm">Surface</CFormLabel>
+				<CCol sm={9}>
+					<DropdownInputField
+						choices={gridViewAsController.surfaceChoices}
+						multiple={false}
+						value={gridViewAsController.selectedSurface.id}
+						setValue={(value) => gridViewAsController.setSelectedSurface(value as GridViewSpecialSurface | string)}
+					/>
+				</CCol>
+				<CCol sm={1}></CCol>
 
-			{gridViewAsController.selectedSurface.id !== GridViewSpecialSurface.None &&
-				!gridViewAsController.selectedSurface.layout && (
-					<CAlert color="warning">The layout of this surface is not known, the full grid will be shown instead</CAlert>
-				)}
+				{gridViewAsController.selectedSurface.id !== GridViewSpecialSurface.None &&
+					!gridViewAsController.selectedSurface.layout && (
+						<CCol sm={12}>
+							<CAlert color="warning">
+								The layout of this surface is not known, the full grid will be shown instead
+							</CAlert>
+						</CCol>
+					)}
 
-			<DropdownInputField
-				label="Surface Type"
-				choices={surfaceTypeChoices}
-				multiple={false}
-				disabled={gridViewAsController.selectedSurface.id !== GridViewSpecialSurface.Custom}
-				value={gridViewAsController.selectedSurface.type}
-				setValue={(value) => gridViewAsController.setCustomType(value as string)}
-			/>
+				<CFormLabel className="col-sm-2 col-form-label col-form-label-sm">Surface Type</CFormLabel>
+				<CCol sm={9}>
+					<DropdownInputField
+						choices={surfaceTypeChoices}
+						multiple={false}
+						disabled={gridViewAsController.selectedSurface.id !== GridViewSpecialSurface.Custom}
+						value={gridViewAsController.selectedSurface.type}
+						setValue={(value) => gridViewAsController.setCustomType(value as string)}
+					/>
+				</CCol>
+				<CCol sm={1}></CCol>
 
-			<NumberInputField
-				label="X Offset"
-				value={gridViewAsController.selectedSurface.xOffset}
-				disabled={gridViewAsController.selectedSurface.id !== GridViewSpecialSurface.Custom}
-				setValue={gridViewAsController.setCustomXOffset}
-			/>
+				<CFormLabel className="col-sm-2 col-form-label col-form-label-sm">X Offset</CFormLabel>
+				<CCol sm={9}>
+					<NumberInputField
+						value={gridViewAsController.selectedSurface.xOffset}
+						disabled={gridViewAsController.selectedSurface.id !== GridViewSpecialSurface.Custom}
+						setValue={gridViewAsController.setCustomXOffset}
+					/>
+				</CCol>
+				<CCol sm={1}></CCol>
 
-			<NumberInputField
-				label="Y Offset"
-				value={gridViewAsController.selectedSurface.yOffset}
-				disabled={gridViewAsController.selectedSurface.id !== GridViewSpecialSurface.Custom}
-				setValue={gridViewAsController.setCustomYOffset}
-			/>
+				<CFormLabel className="col-sm-2 col-form-label col-form-label-sm">Y Offset</CFormLabel>
+				<CCol sm={9}>
+					<NumberInputField
+						value={gridViewAsController.selectedSurface.yOffset}
+						disabled={gridViewAsController.selectedSurface.id !== GridViewSpecialSurface.Custom}
+						setValue={gridViewAsController.setCustomYOffset}
+					/>
+				</CCol>
+				<CCol sm={1}></CCol>
+			</CForm>
 		</div>
 	)
 })
