dda1f2fc8b16f80c2292b83231e30cfeadda9f9f
Julian Waller <git@julusian.co.uk>
2024-10-04 21:55:56 +0100

    fix: add korean font character sets #3031

--- companion/lib/Graphics/Controller.ts
+++ companion/lib/Graphics/Controller.ts
@@ -220,6 +220,7 @@ export class GraphicsController extends CoreBase<GraphicsControllerEvents> {
 		GlobalFonts.registerFromPath(generateFontUrl('NotoSansLinearB-Regular.ttf'), 'Companion-symbols6')
 		GlobalFonts.registerFromPath(generateFontUrl('NotoSansGurmukhi-Regular.ttf'), 'Companion-gurmukhi')
 		GlobalFonts.registerFromPath(generateFontUrl('NotoSansSC-Regular.ttf'), 'Companion-simplified-chinese')
+		GlobalFonts.registerFromPath(generateFontUrl('NotoSansKR-Regular.ttf'), 'Companion-korean')
 		GlobalFonts.registerFromPath(generateFontUrl('NotoColorEmoji-compat.ttf'), 'Companion-emoji')
 		GlobalFonts.registerFromPath(generateFontUrl('pf_tempesta_seven.ttf'), '5x7')
 
--- companion/lib/Graphics/Image.ts
+++ companion/lib/Graphics/Image.ts
@@ -20,8 +20,19 @@ import LogController from '../Log/Controller.js'
 import { PNG } from 'pngjs'
 import type { HorizontalAlignment, VerticalAlignment } from '../Resources/Util.js'
 
-const DEFAULT_FONTS =
-	'Companion-sans, Companion-symbols1, Companion-symbols2, Companion-symbols3, Companion-symbols4, Companion-symbols5, Companion-symbols6, Companion-gurmukhi, Companion-simplified-chinese, Companion-emoji'
+const DEFAULT_FONTS = [
+	'Companion-sans',
+	'Companion-symbols1',
+	'Companion-symbols2',
+	'Companion-symbols3',
+	'Companion-symbols4',
+	'Companion-symbols5',
+	'Companion-symbols6',
+	'Companion-gurmukhi',
+	'Companion-simplified-chinese',
+	'Companion-korean',
+	'Companion-emoji',
+].join(', ')
 
 async function pngParse(pngData: string | Buffer): Promise<PNG> {
 	return new Promise((resolve, reject) => {
