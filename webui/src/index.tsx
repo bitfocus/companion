1b77e7ddf4fb80acac190ad3d2455c8e3cdf33a1
Julian Waller <git@julusian.co.uk>
2024-11-16 00:03:49 +0000

    fix: checkbox indeterminate styling

--- webui/src/index.tsx
+++ webui/src/index.tsx
@@ -5,7 +5,8 @@ import '@fontsource/fira-code'
 import './App.scss'
 import './Resources/Constants.js'
 import alignmentImg from '~/scss/img/alignment.png'
-import checkImg from '~/scss/img/check.png'
+import checkImg from '~/scss/img/check.svg?no-inline'
+import indeterminateImg from '~/scss/img/indeterminate.svg?no-inline' // This doesn't embed if being inlined
 
 if (process.env.NODE_ENV === 'development') {
 	const defineProperties = Object.defineProperties
@@ -63,6 +64,7 @@ declare module '@tanstack/react-router' {
 // This is not nice, but we need to load these images not by a url, but as a data url
 document.body.style.setProperty('--companion-img-alignment', `url(${alignmentImg})`)
 document.body.style.setProperty('--companion-img-check', `url(${checkImg})`)
+document.body.style.setProperty('--companion-img-indeterminate', `url(${indeterminateImg})`)
 
 const rootElm = document.getElementById('root')!
 const root = createRoot(rootElm)
