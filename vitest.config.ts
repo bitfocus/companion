895ea1e7bbf5cf3815f8da308cc62f28ffdc5ccd
Julian Waller <git@julusian.co.uk>
2025-01-31 20:32:28 +0000

    fix: isVisibleFn failing when data is passed

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,6 +2,13 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
 	test: {
-		exclude: ['**/module-local-dev/**', '**/node_modules/**', '**/dist/**', '**/build/**', '**/coverage/**'],
+		exclude: [
+			'**/module-local-dev/**',
+			'**/node_modules/**',
+			'**/dist/**',
+			'**/build/**',
+			'**/coverage/**',
+			'**/webui/**',
+		],
 	},
 })
