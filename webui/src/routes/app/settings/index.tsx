97b05210abfff95c7233b91a3d3f0aeb61dfb168
Julian Waller <git@julusian.co.uk>
2025-02-03 22:53:35 +0000

    chore: fix format

--- webui/src/routes/app/settings/index.tsx
+++ webui/src/routes/app/settings/index.tsx
@@ -2,5 +2,5 @@ import { createFileRoute } from '@tanstack/react-router'
 import { SettingsSelectPage } from '../../../UserConfig/index.js'
 
 export const Route = createFileRoute('/_app/settings/')({
-  component: SettingsSelectPage,
+	component: SettingsSelectPage,
 })
