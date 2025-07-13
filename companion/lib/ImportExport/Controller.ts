63ec1c6cafd246f8d1cfd52a999faef138131c22
Julian Waller <git@julusian.co.uk>
2025-07-13 21:03:20 +0100

    fix: imports

--- companion/lib/ImportExport/Controller.ts
+++ companion/lib/ImportExport/Controller.ts
@@ -54,7 +54,7 @@ import { MultipartUploader } from '../Resources/MultipartUploader.js'
 import { publicProcedure, router, toIterable } from '../UI/TRPC.js'
 import { zodLocation } from '../Graphics/Preview.js'
 import z from 'zod'
-import { EventEmitter } from 'node:stream'
+import { EventEmitter } from 'node:events'
 
 const MAX_IMPORT_FILE_SIZE = 1024 * 1024 * 500 // 500MB. This is small enough that it can be kept in memory
 
