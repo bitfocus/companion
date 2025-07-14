9f10b2d7bcf4bdc63fde5f9348d0ebe43fabeb42
Julian Waller <git@julusian.co.uk>
2025-07-14 23:33:22 +0100

    feat: expression-streaming for client

--- companion/lib/Preview/Controller.ts
+++ companion/lib/Preview/Controller.ts
@@ -3,14 +3,14 @@ import type { GraphicsController } from '../Graphics/Controller.js'
 import type { IPageStore } from '../Page/Store.js'
 import type { VariablesValues } from '../Variables/Values.js'
 import type { InstanceDefinitions } from '../Instance/Definitions.js'
-// import { PreviewExpressionStream } from './ExpressionStream.js'
+import { PreviewExpressionStream } from './ExpressionStream.js'
 import { PreviewGraphics } from './Graphics.js'
 import { PreviewPresets } from './Presets.js'
 import { router } from '../UI/TRPC.js'
 
 export class PreviewController {
 	readonly #graphics: PreviewGraphics
-	// readonly #expressionStream: PreviewExpressionStream
+	readonly #expressionStream: PreviewExpressionStream
 	readonly #presets: PreviewPresets
 
 	constructor(
@@ -21,14 +21,14 @@ export class PreviewController {
 		instanceDefinitions: InstanceDefinitions
 	) {
 		this.#graphics = new PreviewGraphics(graphicsController, pageStore, controlsController)
-		// this.#expressionStream = new PreviewExpressionStream(pageStore, variablesValuesController, controlsController)
+		this.#expressionStream = new PreviewExpressionStream(pageStore, controlsController)
 		this.#presets = new PreviewPresets(graphicsController, variablesValuesController, instanceDefinitions)
 	}
 
 	createTrpcRouter() {
 		return router({
 			graphics: this.#graphics.createTrpcRouter(),
-			// expressionStream: this.#expressionStream.createTrpcRouter(),
+			expressionStream: this.#expressionStream.createTrpcRouter(),
 			presets: this.#presets.createTrpcRouter(),
 		})
 	}
@@ -39,5 +39,6 @@ export class PreviewController {
 
 	onVariablesChanged(allChangedSet: Set<string>, fromControlId: string | null): void {
 		this.#graphics.onVariablesChanged(allChangedSet, fromControlId)
+		this.#expressionStream.onVariablesChanged(allChangedSet, fromControlId)
 	}
 }
