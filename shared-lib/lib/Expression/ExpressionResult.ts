7bd3809408abe660a1cc2a2efd5dd3e050a9dd3c
Julian Waller <git@julusian.co.uk>
2025-07-15 00:11:14 +0100

    chore: fix lint

---
+++ shared-lib/lib/Expression/ExpressionResult.ts
@@ -0,0 +1,28 @@
+export type ExecuteExpressionResult = ExecuteExpressionResultOk | ExecuteExpressionResultError
+export interface ExecuteExpressionResultOk {
+	ok: true
+	value: boolean | number | string | undefined
+	variableIds: ReadonlySet<string>
+}
+export interface ExecuteExpressionResultError {
+	ok: false
+	error: string
+	variableIds: ReadonlySet<string>
+}
+
+export type ExpressionStreamResult = ExpressionStreamResultOk | ExpressionStreamResultError
+export interface ExpressionStreamResultOk {
+	ok: true
+	value: boolean | number | string | undefined
+	// variableIds: Set<string>
+}
+export interface ExpressionStreamResultError {
+	ok: false
+	error: string
+	// variableIds: Set<string>
+}
+
+export interface ParseVariablesResult {
+	text: string
+	variableIds: ReadonlySet<string>
+}
