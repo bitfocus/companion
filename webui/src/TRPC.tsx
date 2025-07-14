cebcca1c2299180c1f0d90b6d112120b56e86776
Julian Waller <git@julusian.co.uk>
2025-07-14 21:02:01 +0100

    fix: reduce properties on useMutationExt

--- webui/src/TRPC.tsx
+++ webui/src/TRPC.tsx
@@ -38,20 +38,19 @@ export const trpc = createTRPCOptionsProxy<AppRouter>({
  * A wrapper around `useMutation` that memoizes the returned values.
  * Without this it is very hard to use this in a useMemo or similar hook, due to the `useMutation` returning a new object on every render,
  * and eslint complaining about the dependency unless the mutation object is not destructured.
+ * Note: this is intenionally minimal values so that it doesnt invalidate when the mutation is used
  */
 export function useMutationExt<TData = unknown, TError = DefaultError, TVariables = void, TContext = unknown>(
 	options: UseMutationOptions<TData, TError, TVariables, TContext>,
 	queryClient?: QueryClient
-): Pick<UseMutationResult<TData, TError, TVariables, TContext>, 'mutateAsync' | 'mutate' | 'isPending' | 'error'> {
+): Pick<UseMutationResult<TData, TError, TVariables, TContext>, 'mutateAsync' | 'mutate'> {
 	const rawUseMutation = useMutation(options, queryClient)
 
 	return useMemo(
 		() => ({
 			mutateAsync: rawUseMutation.mutateAsync,
 			mutate: rawUseMutation.mutate,
-			isPending: rawUseMutation.isPending,
-			error: rawUseMutation.error,
 		}),
-		[rawUseMutation.mutateAsync, rawUseMutation.mutate, rawUseMutation.isPending, rawUseMutation.error]
+		[rawUseMutation.mutateAsync, rawUseMutation.mutate]
 	)
 }
