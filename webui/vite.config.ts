5ceac464e64702d77f7c094fb41529848c0610bd
Julian Waller <git@julusian.co.uk>
2025-07-17 20:58:40 +0100

    fix: redirect `/connections/instance` to `/isntance` to avoid breaking some links from module config
    This broke when the router was introduced for the connection config editor

--- webui/vite.config.ts
+++ webui/vite.config.ts
@@ -48,6 +48,14 @@ export default defineConfig({
 	},
 	server: {
 		proxy: {
+			[`${normalizedBase}/instance`]: {
+				target: `http://${upstreamUrl}`,
+				rewrite: (path) => path.slice(normalizedBase.length),
+			},
+			[`${normalizedBase}/connections/instance`]: {
+				target: `http://${upstreamUrl}`,
+				rewrite: (path) => path.slice(normalizedBase.length),
+			},
 			[`${normalizedBase}/int`]: {
 				target: `http://${upstreamUrl}`,
 				rewrite: (path) => path.slice(normalizedBase.length),
