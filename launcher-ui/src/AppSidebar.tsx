aee32b048d3cde9e4f7e4359c2c207cec59c881a
Julian Waller <git@julusian.co.uk>
2025-08-07 23:29:54 +0100

    wip

---
+++ launcher-ui/src/AppSidebar.tsx
@@ -0,0 +1,42 @@
+import { CogIcon } from 'lucide-react'
+import React from 'react'
+import {
+	Sidebar,
+	SidebarContent,
+	SidebarGroup,
+	SidebarGroupContent,
+	SidebarHeader,
+	SidebarMenu,
+	SidebarMenuButton,
+	SidebarMenuItem,
+} from '~/components/ui/sidebar.js'
+import { SectionDefinitions } from './Sections'
+
+export function AppSidebar(): JSX.Element {
+	return (
+		<Sidebar>
+			<SidebarHeader>
+				<span className="flex flex-row gap-2">
+					<CogIcon className="h-6 w-6" />
+					Advanced Settings
+				</span>
+			</SidebarHeader>
+			<SidebarContent>
+				<SidebarGroup>
+					{/* <SidebarGroupLabel>{item.title}</SidebarGroupLabel> */}
+					<SidebarGroupContent>
+						<SidebarMenu>
+							{SectionDefinitions.map((item) => (
+								<SidebarMenuItem key={item.id}>
+									<SidebarMenuButton asChild /*isActive={item.isActive}*/>
+										<a href={`#${item.id}`}>{item.title}</a>
+									</SidebarMenuButton>
+								</SidebarMenuItem>
+							))}
+						</SidebarMenu>
+					</SidebarGroupContent>
+				</SidebarGroup>
+			</SidebarContent>
+		</Sidebar>
+	)
+}
