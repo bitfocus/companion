bd84bd3d9336ef97d56635696ba6fe3d77ef9e92
Julian Waller <git@julusian.co.uk>
2025-09-28 17:08:47 +0100

    wip: ci

--- playwright/tests/example.spec.ts
+++ playwright/tests/example.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test'
 
 test('has title', async ({ page }) => {
-	await page.goto('https://playwright.dev/')
+	await page.goto('http://bs-local.com:8000/')
 
 	// Expect a title "to contain" a substring.
 	await expect(page).toHaveTitle(/Playwright/)
