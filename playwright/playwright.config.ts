94d85604a50fac2484fea59fb3157b674e37d194
Julian Waller <git@julusian.co.uk>
2025-09-28 16:14:02 +0100

    wip: poc

---
+++ playwright/playwright.config.ts
@@ -0,0 +1,35 @@
+import { defineConfig } from '@playwright/test'
+
+// This is a sample config for what users might be running locally
+export default defineConfig({
+	testDir: './tests',
+
+	/* Fail the build on CI if you accidentally left test.only in the source code. */
+	forbidOnly: !!process.env.CI,
+	/* Retry on CI only */
+	retries: process.env.CI ? 2 : 0,
+	/* Opt out of parallel tests on CI. */
+	workers: process.env.CI ? 1 : undefined,
+
+	/* Maximum time one test can run for. */
+	timeout: 90 * 1000,
+	expect: {
+		/**
+		 * Maximum time expect() should wait for the condition to be met.
+		 * For example in `await expect(locator).toHaveText();`
+		 */
+		timeout: 5000,
+	},
+	/* Reporter to use. See https://playwright.dev/docs/test-reporters */
+	reporter: 'html',
+	/* Configure projects for major browsers */
+	projects: [
+		{
+			name: 'chrome',
+			use: {
+				browserName: 'chromium',
+				channel: 'chrome',
+			},
+		},
+	],
+})
