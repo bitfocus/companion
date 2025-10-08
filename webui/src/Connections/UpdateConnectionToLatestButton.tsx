55f6d5d915aefadbe3598dd64194964ef688ff1c
Julian Waller <git@julusian.co.uk>
2025-10-08 20:30:04 +0100

    fix: ensure invalid version doesnt crash connections page

--- webui/src/Connections/UpdateConnectionToLatestButton.tsx
+++ webui/src/Connections/UpdateConnectionToLatestButton.tsx
@@ -31,32 +31,36 @@ const UpdateConnectionToLatestButtonInner = observer(function ModuleVersionInfoI
 
 	let message: string | undefined
 
-	if (upgradeToVersions.length > 0 && connection.updatePolicy !== ConnectionUpdatePolicy.Manual) {
-		message = 'A replacement for this module is available'
-	} else {
-		const latestStableVersion = getLatestVersion(moduleStoreInfo?.versions, false)
-		const latestBetaVersion = getLatestVersion(moduleStoreInfo?.versions, true)
+	try {
+		if (upgradeToVersions.length > 0 && connection.updatePolicy !== ConnectionUpdatePolicy.Manual) {
+			message = 'A replacement for this module is available'
+		} else {
+			const latestStableVersion = getLatestVersion(moduleStoreInfo?.versions, false)
+			const latestBetaVersion = getLatestVersion(moduleStoreInfo?.versions, true)
 
-		let latestVersion: string | null = connection.moduleVersionId
+			let latestVersion: string | null = connection.moduleVersionId
 
-		// Use the latest stable if newer than the current version, for both modes
-		if (latestStableVersion && (!latestVersion || semver.gt(latestStableVersion.id, latestVersion))) {
-			latestVersion = latestStableVersion.id
+			// Use the latest stable if newer than the current version, for both modes
+			if (latestStableVersion && (!latestVersion || semver.gt(latestStableVersion.id, latestVersion))) {
+				latestVersion = latestStableVersion.id
+			}
+
+			// If update policy allows beta versions, and there is a newer beta version, use that
+			if (
+				connection.updatePolicy === ConnectionUpdatePolicy.Beta &&
+				latestBetaVersion &&
+				(!latestVersion || semver.gt(latestBetaVersion.id, latestVersion))
+			) {
+				latestVersion = latestBetaVersion.id
+			}
+
+			// If no match was found, or it matched the current version, hide the icon
+			if (latestVersion && latestVersion !== connection.moduleVersionId) {
+				message = `Module version v${latestVersion} is available`
+			}
 		}
-
-		// If update policy allows beta versions, and there is a newer beta version, use that
-		if (
-			connection.updatePolicy === ConnectionUpdatePolicy.Beta &&
-			latestBetaVersion &&
-			(!latestVersion || semver.gt(latestBetaVersion.id, latestVersion))
-		) {
-			latestVersion = latestBetaVersion.id
-		}
-
-		// If no match was found, or it matched the current version, hide the icon
-		if (latestVersion && latestVersion !== connection.moduleVersionId) {
-			message = `Module version v${latestVersion} is available`
-		}
+	} catch (_e) {
+		// Ignore invalid
 	}
 
 	if (!message) return null
