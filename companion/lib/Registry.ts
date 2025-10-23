766f1dc1cee3ec96074aecb8205be599597aa10d
Julian Waller <git@julusian.co.uk>
2025-10-23 20:52:42 +0100

    feat: initial surface modules implementation

--- companion/lib/Registry.ts
+++ companion/lib/Registry.ts
@@ -225,6 +225,7 @@ export class Registry {
 				this.controls,
 				this.graphics,
 				this.variables,
+				this.surfaces,
 				oscSender
 			)
 			this.ui.express.connectionApiRouter = this.instance.connectionApiRouter
