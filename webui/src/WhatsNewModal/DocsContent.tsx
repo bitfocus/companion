754b32e12e4919555f872c8cc407bf96a66a216d
Julian Waller <git@julusian.co.uk>
2025-10-30 18:17:01 +0000

    wip

--- webui/src/WhatsNewModal/DocsContent.tsx
+++ webui/src/WhatsNewModal/DocsContent.tsx
@@ -1,33 +1,55 @@
 import React from 'react'
 import ReactMarkdown, { defaultUrlTransform } from 'react-markdown'
 import remarkGfm from 'remark-gfm'
+import { useQuery } from '@tanstack/react-query'
+import { CButton } from '@coreui/react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faRefresh } from '@fortawesome/free-solid-svg-icons'
 import { makeAbsolutePath } from '~/Resources/util.js'
-import type { WhatsNewContent } from './content'
 
 interface DocsContentProps {
-	file: WhatsNewContent
+	file: string
 }
 
 export function DocsContent({ file }: DocsContentProps): React.JSX.Element {
 	// strip filename
+	const baseUrl = `${file}`.replace(/\/[^/]+$/, '/')
 
-	const data = file.default
-	const baseUrl = '123'
+	const { isPending, error, data, refetch } = useQuery<string>({
+		queryKey: [`docs_${file}`],
+		queryFn: async () => fetch(makeAbsolutePath(`/docs/${file}`)).then(async (res) => res.text()),
+		retry: false,
+	})
 
 	return (
 		<div>
-			<ReactMarkdown
-				urlTransform={(src, key, _node) => {
-					if (key === 'src' || (key === 'href' && !src.startsWith('http') && !src.startsWith('#'))) {
-						// img tag
-						return makeAbsolutePath(`/docs/${baseUrl}${defaultUrlTransform(src)}`)
-					} else {
-						return defaultUrlTransform(src)
-					}
-				}}
-				children={data}
-				remarkPlugins={[remarkGfm]}
-			/>
+			{isPending && 'loading'}
+			{error && <div>Error: {error.message}</div>}
+			{data && (
+				<>
+					{process.env.NODE_ENV !== 'production' && (
+						<CButton
+							title="Dev Refresh"
+							onClick={() => void refetch()}
+							style={{ float: 'right', position: 'absolute', top: -40, right: 0 }}
+						>
+							<FontAwesomeIcon icon={faRefresh} />
+						</CButton>
+					)}
+					<ReactMarkdown
+						urlTransform={(src, key, _node) => {
+							if (key === 'src' || (key === 'href' && !src.startsWith('http') && !src.startsWith('#'))) {
+								// img tag
+								return makeAbsolutePath(`/docs/${baseUrl}${defaultUrlTransform(src)}`)
+							} else {
+								return defaultUrlTransform(src)
+							}
+						}}
+						children={data}
+						remarkPlugins={[remarkGfm]}
+					/>
+				</>
+			)}
 		</div>
 	)
 }
--- webui/src/WhatsNewModal/content.tsx
+++ webui/src/WhatsNewModal/content.tsx
@@ -1,31 +1,28 @@
-// eslint-disable-next-line @typescript-eslint/consistent-type-imports
-export type WhatsNewContent = typeof import('*?raw')
-
 export interface WhatsNewPage {
 	version: string
 	label: string
-	file: WhatsNewContent
+	file: string
 }
 
 export const Pages: WhatsNewPage[] = [
 	{
 		version: '4.1.0',
 		label: 'v4.1.0',
-		file: await import('../../../docs/docs/9_whatsnew/v4-1-0.md?raw'),
+		file: 'v4-1-0',
 	},
 	{
 		version: '4.0.0',
 		label: 'v4.0.0',
-		file: await import('../../../docs/docs/9_whatsnew/v4-0-0.md?raw'),
+		file: 'v4-0-0',
 	},
 	{
 		version: '3.5.0',
 		label: 'v3.5.0',
-		file: await import('../../../docs/docs/9_whatsnew/v3-5-0.md?raw'),
+		file: 'v3-5-0',
 	},
 	{
 		version: '3.4.0',
 		label: 'v3.4.0',
-		file: await import('../../../docs/docs/9_whatsnew/v3-4-0.md?raw'),
+		file: 'v3-4-0',
 	},
 ]
