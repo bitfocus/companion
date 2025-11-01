2095b66f2cb3fbecdfc7e3b1423ae6f92cc5f27a
Julian Waller <git@julusian.co.uk>
2025-11-01 19:04:54 +0000

    feat: update sidebar notifications for surfaces

--- webui/src/Surfaces/TabNotifyIcon.tsx
+++ webui/src/Surfaces/TabNotifyIcon.tsx
@@ -5,6 +5,26 @@ import { useMissingVersionsCount } from '~/Instances/MissingVersionsWarning'
 import { RootAppStoreContext } from '~/Stores/RootAppStore.js'
 
 export const SurfacesTabNotifyIcon = observer(function SurfacesTabNotifyIcon(): JSX.Element | null {
+	const { surfaces, surfaceInstances } = useContext(RootAppStoreContext)
+
+	const updateCount = surfaces.countFirmwareUpdates()
+	const missingCount = useMissingVersionsCount(ModuleInstanceType.Surface, surfaceInstances.instances)
+
+	if (updateCount === 0 && missingCount === 0) return null
+
+	const lines = [
+		updateCount > 0 ? `${updateCount} surfaces have firmware updates available` : null,
+		missingCount > 0 ? `Missing ${missingCount} needed modules` : null,
+	].filter(Boolean)
+
+	return (
+		<span className="notification-count" title={lines.join(', ')}>
+			{updateCount + missingCount}
+		</span>
+	)
+})
+
+export const SurfacesConfiguredTabNotifyIcon = observer(function SurfacesConfiguredTabNotifyIcon(): JSX.Element | null {
 	const { surfaces } = useContext(RootAppStoreContext)
 
 	const updateCount = surfaces.countFirmwareUpdates()
@@ -13,6 +33,19 @@ export const SurfacesTabNotifyIcon = observer(function SurfacesTabNotifyIcon(): 
 	return (
 		<span className="notification-count" title={`${updateCount} surfaces have firmware updates available`}>
 			{updateCount}
+		</span>
+	)
+})
+
+export const SurfacesInstancesTabNotifyIcon = observer(function SurfacesInstancesTabNotifyIcon(): JSX.Element | null {
+	const { surfaceInstances } = useContext(RootAppStoreContext)
+
+	const missingCount = useMissingVersionsCount(ModuleInstanceType.Surface, surfaceInstances.instances)
+	if (missingCount === 0) return null
+
+	return (
+		<span className="notification-count" title={`Missing ${missingCount} needed modules`}>
+			{missingCount}
 		</span>
 	)
 })
