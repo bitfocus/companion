3093ad22ad26ec6291bfdf5d7f50468faf36b390
arikorn <arikorn@users.noreply.github.com>
2025-11-02 19:37:24 -0800

    Revert "Fix `createRequire` in a way that doesn't require hardcoding the filename in the source file."
    This reverts commit a03cfae22391def91d467197c185ee18a18d28d7.

--- companion/lib/Instance/ProcessManager.ts
+++ companion/lib/Instance/ProcessManager.ts
@@ -18,6 +18,11 @@ import { ModuleInstanceType, type InstanceConfig } from '@companion-app/shared/M
 import { assertNever } from '@companion-app/shared/Util.js'
 import type { SomeModuleVersionInfo } from './Types.js'
 
+// `import.meta.url` gets hardcoded to the build time file by webpack (or so Google's AI Overview insists... regardless, it _is_ getting hardcoded)
+//  so it puts the build-time path into main.js.  The following "trick" fixes it.
+// (Unfortunately, I haven't found a way that skips hardcoding the filename. Using '.' or '' in new URL() fails; the latter fails after build. And fileURLToPath() doesn't help either.)
+const require = createRequire(new URL('ProcessManager.js', import.meta.url))
+
 /**
  * A backoff sleep strategy
  * @returns ms to sleep
@@ -645,10 +650,6 @@ export class InstanceProcessManager {
 				if (!moduleInfo.isPackaged) {
 					// When not packaged, lookup the version from the library itself
 					try {
-						// `import.meta.url` in `createRequire()` gets hardcoded to the build time file by webpack so it puts the build-time path into main.js.
-						// Using `moduleInfo.basePath` here avoids the problem and may be more appropriate anyway.
-						const require = createRequire(moduleInfo.basePath)
-
 						const moduleLibPackagePath = require.resolve('@companion-module/base/package.json', {
 							paths: [moduleInfo.basePath],
 						})
