d90bae709c176dc7e595a7bf5aac3860e5de1c14
Julian Waller <git@julusian.co.uk>
2025-11-13 19:06:39 +0000

    chore: tidy webpack

--- companion/webpack.config.js
+++ companion/webpack.config.js
@@ -2,12 +2,8 @@ import path from 'path'
 import fs from 'fs'
 import { sentryWebpackPlugin } from '@sentry/webpack-plugin'
 import { fileURLToPath } from 'url'
-// import { createRequire } from 'module'
-// const require = createRequire(import.meta.url)
 
 // Allow user to set mode at run time. Default is 'development' mode.
-// note: alternatively, leave devMode out and use CLI argument --mode=development (default is production)
-// but that would take a bit of argv management in dist.mts...
 const devMode = process.env.WEBPACK_IN_DEV_MODE ? 'development' : 'production'
 //const optimizeUseProduction = false // for enabling/disabling optimizations; default: devMode === production'
 console.log(`Running webpack in ${devMode} mode.`)
@@ -26,17 +22,15 @@ export default {
 	},
 	mode: devMode,
 	// note: `undefined` defaults to 'eval' in dev mode, which is not compatible with `output.module: true` (particularly when `importMeta: false`)
-	devtool: devMode === 'production' ? 'source-map' : false, //sentryAuthToken ? 'source-map' : false,
+	devtool: devMode === 'production' ? 'source-map' : false,
 	output: {
 		filename: '[name].js', // override default .mjs for `output.module: true`
 		path: distPath,
-		//clean: true, // works but maybe safer not to since we call webpack twice.
 		pathinfo: true,
 		// environment: {
 		// 	module: true,
 		// },
 		module: true,
-		// scriptType: 'module', // the default when module is true.
 	},
 	context: path.resolve(modulePath, '.'),
 	target: 'node',
@@ -50,17 +44,6 @@ export default {
 			'.js': ['.ts', '.js'],
 			'.mjs': ['.mts', '.mjs'],
 		},
-		// 	fallback: {
-		// 		// use native node modules
-		// 		fs: false,
-		// 		buffer: false,
-		// 		path: false,
-		// 		stream: false,
-		// 		zlib: false,
-		// 		timers: false,
-		// 		http: false,
-		// 		https: false,
-		// 	},
 	},
 	externalsPresets: { node: true },
 	externals: {
@@ -75,26 +58,6 @@ export default {
 		outputModule: true,
 	},
 	module: {
-		rules: [
-			// {
-			// 	test: /\.json/,
-			// 	type: 'asset/inline',
-			// },
-			{
-				test: /BUILD$/,
-				type: 'asset/resource',
-				generator: {
-					filename: 'BUILD',
-				},
-			},
-			{
-				test: /SENTRY$/,
-				type: 'asset/resource',
-				generator: {
-					filename: 'SENTRY',
-				},
-			},
-		],
 		parser: {
 			javascript: {
 				importMeta: false, // don't convert import.meta.url to a hardcoded string
