40d83ee8d7b47ed753a4b5fe912fca814aa4ee95
Julian Waller <git@julusian.co.uk>
2025-11-16 18:07:30 +0000

    feat: remove old surface firmware update check

--- companion/lib/Surface/Controller.ts
+++ companion/lib/Surface/Controller.ts
@@ -38,7 +38,6 @@ import { createOrSanitizeSurfaceHandlerConfig } from './Config.js'
 import { EventEmitter } from 'events'
 import LogController from '../Log/Controller.js'
 import type { DataDatabase } from '../Data/Database.js'
-import { SurfaceFirmwareUpdateCheck } from './FirmwareUpdateCheck.js'
 import type { DataStoreTableView } from '../Data/StoreBase.js'
 import { publicProcedure, router, toIterable } from '../UI/TRPC.js'
 import z from 'zod'
@@ -122,8 +121,6 @@ export class SurfaceController extends EventEmitter<SurfaceControllerEvents> {
 
 	readonly #outboundController: SurfaceOutboundController
 
-	readonly #firmwareUpdates: SurfaceFirmwareUpdateCheck
-
 	get outbound(): SurfaceOutboundController {
 		return this.#outboundController
 	}
@@ -138,7 +135,6 @@ export class SurfaceController extends EventEmitter<SurfaceControllerEvents> {
 		this.#updateEvents.setMaxListeners(0)
 
 		this.#outboundController = new SurfaceOutboundController(db)
-		this.#firmwareUpdates = new SurfaceFirmwareUpdateCheck(this.#surfaceHandlers, () => this.triggerUpdateDevicesList())
 
 		this.#surfacesAllLocked = !!this.#handlerDependencies.userconfig.getKey('link_lockouts')
 
@@ -371,9 +367,6 @@ export class SurfaceController extends EventEmitter<SurfaceControllerEvents> {
 
 		// Update the group to have the new surface
 		this.#attachSurfaceToGroup(handler)
-
-		// Perform an update check in the background
-		this.#firmwareUpdates.triggerCheckSurfaceForUpdates(handler)
 
 		return handler
 	}
