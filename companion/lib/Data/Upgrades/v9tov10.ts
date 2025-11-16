f8dce49569153e3c5d846baae9f160236fbb30ac
Julian Waller <git@julusian.co.uk>
2025-11-16 21:41:47 +0000

    feat: remove old userconfig fields, and auto-add needed instances

--- companion/lib/Data/Upgrades/v9tov10.ts
+++ companion/lib/Data/Upgrades/v9tov10.ts
@@ -14,6 +14,7 @@ import {
 	ModuleInstanceType,
 } from '@companion-app/shared/Model/Instance.js'
 import { nanoid } from 'nanoid'
+import type { UserConfigGridSize, BackupRulesConfig } from '@companion-app/shared/Model/UserConfigModel.js'
 
 interface LegacyOutboundSurfaceInfo {
 	id: string
@@ -24,6 +25,83 @@ interface LegacyOutboundSurfaceInfo {
 	port: number
 	collectionId: string | null
 	sortOrder: number
+}
+
+interface OldUserConfigModel {
+	setup_wizard: number
+
+	page_direction_flipped: boolean
+	page_plusminus: boolean
+	remove_topbar: boolean
+
+	xkeys_enable: boolean
+	elgato_plugin_enable: boolean // Also disables local streamdeck
+	usb_hotplug: boolean
+	loupedeck_enable: boolean
+	mirabox_streamdock_enable: boolean
+	contour_shuttle_enable: boolean
+	vec_footpedal_enable: boolean
+	blackmagic_controller_enable: boolean
+	mystrix_enable: boolean
+	logitech_mx_console_enable: boolean
+
+	pin_enable: boolean
+	link_lockouts: boolean
+	pin: string
+	pin_timeout: number
+
+	http_api_enabled: boolean
+	http_legacy_api_enabled: boolean
+
+	tcp_enabled: boolean
+	tcp_listen_port: number
+	tcp_legacy_api_enabled: boolean
+
+	udp_enabled: boolean
+	udp_listen_port: number
+	udp_legacy_api_enabled: boolean
+
+	osc_enabled: boolean
+	osc_listen_port: number
+	osc_legacy_api_enabled: boolean
+
+	rosstalk_enabled: boolean
+
+	emberplus_enabled: boolean
+
+	videohub_panel_enabled: boolean
+
+	artnet_enabled: boolean
+	artnet_universe: number
+	artnet_channel: number
+
+	https_enabled: boolean
+	https_port: number
+	https_cert_type: string
+	https_self_cn: string
+	https_self_expiry: number
+	https_self_cert: string
+	https_self_cert_created: string
+	https_self_cert_cn: string
+	https_self_cert_expiry: string
+	https_self_cert_private: string
+	https_self_cert_public: string
+	https_ext_private_key: string
+	https_ext_certificate: string
+	https_ext_chain: string
+
+	admin_lockout: boolean
+	admin_timeout: number
+	admin_password: string
+
+	gridSize: UserConfigGridSize
+	gridSizeInlineGrow: boolean
+	gridSizePromptGrow: boolean
+
+	installName: string
+	default_export_filename: string
+
+	backups: BackupRulesConfig[]
 }
 
 /**
@@ -62,14 +140,62 @@ function convertDatabaseToV10(db: DataStoreBase<any>, _logger: Logger): void {
 		})
 	}
 
+	// Create surface instances based on old userconfig settings
+	const userconfig: Partial<OldUserConfigModel> = db.defaultTableView.getOrDefault('userconfig', {})
+
+	const createInstanceIfNeeded = (
+		oldKey: keyof OldUserConfigModel,
+		moduleId: string,
+		isBuiltin: boolean,
+		invertState = false,
+		preserveConfig = false
+	) => {
+		// Only create instance if the setting is enabled
+		const isEnabled = invertState ? !userconfig[oldKey] : userconfig[oldKey]
+		if (!preserveConfig) delete userconfig[oldKey]
+		if (!isEnabled) return
+
+		// Don't create duplicate instances
+		if (allSurfaceInstanceModuleIds.has(moduleId)) return
+
+		const newInstanceId = nanoid()
+		allSurfaceInstanceModuleIds.set(moduleId, newInstanceId)
+
+		// Create the instance
+		instances.set(newInstanceId, {
+			moduleInstanceType: ModuleInstanceType.Surface,
+			moduleId: moduleId,
+			moduleVersionId: isBuiltin ? 'builtin' : null,
+			label: moduleId,
+			config: {},
+			secrets: undefined,
+			isFirstInit: true,
+			lastUpgradeIndex: 0,
+			enabled: true,
+			sortOrder: 0,
+			updatePolicy: InstanceVersionUpdatePolicy.Stable,
+		} satisfies InstanceConfig)
+	}
+
+	// Note: this will remove the old config fields
+	createInstanceIfNeeded('elgato_plugin_enable', 'elgato-stream-deck', true, true, true)
+	createInstanceIfNeeded('xkeys_enable', 'xkeys', false)
+	createInstanceIfNeeded('loupedeck_enable', 'loupedeck', false)
+	createInstanceIfNeeded('mirabox_streamdock_enable', 'mirabox-streamdock', false)
+	createInstanceIfNeeded('contour_shuttle_enable', 'contour-shuttle', false)
+	createInstanceIfNeeded('vec_footpedal_enable', 'vec-footpedal', false)
+	createInstanceIfNeeded('blackmagic_controller_enable', 'blackmagic-controller', false)
+	createInstanceIfNeeded('mystrix_enable', '203-systems-mystrix', false)
+	createInstanceIfNeeded('logitech_mx_console_enable', 'logitech-mx-creative-console', false)
+	createInstanceIfNeeded('videohub_panel_enabled', 'blackmagic-videohub-panel', false)
+
+	db.defaultTableView.set('userconfig', userconfig)
+
+	// Convert any remote surfaces to new format
 	const remoteSurfaces = db.getTableView('surfaces_remote')
 	const anyRemoteSurfacesAreLegacyElgato = Object.values(remoteSurfaces.all()).some((surface) => {
 		return (surface as LegacyOutboundSurfaceInfo).type === 'elgato'
 	})
-
-	// TODO - auto-create streamdeck instances for surfaces that need them
-
-	// Convert any remote surfaces to new format
 	if (anyRemoteSurfacesAreLegacyElgato) {
 		let elgatoSurfaceInstanceId = allSurfaceInstanceModuleIds.get('elgato-stream-deck')
 		if (!elgatoSurfaceInstanceId) {
