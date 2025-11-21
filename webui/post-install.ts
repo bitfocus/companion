aac97d27867c10dbe7a971bfef21c605e2bd2fa4
arikorn <arikorn@users.noreply.github.com>
2025-11-21 11:10:39 -0800

    Move post-install.ts to webui/, fix linting issue
    It's not part of the delivery, so webui/src seems wrong.
    
    Basically treat it like the files in tools to avoid linting errors.

--- webui/post-install.ts
+++ webui/post-install.ts
@@ -3,8 +3,10 @@
 import { fs } from 'zx'
 
 //  rm -Rf public/_deps; mkdir public/_deps && cp -R ../node_modules/monaco-editor/min/vs public/_deps/monaco
-// Trash old, create folder if it doesn't exist
+
+// Trash an old folder and/or create a new one
 fs.emptyDirSync('public/_deps')
 
+// copy monaco editor run-time
 const cpOpts = { preserveTimestamps: true }
 fs.copySync('../node_modules/monaco-editor/min/vs', 'public/_deps/monaco', cpOpts)
