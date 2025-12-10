5a7a7b40ab964a381f2a8b02af3b056a58d45f30
Julian Waller <git@julusian.co.uk>
2025-12-10 19:13:45 +0000

    fix: startup crash if bufferutil not found #3826

--- companion/lib/Resources/FixImports.ts
+++ companion/lib/Resources/FixImports.ts
@@ -1,6 +1,5 @@
-import fs from 'fs-extra'
-import path from 'path'
-import { isPackaged } from './Util.js'
+import fs from 'node:fs'
+import path from 'node:path'
 
 // Always disable the utf-8-validate module, it is not needed since nodejs v18.14.0
 process.env.WS_NO_UTF_8_VALIDATE = '1'
@@ -11,9 +10,9 @@ process.env.WS_NO_UTF_8_VALIDATE = '1'
  */
 
 // Only run if webpacked
-if (isPackaged()) {
+if (typeof __webpack_require__ === 'function') {
 	// Disable loading some ws libs if they were not provided on disk, otherwise the application will crash
-	if (!fs.pathExistsSync(path.join(import.meta.dirname, 'node_modules/bufferutil'))) {
+	if (!fs.existsSync(path.join(import.meta.dirname, 'node_modules/bufferutil'))) {
 		process.env.WS_NO_BUFFER_UTIL = '1'
 	}
 }
