8aabec37c5660c763c8c24681d107bd6e49ce490
Ari Kornfeld <arikorn@users.noreply.github.com>
2025-12-24 00:18:31 -0800

    Rewrite the dropdown using CoreUI's `CDropdown`
    It's a bit more direct, and works better too. But I'm keeping the react-select version in the previous commit (rather than overwriting it), just in case, for now.

--- webui/src/Layout/Header.tsx
+++ webui/src/Layout/Header.tsx
@@ -1,5 +1,18 @@
 import React, { type ReactElement, useCallback, useContext } from 'react'
-import { CHeader, CHeaderBrand, CHeaderNav, CNavItem, CNavLink, CHeaderToggler, CContainer } from '@coreui/react'
+import {
+	CHeader,
+	CHeaderBrand,
+	CHeaderNav,
+	CNavItem,
+	CNavLink,
+	CHeaderToggler,
+	CContainer,
+	CDropdownToggle,
+	CDropdown,
+	CDropdownItem,
+	CDropdownMenu,
+	CDropdownDivider,
+} from '@coreui/react'
 import {
 	faBars,
 	// faCircle, // if backing the faCircleQuestion icon
@@ -20,105 +33,50 @@ import { useSidebarState } from './Sidebar.js'
 import { trpc } from '../Resources/TRPC.js'
 import { useSubscription } from '@trpc/tanstack-react-query'
 import { Link } from '@tanstack/react-router'
-import Select, {
-	components as SelectComponents,
-	type DropdownIndicatorProps,
-	type OptionProps,
-	// type ControlProps,
-	// type ValueContainerProps,
-	// type InputActionMeta,
-} from 'react-select'
 
 interface MyHeaderProps {
 	canLock: boolean
 	setLocked: (locked: boolean) => void
 }
+
+// provide a declarative menu specification:
 interface MenuOption {
 	readonly value: string
 	readonly label: string
 	readonly icon?: IconDefinition | (() => ReactElement)
-	readonly destination?: string
+	readonly destination: string | (() => void)
 	readonly tooltip?: string
 	readonly isExternal?: boolean
-	readonly newSection?: boolean
-	readonly isFixed?: boolean
-	readonly isDisabled?: boolean
+	readonly isSeparator?: boolean
 }
 
+const MenuSeparator: MenuOption = {
+	value: '',
+	label: '',
+	destination: '',
+	isSeparator: true,
+}
+
+// make our own circleInfo since it's not in the free FontAwesome offering
 function circleInfo(stacked = false): ReactElement {
 	return stacked ? (
-		<span className="fa-stack fa-sm">
-			<FontAwesomeIcon icon={faOpenCircle} className="fa-stack-2x" style={{ marginLeft: '-0.55em' }} />
+		<span className="fa-stack fa-2xs" style={{ marginRight: '-0.75em', marginTop: '-0.2em' }}>
+			<FontAwesomeIcon icon={faOpenCircle} className="fa-stack-2x" style={{ marginLeft: '-0.45em' }} />
 			<FontAwesomeIcon icon={faInfo} className="fa-stack-1x" style={{ marginLeft: '0.1em' }} />
 		</span>
 	) : (
 		<FontAwesomeIcon
 			icon={faInfo}
-			className="fa-sm"
+			className="fa-xs"
 			style={{
-				border: '0.2em, solid, #fff',
+				border: '0.2em, solid',
 				borderRadius: '100%',
 				padding: '0.15em 0.05em 0.35em 0.05em', // top right bottom left
 				marginBottom: '-0.45em',
-				marginLeft: '-0.15em',
-				marginRight: '0.35em',
 			}}
 		/>
 	)
 }
-
-const helpMenuOptions: MenuOption[] = [
-	{
-		value: 'help',
-		label: 'User Guide',
-		icon: circleInfo,
-		destination: '/user-guide/',
-		tooltip: 'Open the User Guide in a new tab.',
-		isExternal: true,
-	},
-	{
-		value: 'new',
-		label: "What's New",
-		icon: faStar,
-		destination: 'whatsNewOpen',
-		tooltip: 'Show the current release notes.',
-		isExternal: false,
-	},
-	{
-		newSection: true,
-		value: 'fb',
-		label: 'Community Support',
-		icon: faFacebook,
-		destination: 'https://bfoc.us/qjk0reeqmy',
-		tooltip: 'Share you experience or ask questions to your Companions.',
-		isExternal: true,
-	},
-	{
-		value: 'slack',
-		label: 'Slack Workspace',
-		icon: faSlack,
-		destination: 'https://bfoc.us/ke7e9dqgaz',
-		tooltip: 'Discuss technical issues on Slack.',
-		isExternal: true,
-	},
-	{
-		value: 'github',
-		label: 'Report an Issue',
-		icon: faGithub,
-		destination: 'https://bfoc.us/fiobkz0yqs',
-		tooltip: 'Report bugs or request features on GitHub.',
-		isExternal: true,
-	},
-	{
-		newSection: true,
-		value: 'donate',
-		label: 'Donate',
-		icon: faDollarSign,
-		destination: 'https://bfoc.us/ccfbf8wm2x',
-		tooltip: 'Contribute funds to Bitfocus Companion.',
-		isExternal: true,
-	},
-]
 
 export const MyHeader = observer(function MyHeader({ canLock, setLocked }: MyHeaderProps) {
 	const { userConfig } = useContext(RootAppStoreContext)
@@ -178,118 +136,152 @@ export const MyHeader = observer(function MyHeader({ canLock, setLocked }: MyHea
 						</CNavItem>
 					)}
 
-					<HelpMenu menuOptions={helpMenuOptions} />
+					<HelpMenu />
 				</CHeaderNav>
 			</CContainer>
 		</CHeader>
 	)
 })
 
-// Help menu
-function HelpMenu({ menuOptions }: { menuOptions: MenuOption[] }) {
+function HelpMenu({ ...props }) {
 	// we could add the config wizard here too?
-	// couldn't figure out how to make this an external function so we don't rely on a magic string in the menuOption
 	const { whatsNewModal } = useContext(RootAppStoreContext)
 	const whatsNewOpen = useCallback(() => whatsNewModal.current?.show(), [whatsNewModal])
 
-	// The "circleQuestion" icon that shows in the header:
-	const HelpIconIndicator = (props: DropdownIndicatorProps<MenuOption>) => (
-		<SelectComponents.DropdownIndicator {...props}>
-			<FontAwesomeIcon icon={faCircleQuestion} className="fa-2xl" />
-		</SelectComponents.DropdownIndicator>
-	)
-
-	// create menu-entries with (1) optional left-hand icon, (2) label, (3) optional right-side "external link" icon
-	const CustomOption = ({ children, ...props }: OptionProps<MenuOption>) => {
-		const { data } = props
-
-		const isUrl = data.destination !== 'whatsNewOpen'
-		const navProps = isUrl
-			? { to: data.destination, target: '_blank', rel: 'noopener noreferrer', as: Link }
-			: { onClick: whatsNewOpen }
-
-		// Structure: [CNavLink [left-icon, text, right-icon ]]
-		return (
-			<>
-				{data.newSection && (
-					<hr style={{ margin: '0px 10px', borderColor: '#000' }} /> // Simple styling for the divider
-				)}
-				<SelectComponents.Option {...props}>
-					<CNavLink {...navProps} title={data.tooltip}>
-						<span
-							{...props.innerProps}
-							style={{
-								display: 'flex',
-								alignItems: 'center',
-								justifyContent: 'space-between',
-							}}
-						>
-							<span>
-								{typeof data.icon === 'function' ? (
-									data.icon()
-								) : (
-									<FontAwesomeIcon
-										icon={data.icon ? data.icon : faInfo}
-										style={{ visibility: data.icon ? 'inherit' : 'hidden', paddingRight: '0.5em' }}
-									/>
-								)}
-
-								{children}
-							</span>
-
-							{data.isExternal ? <FontAwesomeIcon icon={faExternalLinkSquare} /> : ' '}
-						</span>
-					</CNavLink>
-				</SelectComponents.Option>
-			</>
-		)
+	// specifying this inline below, causes a spurious TypeScript error message on '--cui-dropdown-link-hover-bg'
+	const menuStyle = {
+		backgroundColor: 'var(--cui-header-bg)',
+		// note: hover color is controlled by `.header .nav-link:hover` in _layout.scss
+		'--cui-dropdown-link-hover-bg': 'var(--companion-header-brand-bg)',
+		padding: 0,
 	}
 
+	// note: this const has to be inside the component so that we can grab `whatsNewOpen` which is a useCallback...
+	const helpMenuOptions: MenuOption[] = [
+		{
+			value: 'help',
+			label: 'User Guide / Help',
+			icon: circleInfo,
+			destination: '/user-guide/',
+			tooltip: 'Open the User Guide in a new tab.',
+			isExternal: true,
+		},
+		{
+			value: 'new',
+			label: "What's New",
+			icon: faStar,
+			destination: whatsNewOpen,
+			tooltip: 'Show the current release notes.',
+			isExternal: false,
+		},
+		MenuSeparator,
+		{
+			value: 'fb',
+			label: 'Community Support',
+			icon: faFacebook,
+			destination: 'https://bfoc.us/qjk0reeqmy',
+			tooltip: 'Share you experience or ask questions to your Companions.',
+			isExternal: true,
+		},
+		{
+			value: 'slack',
+			label: 'Slack Workspace',
+			icon: faSlack,
+			destination: 'https://bfoc.us/ke7e9dqgaz',
+			tooltip: 'Discuss technical issues on Slack.',
+			isExternal: true,
+		},
+		{
+			value: 'github',
+			label: 'Report an Issue',
+			icon: faGithub,
+			destination: 'https://bfoc.us/fiobkz0yqs',
+			tooltip: 'Report bugs or request features on GitHub.',
+			isExternal: true,
+		},
+		MenuSeparator,
+		{
+			value: 'donate',
+			label: 'Donate',
+			icon: faDollarSign,
+			destination: 'https://bfoc.us/ccfbf8wm2x',
+			tooltip: 'Contribute funds to Bitfocus Companion.',
+			isExternal: true,
+		},
+	]
+
 	return (
-		<Select
-			controlShouldRenderValue={false}
-			options={menuOptions}
-			value={null}
-			maxMenuHeight={1000} // Need to figure something better?
-			components={{
-				//Control: CustomControl,
-				ValueContainer: () => null, // don't show input
-				DropdownIndicator: HelpIconIndicator, // the menu icon
-				Input: () => null, // don't show input
-				IndicatorSeparator: () => null, // don't show vertical line
-				//IndicatorsContainer: () => null, // don't show down arrow, etc.
-				Option: CustomOption,
+		<CDropdown
+			{...props}
+			className=""
+			offset={[10, 7]}
+			style={{
+				//without the next line, the menu doesn't show:
+				position: 'inherit',
 			}}
-			styles={{
-				control: (base) => ({
-					// affects the parent of the value-container & dropdown indicator...
-					...base,
-					backgroundColor: 'var(--cui-header-bg)',
-					border: 0,
-				}),
-				dropdownIndicator: (base) => ({
-					...base,
-					// provide a darker background for the circle-help icon
+		>
+			<CDropdownToggle
+				color="primary"
+				caret={false}
+				style={{
+					// this is all needed to make the focus/hover-highlight round and just slightly larger than the icon
+					padding: '0.2em 0',
+					marginLeft: '1em',
 					borderRadius: '100%',
-					padding: '0.2em 0em', // to make the circle round
-					backgroundColor: 'var(--companion-header-brand-bg)',
-					color: 'var(--cui-header-color)', // color when focused is handled independently. not sure where!
-				}),
-				menu: (base) => ({
-					// the drop-down
-					...base,
-					right: '-1em', // right-justify the pull-down relative to the control or container
-					width: '16em',
-					backgroundColor: 'var(--cui-header-bg)',
-				}),
-				option: (base, state) => ({
-					...base,
-					// note: hover color is controlled by `.header .nav-link:hover` in _layout.scss, which overrides color here
-					color: 'var(--cui-header-color)',
-					backgroundColor: state.isFocused ? 'var(--companion-header-brand-bg)' : 'var(--cui-header-bg)',
-					paddingLeft: 0,
-				}),
+				}}
+			>
+				<FontAwesomeIcon icon={faCircleQuestion} className="fa-2xl" />
+			</CDropdownToggle>
+			<CDropdownMenu style={menuStyle}>
+				{helpMenuOptions.map((option) => (
+					<MenuItem key={option.value} data={option} {...props} />
+				))}
+			</CDropdownMenu>
+		</CDropdown>
+	)
+}
+
+// create menu-entries with (1) optional left-hand icon, (2) label, (3) optional right-side "external link" icon
+function MenuItem({ data, ...props }: { data: MenuOption }) {
+	const navProps =
+		typeof data.destination === 'string'
+			? { to: data.destination, target: '_blank', rel: 'noopener noreferrer', as: Link }
+			: { onClick: data.destination }
+
+	// Structure: [CDropdownItem [CNavLink [left-icon, text, right-icon ]]] (or separator)
+	return data.isSeparator ? (
+		<CDropdownDivider
+			style={{
+				borderTop: '1px solid #888',
+				margin: '0px 5px',
 			}}
 		/>
+	) : (
+		<CDropdownItem as="button" style={{ paddingLeft: 0, paddingRight: 5 }}>
+			<CNavLink {...navProps} title={data.tooltip}>
+				<span
+					{...props}
+					style={{
+						display: 'flex',
+						alignItems: 'center',
+						justifyContent: 'space-between',
+					}}
+				>
+					<span style={{ paddingLeft: '0.5em' }}>
+						{typeof data.icon === 'function' ? (
+							data.icon()
+						) : (
+							<FontAwesomeIcon
+								icon={data.icon ? data.icon : faInfo}
+								style={{ visibility: data.icon ? 'inherit' : 'hidden' }}
+							/>
+						)}
+						<span style={{ paddingRight: '1em', paddingLeft: '1em' }}>{data.label}</span>
+					</span>
+
+					{data.isExternal ? <FontAwesomeIcon icon={faExternalLinkSquare} /> : ' '}
+				</span>
+			</CNavLink>
+		</CDropdownItem>
 	)
 }
