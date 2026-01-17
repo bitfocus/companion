5205f3ed801be8afc4a200f912d2236ff9478be0
Julian Waller <git@julusian.co.uk>
2026-01-17 19:16:17 +0000

    fix: respect DISABLE_IPV6 env var to disable admin interface binding to ipv6

--- companion/lib/main.ts
+++ companion/lib/main.ts
@@ -19,9 +19,10 @@ import envPaths from 'env-paths'
 import { nanoid } from 'nanoid'
 import { ConfigReleaseDirs } from '@companion-app/shared/Paths.js'
 import { type SyslogTransportOptions } from 'winston-syslog'
-import net from 'net'
+import net, { isIPv6 } from 'net'
 import { ModuleInstanceType } from '@companion-app/shared/Model/Instance.js'
 import { isPackaged } from './Resources/Util.js'
+import { DISABLE_IPv6, GLOBAL_BIND_ADDRESS } from './Resources/Constants.js'
 
 const program = new Command()
 
@@ -101,7 +102,12 @@ program.command('start', { isDefault: true, hidden: true }).action(() => {
 		process.exit(1)
 	}
 
-	let adminIp = options.adminAddress || '::' // default to admin global
+	let adminIp = options.adminAddress || GLOBAL_BIND_ADDRESS // default to admin global
+
+	if (DISABLE_IPv6 && isIPv6(adminIp)) {
+		console.error(`IPv6 has been disabled but has been specified as the admin address`)
+		process.exit(1)
+	}
 
 	if (options.adminInterface) {
 		adminIp = null
