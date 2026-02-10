b05bdb4c28ac6a8f5ffa270f54aa3904cba62283
Julian Waller <git@julusian.co.uk>
2026-02-10 22:10:53 +0000

    wip

--- shared-lib/lib/Model/Options.ts
+++ shared-lib/lib/Model/Options.ts
@@ -1,6 +1,6 @@
 import type { DropdownChoice, DropdownChoiceId } from './Common.js'
 import type { JsonValue } from 'type-fest'
-import type { CompanionOptionValues } from '@companion-module/host'
+import type { CompanionOptionValues, CompanionPresetOptionValues } from '@companion-module/host'
 import z from 'zod'
 
 export const JsonValueSchema: z.ZodType<JsonValue> = z.json()
@@ -304,7 +304,7 @@ export function isExpressionOrValue(input: any): input is ExpressionOrValue<any>
 }
 
 export function optionsObjectToExpressionOptions(
-	options: CompanionOptionValues,
+	options: CompanionOptionValues | CompanionPresetOptionValues<any>,
 	allowExpressions = true
 ): ExpressionableOptionsObject {
 	const res: ExpressionableOptionsObject = {}
