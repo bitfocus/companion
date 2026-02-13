c2845fca117b47053561a66f11cdbfb0d53f8443
Ari Kornfeld <48372817+arikorn@users.noreply.github.com>
2026-02-13 14:18:49 -0800

    fix: remaining button-group `border-radius` issues (#3963)

--- webui/src/Buttons/EditButton/SelectButtonTypeDropdown.tsx
+++ webui/src/Buttons/EditButton/SelectButtonTypeDropdown.tsx
@@ -1,6 +1,6 @@
 import type { SomeButtonModel } from '@companion-app/shared/Model/ButtonModel.js'
 import type { ControlLocation } from '@companion-app/shared/Model/Common.js'
-import { CDropdown, CButtonGroup, CButton, CDropdownToggle, CDropdownMenu, CDropdownItem } from '@coreui/react'
+import { CDropdown, CButton, CDropdownToggle, CDropdownMenu, CDropdownItem } from '@coreui/react'
 import React, { useCallback } from 'react'
 import type { GenericConfirmModalRef } from '~/Components/GenericConfirmModal.js'
 import { trpc, useMutationExt } from '~/Resources/TRPC'
@@ -55,21 +55,17 @@ export function SelectButtonTypeDropdown({
 	)
 
 	return (
-		<CDropdown className="" style={{ display: 'inline-block', marginRight: -4, position: 'inherit' }}>
-			<CButtonGroup>
-				{/* This could be simplified to use the split property on CDropdownToggle, but then onClick doesnt work https://github.com/coreui/coreui-react/issues/179 */}
-				<CButton color="danger" onClick={() => setButtonType('button')}>
-					Create button
-				</CButton>
-				<CDropdownToggle
-					caret
-					color="danger"
-					style={{ opacity: 0.7, paddingLeft: 14, paddingRight: 16 }}
-					className="dropdown-toggle dropdown-toggle-split"
-				>
-					{/* <span className="sr-only">Toggle Dropdown</span> */}
-				</CDropdownToggle>
-			</CButtonGroup>
+		<CDropdown variant="btn-group">
+			<CButton color="danger" onClick={() => setButtonType('button')} title="Create regular button.">
+				Create button
+			</CButton>
+			<CDropdownToggle
+				color="danger"
+				split
+				style={{ opacity: 0.7, padding: '0 0.75em' }}
+				aria-label="Toggle Button-Type Dropdown"
+				title="Toggle Button-Type Dropdown"
+			/>
 			<CDropdownMenu>
 				<CDropdownItem onClick={() => setButtonType('button')}>Regular button</CDropdownItem>
 				<CDropdownItem onClick={() => setButtonType('pageup')}>Page up</CDropdownItem>
