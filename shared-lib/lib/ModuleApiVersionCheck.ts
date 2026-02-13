f1b400456c9c242616ec8ce34e9e043886e8e01e
Julian Waller <git@julusian.co.uk>
2026-02-13 22:56:35 +0000

    fix package version

--- shared-lib/lib/ModuleApiVersionCheck.ts
+++ shared-lib/lib/ModuleApiVersionCheck.ts
@@ -2,7 +2,7 @@ import semver from 'semver'
 import { ModuleInstanceType } from './Model/Instance.js'
 import { assertNever } from './Util.js'
 
-export const MODULE_BASE_VERSIONS = ['1.14.0', '2.0.0-0-nightly-feat-2-0-20260131-124809-4f9655d']
+export const MODULE_BASE_VERSIONS = ['1.14.0', '2.0.0-0-nightly-feat-2-0-20260213-225320-440847a']
 export const SURFACE_BASE_VERSION = '1.1.0'
 
 const moduleBaseRules = MODULE_BASE_VERSIONS.map((v) => {
