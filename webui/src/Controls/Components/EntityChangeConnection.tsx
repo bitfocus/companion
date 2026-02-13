ee4384a476e660a171ffb82aea27548021e64dd8
Julian Waller <git@julusian.co.uk>
2026-02-13 22:03:34 +0000

    feat: apply grouping to entity connection chooser

--- webui/src/Controls/Components/EntityChangeConnection.tsx
+++ webui/src/Controls/Components/EntityChangeConnection.tsx
@@ -1,10 +1,13 @@
-import type { DropdownChoiceId } from '@companion-app/shared/Model/Common.js'
+import type { DropdownChoice, DropdownChoiceId } from '@companion-app/shared/Model/Common.js'
 import React, { useContext } from 'react'
 import { DropdownInputField } from '~/Components/DropdownInputField.js'
 import { RootAppStoreContext } from '~/Stores/RootAppStore.js'
 import { useComputed } from '~/Resources/util.js'
 import { observer } from 'mobx-react-lite'
 import { CCol, CFormLabel } from '@coreui/react'
+import type { DropdownChoicesOrGroups } from '~/Components'
+import { groupItemsByCollection } from '~/Helpers/CollectionGrouping'
+import type { ClientConnectionConfig } from '@companion-app/shared/Model/Connections.js'
 
 interface EntityCellLeftMainProps {
 	entityConnectionId: string
@@ -16,19 +19,34 @@ export const EntityChangeConnection = observer(function EntityCellLeftMain({
 }: EntityCellLeftMainProps) {
 	const { connections } = useContext(RootAppStoreContext)
 
-	const connectionChoices = useComputed(() => {
+	const connectionChoices = useComputed((): DropdownChoicesOrGroups | null => {
 		const connectionInfo = connections.getInfo(entityConnectionId)
+		if (!connectionInfo) return null
 
-		const connectionsWithSameType = connectionInfo ? connections.getAllOfModuleId(connectionInfo.moduleId) : []
+		// Convert connections map to array
+		const allConnections = Array.from(connections.connections.values())
 
-		return connectionsWithSameType
-			.sort((connectionA, connectionB) => connectionA.sortOrder - connectionB.sortOrder)
-			.map((info) => {
-				return { id: info.id, label: info.label }
-			})
-	}, [connections, entityConnectionId])
+		const getItemChoice = (config: ClientConnectionConfig): DropdownChoice => ({
+			id: config.id,
+			label: config.label,
+		})
+		const filterItem = (config: ClientConnectionConfig) => config.moduleId === connectionInfo.moduleId
 
-	if (connectionChoices.length <= 1) return null
+		const groupsOrItems = groupItemsByCollection(
+			connections.rootCollections(),
+			allConnections,
+			getItemChoice,
+			filterItem
+		)
+
+		// Check if there are any connections with the same moduleId
+		const count = groupsOrItems.reduce((acc, item) => acc + ('options' in item ? item.options.length : 1), 0)
+		if (count <= 1) return null
+
+		return groupsOrItems
+	}, [connections])
+
+	if (!connectionChoices) return null
 
 	return (
 		<>
