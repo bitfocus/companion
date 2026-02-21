83bf090694f324ef5c63f345bfba2f8e7f2c7607
Ari Kornfeld <arikorn@users.noreply.github.com>
2026-02-21 13:03:42 -0800

    fix module filtering
    1. current threshold of -10,000 results in no filtering since the expected range is 0-1, resulting in many false positives.
    
    based on a bit of testing, 0.3 seems very permissive yet a decent filter. 0.5 is better if you want stricter filtering.
    
    2. Also: add a note to the "Modules are ignored" warning to better explain what it means and how to include the ignored files.

--- webui/src/Hooks/useFilteredProducts.ts
+++ webui/src/Hooks/useFilteredProducts.ts
@@ -90,10 +90,16 @@ export function filterProducts(allProducts: FuzzyProduct[], filter: string, incl
 	const keys: Array<keyof FuzzyProduct> = ['product', 'name', 'keywords']
 	if (includeType) keys.push('moduleType')
 
-	return fuzzySearch(filter, allProducts, {
+	console.log('Filtering ' + allProducts.length + ' products.')
+	const result = fuzzySearch(filter, allProducts, {
 		keys,
-		threshold: -10_000,
-	}).map((x) => x.obj)
+		// threshold is // 0 - 1, where 1 is "perfect".
+		// But note that even exact word matches may not get a score of 1!
+		// ("Elgato", for example scores 0.8 - 0.9. -- you may need the whole field to match for 1.0...)
+		threshold: 0.3, // tolerates some typos; 0.5 looks like a good "strict" threshold.
+	})
+	console.log('fuzzy (' + result.length + '): ' + result.map((x) => x.score.toFixed(3)))
+	return result.map((x) => x.obj)
 }
 
 export interface FuzzyProduct {
