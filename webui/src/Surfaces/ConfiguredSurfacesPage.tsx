bitfocus/companion

commit d3e5ccb052e934c57e684348f00f4b2aaa595d57

chore: improve wording and help on Surfaces page.
(Makes it more like the "Connections" and other pages.)

webui/src/Surfaces/ConfiguredSurfacesPage.tsx
@@ -91,12 +91,23 @@ export const ConfiguredSurfacesPage = observer(function ConfiguredSurfacesPage()
 			>
 				<div className="fixed-header">
 					<h4 className="btn-inline">
-						Configured Surfaces
-						<ContextHelpButton action="/user-guide/config/surfaces" />
+						Surfaces
+						<ContextHelpButton action="/user-guide/config/surfaces">
+							Use the table, below to configure currently-known surfaces and groups.
+							<br />
+							To configure a surface integration or global setting,
+							<span className="d-inline d-xl-none">
+								{' '}
+								click the "Show Settings" button, above the table on the right
+							</span>
+							<span className="d-none d-xl-inline"> use the panel to the right</span>.
+							<br />
+							Click this icon for more help.
+						</ContextHelpButton>
 					</h4>
 
 					<p style={{ marginBottom: '0.5rem' }}>
-						Click on any item to edit the configuration of a currently-known surface or group.
+						Click on any item below to edit the configuration of a currently-known surface or group.
 						<br />
 						If your streamdeck is missing from this list, you might need to close the Elgato Streamdeck application and
 						click the Rescan button below.

webui/src/Surfaces/KnownSurfacesTable.tsx
@@ -94,8 +94,8 @@ export const KnownSurfacesTable = observer(function KnownSurfacesTable({
 			<GenericConfirmModal ref={confirmRef} />
 
 			<div className="scrollable-content surfaces-grid-container">
-				<div className="grid-header-cell">NO</div>
-				<div className="grid-header-cell">Info</div>
+				<div className="grid-header-cell">Nr.</div>
+				<div className="grid-header-cell">Configured Surfaces and Groups</div>
 				<div className="grid-header-cell"></div>
 				{surfacesList.map((group) => {
 					if (group.isAutoGroup && (group.surfaces || []).length === 1) {

commit 9555c3becadf62c66f9b97d3f7e440538521693a

chore: fix commit d3e5ccb0, "improve wording and help on Surfaces page"
This PR/commit fixes a regression introduced in d3e5ccb0, and also further improves the wording, slightly.

(It is mostly derived from the PR #4078: refactor surface routes.)

webui/src/Surfaces/ConfiguredSurfacesPage.tsx
@@ -96,11 +96,9 @@ export const ConfiguredSurfacesPage = observer(function ConfiguredSurfacesPage()
 							Use the table, below to configure currently-known surfaces and groups.
 							<br />
 							To configure a surface integration or global setting,
-							<span className="d-inline d-xl-none">
-								{' '}
-								click the "Show Settings" button, above the table on the right
-							</span>
-							<span className="d-none d-xl-inline"> use the panel to the right</span>.
+							{twoPanelMode
+								? ' use the panel to the right.'
+								: '	click the "Show Settings" button on the right, just above the table.'}
 							<br />
 							Click this icon for more help.
 						</ContextHelpButton>