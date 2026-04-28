176ef3699ddbe2c89723b166e3f37b5e73487893
Julian Waller <git@julusian.co.uk>
2026-04-28 21:08:29 +0100

    chore: rebuild add entity/event dropdowns with base-ui

--- webui/src/Triggers/AddEventDropdown.tsx
+++ webui/src/Triggers/AddEventDropdown.tsx
@@ -1,14 +1,13 @@
+import { Combobox } from '@base-ui/react/combobox'
+import { ChevronDownIcon } from 'lucide-react'
 import { observer } from 'mobx-react-lite'
-import { useCallback, useContext } from 'react'
-import Select from 'react-select'
+import { useCallback, useContext, useRef } from 'react'
 import type { DropdownChoice, DropdownChoiceId } from '@companion-app/shared/Model/Common.js'
+import { DropdownInputPopup } from '~/Components/DropdownInputField/Popup'
 import { MenuPortalContext } from '~/Components/MenuPortalContext'
 import { useComputed } from '~/Resources/util.js'
 import { RootAppStoreContext } from '~/Stores/RootAppStore.js'
 
-const noOptionsMessage = () => {
-	return 'No events found'
-}
 interface AddEventDropdownProps {
 	onSelect: (value: DropdownChoiceId) => void
 }
@@ -33,30 +32,39 @@ export const AddEventDropdown = observer(function AddEventDropdown({ onSelect }:
 		return options
 	}, [eventDefinitions])
 
-	const innerChange = useCallback(
-		(e: DropdownChoice | null) => {
-			if (e?.id) {
-				onSelect(e.id)
-			}
+	const onChange = useCallback(
+		(id: DropdownChoice['id'] | null) => {
+			if (id) onSelect(id)
 		},
 		[onSelect]
 	)
 
+	const inputRef = useRef<HTMLInputElement>(null)
+
+	const onOpenChange = useCallback((open: boolean) => {
+		if (!open) {
+			inputRef.current?.blur()
+		}
+	}, [])
+
 	return (
-		<Select
-			menuShouldBlockScroll={!!menuPortal} // The dropdown doesn't follow scroll when in a modal
-			menuPortalTarget={menuPortal || document.body}
-			menuPosition={'fixed'}
-			classNamePrefix="select-control"
-			menuPlacement="auto"
-			isClearable={false}
-			isSearchable={true}
-			isMulti={false}
-			options={options}
-			placeholder="+ Add event"
-			value={null}
-			onChange={innerChange}
-			noOptionsMessage={noOptionsMessage}
-		/>
+		<div className="dropdown-field">
+			<Combobox.Root
+				value={null}
+				items={options}
+				multiple={false}
+				autoHighlight
+				onValueChange={onChange}
+				onOpenChange={onOpenChange}
+			>
+				<Combobox.InputGroup className="dropdown-field-input-group">
+					<Combobox.Input className="dropdown-field-input" placeholder={'+ Add event'} ref={inputRef} />
+					<Combobox.Trigger className="dropdown-field-trigger">
+						<ChevronDownIcon className="dropdown-field-icon" />
+					</Combobox.Trigger>
+				</Combobox.InputGroup>
+				<DropdownInputPopup menuPortal={menuPortal ?? undefined} noOptionsMessage="No events found" />
+			</Combobox.Root>
+		</div>
 	)
 })
