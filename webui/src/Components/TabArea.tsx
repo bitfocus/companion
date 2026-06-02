27972d0c2c816c6b7e8c2eab694fa6f6388784e8
Julian Waller <git@julusian.co.uk>
2026-06-02 22:06:04 +0100

    fix: tab scrollbar not always showing in safari #4103

--- webui/src/Components/TabArea.tsx
+++ webui/src/Components/TabArea.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable react-refresh/only-export-components */
 import { Tabs } from '@base-ui/react/tabs'
 import classNames from 'classnames'
+import { useEffect, useRef } from 'react'
 
 // ─── Root ─────────────────────────────────────────────────────────────────────
 
@@ -19,7 +20,39 @@ export interface TabAreaListProps extends Omit<Tabs.List.Props, 'className'> {
 }
 
 function TabAreaList({ className, ...props }: TabAreaListProps): JSX.Element {
-	return <Tabs.List className={classNames('tab-bar', className)} {...props} />
+	const ref = useRef<HTMLDivElement>(null)
+
+	// Check and watch for overflowing, to manually show the scrollbar
+	// This shouldn't be necessary, but safari doesn't show it reliably if on auto
+	// https://github.com/bitfocus/companion/issues/4103
+	useEffect(() => {
+		const el = ref.current
+		if (!el) return
+
+		const checkOverflow = () => {
+			el.classList.toggle('tab-bar--overflowing', el.scrollWidth > el.clientWidth)
+		}
+
+		const ro = new ResizeObserver(checkOverflow)
+		ro.observe(el)
+		for (const child of el.children) ro.observe(child)
+
+		// Re-observe added tabs and re-check when tabs are added/removed
+		const mo = new MutationObserver((mutations) => {
+			for (const m of mutations) for (const node of m.addedNodes) if (node instanceof Element) ro.observe(node)
+			checkOverflow()
+		})
+		mo.observe(el, { childList: true })
+
+		checkOverflow()
+
+		return () => {
+			ro.disconnect()
+			mo.disconnect()
+		}
+	}, [])
+
+	return <Tabs.List ref={ref} className={classNames('tab-bar', className)} {...props} />
 }
 
 // ─── Tab ──────────────────────────────────────────────────────────────────────
