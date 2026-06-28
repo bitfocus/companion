80ea44b7812c3624438613d53c28e1d021ffa35f
Julian Waller <git@julusian.co.uk>
2026-06-28 14:15:49 +0100

    fix: polish config export and reset modals

--- webui/src/ImportExport/Export.tsx
+++ webui/src/ImportExport/Export.tsx
@@ -1,4 +1,4 @@
-import { faDownload } from '@fortawesome/free-solid-svg-icons'
+import { faDownload, faKey } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { useForm } from '@tanstack/react-form'
 import { observer } from 'mobx-react-lite'
@@ -6,13 +6,13 @@ import { useCallback, useContext, useId, useRef, useState } from 'react'
 import type { ClientExportSelection } from '@companion-app/shared/Model/ImportExport.js'
 import { flattenToQueryParams } from '@companion-app/shared/Util/QueryParamUtil.js'
 import { Button } from '~/Components/Button'
-import { CheckboxInputFieldWithLabel } from '~/Components/CheckboxInputField.js'
 import { Form, FormLabel } from '~/Components/Form.js'
 import { InlineHelpIcon } from '~/Components/InlineHelp.js'
 import { Modal } from '~/Components/Modal.js'
 import { TextInputField } from '~/Components/TextInputField.js'
 import { makeAbsolutePath } from '~/Resources/util.js'
 import { RootAppStoreContext } from '~/Stores/RootAppStore.js'
+import { CONFIG_OPTION_META, ConfigOptionRow, CONTENT_OPTION_KEYS, SURFACE_CHILD_OPTIONS } from './ConfigSelection.js'
 import { ExportFormatDefault, SelectExportFormat } from './ExportFormat.js'
 
 export const ExportWizardModal = observer(function ExportWizardModal() {
@@ -114,237 +114,159 @@ export const ExportWizardModal = observer(function ExportWizardModal() {
 							}}
 						>
 							<Modal.Body>
-								<div>
-									<h5>Export Options</h5>
-									<p>Please select the components you'd like to export:</p>
-									<div className="ms-2 mb-1">
-										<form.Field
-											name="connections"
-											children={(field) => (
-												<CheckboxInputFieldWithLabel
-													id="wizard_connections"
-													value={field.state.value}
-													setValue={field.handleChange}
-													onBlur={field.handleBlur}
-													label="Connections"
-												/>
-											)}
-										/>
+								<div className="config-selection">
+									<p className="config-selection-intro">Choose what to include in your export.</p>
+
+									<div className="config-selection-section">
+										<div className="config-selection-title">Connections</div>
+										<div className="config-selection-list">
+											<form.Field
+												name="connections"
+												children={(field) => (
+													<ConfigOptionRow
+														icon={CONFIG_OPTION_META.connections.icon}
+														label={CONFIG_OPTION_META.connections.label}
+														value={field.state.value}
+														setValue={field.handleChange}
+														onBlur={field.handleBlur}
+													/>
+												)}
+											/>
+											<form.Field
+												name="includeSecrets"
+												children={(field) => (
+													<form.Subscribe
+														selector={(state) => [state.values.connections]}
+														children={([connections]) => (
+															<ConfigOptionRow
+																sub
+																icon={faKey}
+																value={field.state.value}
+																setValue={field.handleChange}
+																disabled={!connections}
+																onBlur={field.handleBlur}
+																label={
+																	<>
+																		Include secrets
+																		<InlineHelpIcon className="ms-1">
+																			Some connections have secret values that can be omitted from the export. Not all
+																			modules are compatible with this.
+																		</InlineHelpIcon>
+																	</>
+																}
+															/>
+														)}
+													/>
+												)}
+											/>
+										</div>
 									</div>
-									<div className="ms-2 mb-1">
-										<form.Field
-											name="includeSecrets"
-											children={(field) => (
-												<form.Subscribe
-													selector={(state) => [state.values.connections]}
-													children={([connections]) => (
-														<CheckboxInputFieldWithLabel
-															id="wizard_include_secrets"
-															className="ms-4"
-															value={field.state.value}
+
+									<div className="config-selection-section">
+										<div className="config-selection-title">Content</div>
+										<div className="config-selection-list">
+											{CONTENT_OPTION_KEYS.map((key) => (
+												<form.Field
+													key={key}
+													name={key}
+													children={(field) => (
+														<ConfigOptionRow
+															icon={CONFIG_OPTION_META[key].icon}
+															label={CONFIG_OPTION_META[key].label}
+															value={!!field.state.value}
 															setValue={field.handleChange}
-															disabled={!connections}
 															onBlur={field.handleBlur}
-															label={
-																<>
-																	Include secrets
-																	<InlineHelpIcon className="ms-1">
-																		Some connections have secret values that can be omitted from the export. Not all
-																		modules are compatible with this.
-																	</InlineHelpIcon>
-																</>
-															}
 														/>
 													)}
 												/>
-											)}
-										/>
-									</div>
-									<div className="ms-2 mb-1">
-										<form.Field
-											name="buttons"
-											children={(field) => (
-												<CheckboxInputFieldWithLabel
-													value={field.state.value}
-													setValue={field.handleChange}
-													onBlur={field.handleBlur}
-													label="Buttons"
-												/>
-											)}
-										/>
-									</div>
-									<div className="ms-2 mb-1">
-										<form.Field
-											name="triggers"
-											children={(field) => (
-												<CheckboxInputFieldWithLabel
-													value={field.state.value}
-													setValue={field.handleChange}
-													onBlur={field.handleBlur}
-													label="Triggers"
-												/>
-											)}
-										/>
-									</div>
-									<div className="ms-2 mb-1">
-										<form.Field
-											name="customVariables"
-											children={(field) => (
-												<CheckboxInputFieldWithLabel
-													value={field.state.value}
-													setValue={field.handleChange}
-													onBlur={field.handleBlur}
-													label="Custom Variables"
-												/>
-											)}
-										/>
-									</div>
-									<div className="ms-2 mb-1">
-										<form.Field
-											name="expressionVariables"
-											children={(field) => (
-												<CheckboxInputFieldWithLabel
-													value={field.state.value}
-													setValue={field.handleChange}
-													onBlur={field.handleBlur}
-													label="Expression Variables"
-												/>
-											)}
-										/>
-									</div>
-									<div className="ms-2 mb-1">
-										<form.Field
-											name="surfaces"
-											children={(field) => {
-												const isAChildChecked = !!field.state.value && Object.values(field.state.value).some((v) => !!v)
-												const isAChildUnchecked =
-													!!field.state.value && Object.values(field.state.value).some((v) => !v)
-
-												return (
-													<CheckboxInputFieldWithLabel
-														indeterminate={isAChildChecked && isAChildUnchecked}
-														value={isAChildChecked}
-														setValue={(value) =>
-															field.handleChange(
-																value
-																	? {
-																			known: true,
-																			instances: true,
-																			remote: true,
-																		}
-																	: null
-															)
-														}
-														onBlur={field.handleBlur}
-														label="Surfaces"
-													/>
-												)
-											}}
-										/>
-									</div>
-									<div className="ms-2 mb-1">
-										<form.Field
-											name="surfaces.known"
-											children={(field) => (
-												<CheckboxInputFieldWithLabel
-													className="ms-4"
-													value={!!field.state.value}
-													setValue={field.handleChange}
-													onBlur={field.handleBlur}
-													label={
-														<>
-															Known Surfaces
-															<InlineHelpIcon className="ms-1">
-																The list of known surfaces, and their settings
-															</InlineHelpIcon>
-														</>
-													}
-												/>
-											)}
-										/>
-									</div>
-									<div className="ms-2 mb-1">
-										<form.Field
-											name="surfaces.instances"
-											children={(field) => (
-												<CheckboxInputFieldWithLabel
-													className="ms-4"
-													value={!!field.state.value}
-													setValue={field.handleChange}
-													onBlur={field.handleBlur}
-													label={
-														<>
-															Surface Integrations
-															<InlineHelpIcon className="ms-1">The configured surface integrations</InlineHelpIcon>
-														</>
-													}
-												/>
-											)}
-										/>
-									</div>
-									<div className="ms-2 mb-1">
-										<form.Field
-											name="surfaces.remote"
-											children={(field) => (
-												<CheckboxInputFieldWithLabel
-													className="ms-4"
-													value={!!field.state.value}
-													setValue={field.handleChange}
-													onBlur={field.handleBlur}
-													label={
-														<>
-															Remote Surfaces
-															<InlineHelpIcon className="ms-1">
-																Connections for surfaces that are connected remotely
-															</InlineHelpIcon>
-														</>
-													}
-												/>
-											)}
-										/>
+											))}
+										</div>
 									</div>
 
-									<div className="ms-2 mb-1">
-										<form.Field
-											name="imageLibrary"
-											children={(field) => (
-												<CheckboxInputFieldWithLabel
-													value={field.state.value}
-													setValue={field.handleChange}
-													onBlur={field.handleBlur}
-													label="Image Library"
+									<div className="config-selection-section">
+										<div className="config-selection-title">Surfaces</div>
+										<div className="config-selection-list">
+											<form.Field
+												name="surfaces"
+												children={(field) => {
+													const isAChildChecked =
+														!!field.state.value && Object.values(field.state.value).some((v) => !!v)
+													const isAChildUnchecked =
+														!!field.state.value && Object.values(field.state.value).some((v) => !v)
+
+													return (
+														<ConfigOptionRow
+															icon={CONFIG_OPTION_META.surfaces.icon}
+															label={CONFIG_OPTION_META.surfaces.label}
+															indeterminate={isAChildChecked && isAChildUnchecked}
+															value={isAChildChecked}
+															setValue={(value) =>
+																field.handleChange(
+																	value
+																		? {
+																				known: true,
+																				instances: true,
+																				remote: true,
+																			}
+																		: null
+																)
+															}
+															onBlur={field.handleBlur}
+														/>
+													)
+												}}
+											/>
+											{SURFACE_CHILD_OPTIONS.map((child) => (
+												<form.Field
+													key={child.key}
+													name={`surfaces.${child.key}`}
+													children={(field) => (
+														<ConfigOptionRow
+															sub
+															icon={child.icon}
+															label={child.label}
+															value={!!field.state.value}
+															setValue={field.handleChange}
+															onBlur={field.handleBlur}
+														/>
+													)}
 												/>
-											)}
-										/>
+											))}
+										</div>
 									</div>
 
-									<div style={{ paddingTop: '1em' }}>
-										<FormLabel htmlFor={exportFormatId}>File format</FormLabel>
-										<form.Field
-											name="format"
-											children={(field) => (
-												<SelectExportFormat
-													id={exportFormatId}
-													value={field.state.value}
-													setValue={(val) => field.handleChange(val)}
+									<div className="config-selection-section">
+										<div className="config-selection-title">Output</div>
+										<div className="export-output">
+											<div className="export-output-field">
+												<FormLabel htmlFor={exportFormatId}>File format</FormLabel>
+												<form.Field
+													name="format"
+													children={(field) => (
+														<SelectExportFormat
+															id={exportFormatId}
+															value={field.state.value}
+															setValue={(val) => field.handleChange(val)}
+														/>
+													)}
 												/>
-											)}
-										/>
-									</div>
-									<div style={{ paddingTop: '1em' }}>
-										<FormLabel htmlFor={exportNameId}>File name</FormLabel>
-										<form.Field
-											name="filename"
-											children={(field) => (
-												<TextInputField
-													id={exportNameId}
-													value={String(field.state.value)}
-													setValue={(val) => field.handleChange(val)}
-													useVariables
-													immediateValue
+											</div>
+											<div className="export-output-field">
+												<FormLabel htmlFor={exportNameId}>File name</FormLabel>
+												<form.Field
+													name="filename"
+													children={(field) => (
+														<TextInputField
+															id={exportNameId}
+															value={String(field.state.value)}
+															setValue={(val) => field.handleChange(val)}
+															useVariables
+															immediateValue
+														/>
+													)}
 												/>
-											)}
-										/>
+											</div>
+										</div>
 									</div>
 								</div>
 							</Modal.Body>
