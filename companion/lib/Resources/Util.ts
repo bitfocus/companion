fae35d06282e74711eeee65d5133b6c880bfabd0
Julian Waller <git@julusian.co.uk>
2026-06-30 16:20:29 +0000

    fix: satellite message parsing not allowing = in values

--- companion/lib/Resources/Util.ts
+++ companion/lib/Resources/Util.ts
@@ -188,9 +188,19 @@ export function parseLineParameters(line: string): ParsedParams {
 	const res: ParsedParams = Object.create(null)
 
 	for (const fragment of fragments) {
-		const [key, value] = fragment.split('=', 2)
-		if (BANNED_PROPS.has(key)) continue
-		res[key] = value === undefined ? true : value
+		// Split on the first `=` only, keeping the rest of the value intact. A plain
+		// `split('=', 2)` would truncate at the first `=`, which corrupts values that
+		// legitimately contain it - e.g. the base64 `=` padding of a `data:` url bitmap,
+		// breaking image decoding.
+		const splitIndex = fragment.indexOf('=')
+		if (splitIndex === -1) {
+			if (BANNED_PROPS.has(fragment)) continue
+			res[fragment] = true
+		} else {
+			const key = fragment.substring(0, splitIndex)
+			if (BANNED_PROPS.has(key)) continue
+			res[key] = fragment.substring(splitIndex + 1)
+		}
 	}
 
 	return res
